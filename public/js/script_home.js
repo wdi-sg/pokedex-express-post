1a9f04a1b24fef25e745e90954990f66b6b4b33b
Kok Chee Kean <kok.chee.kean@outlook.com>
2018-03-22 02:28:45 +0800

    Refactored and redesigned code, added comments, CSS, JS, CSS Grids, Toggle Search Criteria, Show Partial Results

--- public/js/script_home.js
+++ public/js/script_home.js
@@ -5,6 +5,18 @@ function revealContent(){
     this.style.display = 'none';
 }
 
+function toggleSort() {
+    var button = document.querySelector('#btn-sort');
+    if (window.location.search === "?sortby=name") {
+        button.textContent = "Sort by ID";
+        button.parentNode.setAttribute('href', '/sort?sortby=id');
+    } else if (window.location.search === "?sortby=id") {
+        button.textContent = "Sort by Name";
+        button.parentNode.setAttribute('href', '/sort?sortby=name');
+    }
+}
+
 document.addEventListener('DOMContentLoaded', function(){
     document.querySelector('#btn-reveal').addEventListener('click', revealContent);
+    toggleSort();
 })
