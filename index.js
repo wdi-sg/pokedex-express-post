ec28c2be654ae4a40df96e02806eb07a0b54c853
Edmund <edmundcjj@gmail.com>
2018-03-21 19:58:54 +0800

    Updated logic to create new pokemon with automated id and num increment

--- index.js
+++ index.js
@@ -1,28 +1,82 @@
+/**
+ * ===================================
+ * Import libraries
+ * ===================================
+ */
+ // Express Library
 const express = require('express');
+
+// Express Handlebar Library
 const handlebars = require('express-handlebars');
+
+// Jsonfile Library
 const jsonfile = require('jsonfile');
 
-const FILE = 'pokedex.json';
+// Body-parser Library
+const bodyParser = require('body-parser');
 
 /**
  * ===================================
  * Configurations and set up
  * ===================================
  */
-
-// Init express app
 const app = express();
+const FILE = 'pokedex.json';
+
+// Serving css static files
+app.use(express.static('css'));
 
 // Set handlebars to be the default view engine
 app.engine('handlebars', handlebars.create().engine);
 app.set('view engine', 'handlebars');
+
+// tell your app to use the module
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 
 /**
  * ===================================
  * Routes
  * ===================================
  */
+ // Display form to create new pokemon
+ app.get('/new', (request, response) => {
+   response.render('newPokemon');
+ });
 
+// Display Homepage
+ app.get('/home', (request, response) => {
+   response.render('home');
+ });
+
+ // Handle get request to sort the pokemons by name
+
+
+ // Handle post request to create new pokemon
+ app.post('/', (request, response) => {
+   //debug code (output request body)
+   console.log(request.body);
+
+   jsonfile.readFile(FILE, (err, obj) => {
+     // Create new id and num for the pokemon
+     let pokemon_length = obj.pokemon.length + 1;
+     request.body.id = pokemon_length;
+     request.body.num = pokemon_length;
+     obj.pokemon.push(request.body);
+
+     // save the request body
+     jsonfile.writeFile(FILE, obj, (err) => {
+       console.error(err)
+
+       // now look inside your json file
+       response.send(request.body);
+     });
+   });
+ });
+
+ // Display pokemon stats of a particular pokemon
 app.get('/:id', (request, response) => {
   // get json from specified file
   jsonfile.readFile(FILE, (err, obj) => {
