d40e6a94968a55acde0e4b6b2dfc99cc686f7756
stonefruit <jeromeng888@gmail.com>
2018-03-21 23:12:35 +0800

    complete exercise minus full layout styling

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
+"use strict";
+
 const express = require('express');
 const handlebars = require('express-handlebars');
 const jsonfile = require('jsonfile');
-const pokedex = require('./pokedex.json');
-
+const bodyParser = require('body-parser');
+const FILE = 'pokedex.json';
 /**
  * ===================================
  * Configurations and set up
@@ -16,19 +18,46 @@ const app = express();
 app.engine('handlebars', handlebars.create().engine);
 app.set('view engine', 'handlebars');
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+app.use(express.static('public'));
+
 /**
  * ===================================
  * Routes
  * ===================================
  */
 
+app.get('/new', (request, response) => {
+  response.render('new_pokemon');
+});
+
+app.post('/', (request, response) => {
+  let newPokemon = request.body;
+  jsonfile.readFile(FILE, (err, obj) => {
+    newPokemon.id = obj.pokemon.length + 1;
+    newPokemon.num = obj.pokemon.length + 1;
+    obj.pokemon.push(newPokemon);
+
+    jsonfile.writeFile(FILE, obj, (err) => {
+      response.send("SUCCESS!");
+    });
+  });
+});
+
 app.get('/:id', (request, response) => {
+  // get json from specified file
+  jsonfile.readFile(FILE, (err, obj) => {
+    // obj is the object from the pokedex json file
     // extract input data from request
     let inputId = request.params.id;
 
     // find pokemon by id from the pokedex json file
     // (note: find() is a built-in method of JavaScript arrays)
-    let pokemon = pokedex.pokemon.find((currentPokemon) => {
+    let pokemon = obj.pokemon.find((currentPokemon) => {
       return currentPokemon.id === parseInt(inputId, 10);
     });
 
@@ -36,6 +65,7 @@ app.get('/:id', (request, response) => {
       // send 404 back
       response.render('404');
     } else {
+      pokemon = [pokemon];
       let context = {
         pokemon: pokemon
       };
@@ -43,6 +73,30 @@ app.get('/:id', (request, response) => {
       // send html file back with pokemon's data
       response.render('pokemon', context);
     }
+  });
+});
+
+app.get('/', (request, response) => {
+  // get json from specified file
+  jsonfile.readFile(FILE, (err, obj) => {
+    let pokemon = obj.pokemon;
+    if (request.query.sortby === "name") {
+      pokemon.sort((a, b) => {
+        if (a.name > b.name)
+            return 1;
+        else if (a.name < b.name)
+            return -1;
+        else return 0;
+      });
+    }
+    let context = {
+      pokemon: pokemon
+    };
+
+    // send html file back with pokemon's data
+    response.render('pokemon', context);
+
+  });
 });
 
 /**
@@ -51,3 +105,9 @@ app.get('/:id', (request, response) => {
  * ===================================
  */
 app.listen(3000, () => console.log('~~~ Tuning in to the waves of port 3000 ~~~'));
+
+// test = [{name:'Ponyta'},{name:''},{name:'Bulbasaur'},{name:'Articuno'}];
+
+// test.sort((a,b) => {
+//   return b.name < a.name;
+// });
