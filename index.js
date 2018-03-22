32900f224ac83f1a767e059fc4415771ba96f57f
Edmund <edmundcjj@gmail.com>
2018-03-22 17:10:32 +0800

    Completed with the sorting logic and display the sorted pokemons by name

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
  * Import libraries
  * ===================================
  */
- // Express Library
+// Express Library
 const express = require('express');
 
 // Express Handlebar Library
@@ -14,6 +14,9 @@ const jsonfile = require('jsonfile');
 
 // Body-parser Library
 const bodyParser = require('body-parser');
+
+// Cookier Parser Library
+const cookieParser = require('cookie-parser');
 
 /**
  * ===================================
@@ -36,66 +39,114 @@ app.use(bodyParser.urlencoded({
   extended: true
 }));
 
+// Use the cookie parser library
+app.use(cookieParser())
+
 /**
  * ===================================
  * Routes
  * ===================================
  */
- // Handle get request to sort the pokemons by name
-app.get('/', (request, response) => {
-  console.log("Entered get function");
-  let context = {
-    pokemons: []
-  }
-  jsonfile.readFile(FILE, (err, obj) => {
-    let poke_length = obj.pokemon.length;
-
-    for (var i = 0; i < poke_length; i++) {
-      let poke_object = {};
-      poke_object.img = obj.pokemon[i].img;
-      poke_object.num = obj.pokemon[i].num;
-      poke_object.name = obj.pokemon[i].name;
-      context.pokemons.push(poke_object);
-    }
-
-    // now look inside your json file
-    response.render('home', context);
-  });
- });
-
- // Display form to create new pokemon
- app.get('/new', (request, response) => {
-   response.render('newPokemon');
- });
-
-// Display Homepage
+ // Handle get request to sort the pokemons by their num and id
  app.get('/home', (request, response) => {
-   response.render('home');
- });
-
- // Handle post request to create new pokemon
- app.post('/newpokemon', (request, response) => {
-   //debug code (output request body)
-   console.log(request.body);
-
+   console.log("Entered handler to display pokemons based on their id");
+   let context = {
+     pokemons: []
+   }
+   // Read the file and display the pokemons according to their num
    jsonfile.readFile(FILE, (err, obj) => {
-     // Create new id and num for the pokemon
-     let pokemon_length = obj.pokemon.length + 1;
-     request.body.id = pokemon_length;
-     request.body.num = pokemon_length;
-     obj.pokemon.push(request.body);
+     let poke_length = obj.pokemon.length;
 
-     // save the request body
-     jsonfile.writeFile(FILE, obj, (err) => {
-       console.error(err)
+     for (var i = 0; i < poke_length; i++) {
+       let poke_object = {};
+       poke_object.img = obj.pokemon[i].img;
+       poke_object.num = obj.pokemon[i].num;
+       poke_object.name = obj.pokemon[i].name;
+       context.pokemons.push(poke_object);
+     }
 
-       // now look inside your json file
-       response.send(request.body);
-     });
+     // now look inside your json file
+     response.render('home', context);
    });
  });
 
- // Display pokemon stats of a particular pokemon
+ // Handle get request to sort the pokemons by their name
+app.get('/', (request, response) => {
+  console.log("Entered handler to display pokemons based on their name");
+  // // get the currently set cookie
+  // var visits = request.cookies['visits'];
+  // let myname = "Edmundcjj";
+  //
+  // // see if there is a cookie
+  // if (visits === undefined) {
+  //
+  //   // set a default value if it doesn't exist
+  //   visits = 1;
+  // } else {
+  //
+  //   // if a cookie exists, make a value thats 1 bigger
+  //   visits = parseInt(visits) + 1;
+  // }
+
+  let context = {
+    sortedPokemons: []
+  }
+  // Read the file and sort the pokemons by their name in alphabetical order
+  jsonfile.readFile(FILE, (err, obj) => {
+    // sort the pokedex data
+    let sorted_array = obj.pokemon.sort(function(a, b) {
+        var textA = a.name.toUpperCase();
+        var textB = b.name.toUpperCase();
+        return (textA < textB) ? -1 : (textA > textB) ? 1 : 0;
+    });
+
+    let poke_length = sorted_array.length;
+
+    for (var i = 0; i < poke_length; i++) {
+      let poke_object = {};
+      poke_object.img = sorted_array[i].img;
+      poke_object.num = sorted_array[i].num;
+      poke_object.name = sorted_array[i].name;
+      context.sortedPokemons.push(poke_object);
+    }
+
+    // Render the sorted pokemons by name to the html page
+    response.render('sortedPokemons', context);
+  });
+
+  // set the cookie
+  // response.cookie('visits', visits);
+  // response.cookie('myname', myname);
+});
+
+// Display form to create new pokemon
+app.get('/new', (request, response) => {
+  response.render('newPokemon');
+});
+
+// Handle post request to create new pokemon
+app.post('/newpokemon', (request, response) => {
+  //debug code (output request body)
+  console.log(request.body);
+
+  jsonfile.readFile(FILE, (err, obj) => {
+    // Create new id and num for the pokemon
+    let pokemon_length = obj.pokemon.length + 1;
+    request.body.id = pokemon_length;
+    request.body.num = pokemon_length;
+    obj.pokemon.push(request.body);
+
+    // save the request body
+    jsonfile.writeFile(FILE, obj, {spaces: 2}, (err) => {
+      console.error(err)
+
+      // now look inside your json file
+      response.send(request.body);
+    });
+  });
+});
+
+// Display pokemon stats of a particular pokemon
 app.get('/:id', (request, response) => {
   // get json from specified file
   jsonfile.readFile(FILE, (err, obj) => {
