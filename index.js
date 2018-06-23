bb4703fff07e38fc1d77cee2a2fe6cfc2da4eeec
Leon Ho <leonhwh@icloud.com>
2018-06-23 10:48:32 +0800

    trying to understand templating with ejs

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const path = require('path');
 
 // Init express app
 const app = express();
-
+app.set('view engine', 'ejs')
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
   extended: true
@@ -27,27 +27,24 @@ app.use(express.static('public'))
  * ===================================
  */
 
+ app.get('/test', (req, res) => {
+   jsonfile.readFile(FILE, (err, obj) => {
+     let pokemon = obj.pokemon
+     res.render('index.ejs', { pokemonnames : pokemon})
+   })
+ })
+
 app.get('/:id', (request, response) => {
-
-  // get json from specified file
   jsonfile.readFile(FILE, (err, obj) => {
-    // obj is the object from the pokedex json file
-    // extract input data from request
     let inputId = request.params.id;
-
-    // find pokemon by id from the pokedex json file
-    // (note: find() is a built-in method of JavaScript arrays)
     let pokemon = obj.pokemon.find((currentPokemon) => {
       return currentPokemon.id === parseInt(inputId, 10);
     });
 
     if (pokemon === undefined) {
-
-      // send 404 back
       response.status(404);
       response.send("not found");
     } else {
-
       response.send(pokemon);
     }
   });
@@ -56,6 +53,7 @@ app.get('/:id', (request, response) => {
 app.get('/pokemon/new', (req, res) => {
   res.sendFile(path.join(__dirname, '/public', 'form.html'));
 })
+
 app.post('/pokemon', (req, res) => {
   console.log(req.body);
   jsonfile.readFile(FILE, (err, obj) => {
@@ -68,9 +66,6 @@ app.post('/pokemon', (req, res) => {
   res.sendFile(path.join(__dirname, '/public', 'success.html'));
 })
 
-/**
- * ===================================
- * Listen to requests on port 3000
- * ===================================
- */
+
+
 app.listen(3000, () => console.log('~~~ Tuning in to the waves of port 3000 ~~~'));
