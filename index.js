3c2cd37e449bd4a299d79287913d77c0b4e325a7
JulianPoh <julianpoh01@gmail.com>
2018-06-24 13:44:42 +0800

    working on deliverables

--- index.js
+++ index.js
@@ -1,52 +1,60 @@
 const express = require('express');
 const jsonfile = require('jsonfile');
-
+const bodyParser = require('body-parser');
 const FILE = 'pokedex.json';
-
-/**
- * ===================================
- * Configurations and set up
- * ===================================
- */
 
 // Init express app
 const app = express();
 
-/**
- * ===================================
- * Routes
- * ===================================
- */
+//Use public folder for publicly availble static files.
+app.use(express.static('public'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true
+}))
+
 
 app.get('/:id', (request, response) => {
-
   // get json from specified file
   jsonfile.readFile(FILE, (err, obj) => {
     // obj is the object from the pokedex json file
     // extract input data from request
     let inputId = request.params.id;
-
     // find pokemon by id from the pokedex json file
     // (note: find() is a built-in method of JavaScript arrays)
     let pokemon = obj.pokemon.find((currentPokemon) => {
       return currentPokemon.id === parseInt(inputId, 10);
     });
-
     if (pokemon === undefined) {
-
       // send 404 back
       response.status(404);
       response.send("not found");
     } else {
-
       response.send(pokemon);
     }
   });
 });
 
+
 app.get('/', (request, response) => {
-  response.send("yay");
+  response.send("Welcome to the Pokedex!");
 });
+
+
+//Intercept /pokemon/new with a form
+app.get('/pokemon/new', (request, response) => {
+  response.send('<form method="POST" action="/pokemon">' +  
+              '<input type="text" name="id" placeholder="id"/>' +
+              '<input type="text" name="num" placeholder="num"/>' +
+              '<input type="text" name="name" placeholder="name"/>' +
+              '<input type="text" name="img" placeholder="img"/>' +
+              '<input type="text" name="height" placeholder="height"/>' +
+              '<input type="text" name="weight" placeholder="weight"/>' +
+              '<input type="submit" value="Create">' +
+              '</form>');
+});
+
+
 
 /**
  * ===================================
