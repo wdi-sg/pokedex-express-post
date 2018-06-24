6ded69a4aaf039137093baad14fbc6ece649058e
vineethkunnathsg@gmail.com <vineethkunnathsg@gmail.com>
2018-06-25 00:25:25 +0800

    basic deliverables done

--- index.js
+++ index.js
@@ -12,6 +12,19 @@ const FILE = 'pokedex.json';
 // Init express app
 const app = express();
 
+// built-in path module so that we can link to the file
+var path = require('path');
+
+//Allows access to files in public folder
+app.use(express.static('public'));
+
+// tell your app to use the module
+app.use(express.json());
+
+app.use(express.urlencoded({
+  extended: true
+}));
+
 /**
  * ===================================
  * Routes
@@ -43,6 +56,50 @@ app.get('/:id', (request, response) => {
     }
   });
 });
+
+//GET request to link to form.html
+app.get('/pokemon/new', (request, response) => {
+  response.sendFile(path.join(__dirname, '/public', 'form.html'))
+});
+
+
+//when the form is submitted, the input is POSTed
+app.post('/pokemon', function(request, response) {
+  
+  //debug code (output request body)
+  console.log(request.body);
+
+  //reads Json file
+  jsonfile.readFile('pokedex.json', (err,obj) => {
+    
+    let newPoke = {
+            "id": request.body.id,
+            "num": request.body.num,
+            "name": request.body.name,
+            "img": request.body.img,
+            "height": request.body.height,
+            "weight": request.body.weight,
+    }
+    
+    //adds new object into pokemon's array
+    obj.pokemon.push(newPoke)
+    
+    //to make sure var pokemon does not get overwritten
+    let updatedPokedex = obj;
+
+    //Updates Json file
+    jsonfile.writeFile('pokedex.json', updatedPokedex, (err) => {
+
+      response.send('pokemon added to pokedex!');
+
+      });
+
+  })
+  
+
+
+});
+
 
 app.get('/', (request, response) => {
   response.send("yay");
