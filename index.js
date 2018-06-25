2dee26943421dedfd42433e68043b7b4af3323df
mirage1129 <meraj.a.qazi@gmail.com>
2018-06-25 20:31:06 +0800

    finished the first part

--- index.js
+++ index.js
@@ -1,56 +1,70 @@
+// variables and making sure the npms are working 
+
 const express = require('express');
 const jsonfile = require('jsonfile');
-
-const FILE = 'pokedex.json';
-
-/**
- * ===================================
- * Configurations and set up
- * ===================================
- */
-
-// Init express app
+const data = 'pokedex.json';
 const app = express();
 
-/**
- * ===================================
- * Routes
- * ===================================
- */
+//make the html file within the public folder available
+app.use(express.static('public'));
 
-app.get('/:id', (request, response) => {
+//module that lets you grab the contents of the form 
+app.use(express.json());
+app.use(express.urlencoded({
+  extended: true
+}));
 
-  // get json from specified file
-  jsonfile.readFile(FILE, (err, obj) => {
-    // obj is the object from the pokedex json file
-    // extract input data from request
-    let inputId = request.params.id;
+app.post('/pokemon', function(request, response) {
 
-    // find pokemon by id from the pokedex json file
-    // (note: find() is a built-in method of JavaScript arrays)
-    let pokemon = obj.pokemon.find((currentPokemon) => {
-      return currentPokemon.id === parseInt(inputId, 10);
-    });
+  jsonfile.readFile('pokedex.json', (err, obj) => {
+  obj.pokemon.push(request.body);
 
-    if (pokemon === undefined) {
+    jsonfile.writeFile('pokedex.json', obj, (err) => {
+    console.error(err)
 
-      // send 404 back
-      response.status(404);
-      response.send("not found");
-    } else {
+      //debug code (output request body)
+      console.log(request.body);
 
-      response.send(pokemon);
-    }
+      response.send(obj.pokemon[obj.pokemon.length-1]);
+      return
+      // save the request body
+    })
   });
 });
 
-app.get('/', (request, response) => {
-  response.send("yay");
-});
 
-/**
- * ===================================
- * Listen to requests on port 3000
- * ===================================
- */
+
+
+
+
+
+// will allow the JS to access html files in my public folder 
+// app.use(express.static('public'));
+
+// app.use(express.json());
+// app.use(express.urlencoded({
+//   extended: true
+// }));
+
+// var path = require('path');
+
+// // post request that listen for pokemon (its in the action of the html form) and calls a function
+// app.post ('/pokemon', (request, response) => {
+//     console.log(request.body);
+    
+ 
+// // XXXXXXXparse the form data and then push it to the json file  as new pokemon
+
+//   // reads json file 
+//   jsonfile.readFile('pokedex.json', (err, obj) => {
+//       obj.pokemon.push(request.body);
+      
+//       jsonfile.writeFile('pokedex.json', obj, (err) => {
+//         console.log(err)
+//         response.send(obj.pokemon[obj.pokemon.length-1]);
+//       });
+//   });
+// });
+
+
 app.listen(3000, () => console.log('~~~ Tuning in to the waves of port 3000 ~~~'));
