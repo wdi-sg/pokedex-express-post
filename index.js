2e41df86780359ce324b0c7e382569b2be290e7f
JulianPoh <julianpoh01@gmail.com>
2018-06-25 15:47:03 +0800

    completed deliverables

--- index.js
+++ index.js
@@ -15,18 +15,12 @@ app.use(bodyParser.urlencoded({
 
 
 app.get('/:id', (request, response) => {
-  // get json from specified file
   jsonfile.readFile(FILE, (err, obj) => {
-    // obj is the object from the pokedex json file
-    // extract input data from request
     let inputId = request.params.id;
-    // find pokemon by id from the pokedex json files
-    // (note: find() is a built-in method of JavaScript arrays)
     let pokemon = obj.pokemon.find((currentPokemon) => {
       return currentPokemon.id === parseInt(inputId, 10);
     });
     if (pokemon === undefined) {
-      // send 404 back
       response.status(404);
       response.send("not found");
     } else {
@@ -42,47 +36,36 @@ app.get('/', (request, response) => {
 
 //Intercept /pokemon/new with a form
 app.get('/pokemon/new', (request, response) => {
-  let htmlForm = '<header><h1>Add New Pokemon</h1></header><br>' +
-              '<form method="POST" action="/pokemon"><br>' +  
-              '<input type="text" name="id" placeholder="id"/><br>' +
-              '<input type="text" name="num" placeholder="num"/><br>' +
-              '<input type="text" name="name" placeholder="name"/><br>' +
-              '<input type="text" name="img" placeholder="img"/><br>' +
-              '<input type="text" name="height" placeholder="height"/><br>' +
-              '<input type="text" name="weight" placeholder="weight"/><br>' +
-              '<input type="submit" value="Create">' +
-              '</form>'
-  response.send(htmlForm);
+  response.sendFile(__dirname + '/public/form.html');
 });
 
-
-app.post('/pokemon', (request, response) => {
-  let newPokemon = {
-    "id": request.body.id,
-    "num": request.body.num,
-    "name": request.body.name,
-    "img": request.body.img,
-    "height": request.body.height,
-    "weight": request.body.weight,
-  }
-    response.send("New Pokemon Created!");
-  });
+//Adding new Pokemon
+// app.post('/pokemon', (request, response) => {
+//   let newPokemon = {
+//     "id": request.body.id,
+//     "num": request.body.num,
+//     "name": request.body.name,
+//     "img": request.body.img,
+//     "height": request.body.height,
+//     "weight": request.body.weight,
+//   }
+//     response.send("New Pokemon Created!");
+//   });
 
 // add new pokemon
 app.post('/pokemon', (request, response) => {
-  console.log('posted stuff')
+  console.log('Created New Pokemon')
   let data = request.body;
-  // making id a number instead of string
   let id = parseInt(data['id']);
   data['id'] = id;
-  // adding the new pokemon data into the list of pokemon
   jsonfile.readFile(FILE, (err, obj) => {
     obj.pokemon.push(data);
+    console.log(data);
     response.send(obj);
-    // writes the obj with new data inside
     jsonfile.writeFile(FILE, obj);
   })
 });
+
 /**
  * ===================================
  * Listen to requests on port 3000
