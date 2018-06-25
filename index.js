fd54281f10f6c28209c884d6ae15e5bd55c0eb59
Lee Wen Yao <peterleewenyao@gmail.com>
2018-06-25 18:11:05 +0800

    posted

--- index.js
+++ index.js
@@ -1,21 +1,17 @@
 const express = require('express');
 const jsonfile = require('jsonfile');
-const bodyParser = require('body-parser')
+const methodOverride = require('method-override');
 const FILE = 'pokedex.json';
+const path = require('path'); 
 
-/**
- * ===================================
- * Configurations and set up
- * ===================================
- */
-
-// Init express app
 const app = express();
 app.use(express.static('public'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
+app.use(express.json());
+app.use(express.urlencoded({
   extended: true
 }));
+
+let pokemonObj;
 
 app.get('/:id', (request, response) => {
 
@@ -24,7 +20,8 @@ app.get('/:id', (request, response) => {
     // obj is the object from the pokedex json file
     // extract input data from request
     let inputId = request.params.id;
-
+    pokemonObj = obj.pokemon;
+    // console.log(pokemonObj);
     // find pokemon by id from the pokedex json file
     // (note: find() is a built-in method of JavaScript arrays)
     let pokemon = obj.pokemon.find((currentPokemon) => {
@@ -37,49 +34,50 @@ app.get('/:id', (request, response) => {
       response.status(404);
       response.send("not found");
     } else {
-
       response.send(pokemon);
     }
   });
 });
 
 app.get('/', (request, response) => {
-  response.send("yay");
+  response.send("POKEDEX Page");
 });
 
 app.get('/pokemon/new', (request, response) =>{
-  
-  let htmlPage = '<form method="POST">'+
-      'Id: <input type="text" name="Id">'+ 
-      'num: <input type="text" name="num">'+ 
-      'Name: <input type="text" name="name">'+ 
-      'Image: <input type="text" name="img">'+ 
-      'Height: <input type="text" name="weight">'+ 
-      'Weight: <input type="text" name="height">'+ 
-      '<button>Submit</button>'+
-      '</form>';
-  response.send(htmlPage);
+  response.sendFile('index.html', {root: path.join(__dirname, './public')})
 });
 
-app.get('/pokemon', submitPokeData);
 
-function submitPokeData(request, response) {
-  let newInput = request.params['id'];
-  let postHtml = '<form method="POST" action="/'+ newInput + '"><input type="text" name="' + newInput + '"><input type="submit" value="Submit"></form>';
-  response.send(html);
-}
-
-app.post('/:pokemon', createPokemonData);
+app.post('/pokemon', createPokemonData);
 
 function createPokemonData(request, response) {
-  let createdPokeData = request.params['pokemon'];
-  let value = request.body[createdPokeData];
-  pokemonObj[createdPokeData] = value;
+
+  jsonfile.readFile('pokedex.json', (err, obj) => {
+    //pokemonArray = obj.pokemon;
+    console.log("hello",obj.pokemon);
+    let newPokeData = request.body;
+    //pokemonArray.push(newPokeData);
+
+    obj.pokemon.push(newPokeData);
+
+  // console.log(pokemonObj)
+  // console.log(request.body)
+
+    jsonfile.writeFile('pokedex.json', obj, (err) => {
+      console.error(err)
+
+    });
+  
+  });
+  
+
+    // now look inside your json file
+    response.send('pokemon saved');
+
+  //let postHtml = '<form method="POST" action="/pokemon'+ newInput + '"><input type="text" name="' + newInput + '"><input type="submit" value="Submit"></form>';
+  //let createdPokeData = request.params['pokemon'];
+  //let value = request.body[createdPokeData];
+  //pokemonObj[createdPokeData] = value;
 }
 
-/**
- * ===================================
- * Listen to requests on port 3000
- * ===================================
- */
 app.listen(3000, () => console.log('~~~ Tuning in to the waves of port 3000 ~~~'));
