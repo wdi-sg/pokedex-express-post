eb8c42c0c9747e9eec4568a926f6803b75d0b6d1
ChloeLiang <nliangxin@gmail.com>
2018-09-26 14:57:20 +0800

    Add new pokemon

--- index.js
+++ index.js
@@ -4,15 +4,20 @@ const jsonfile = require('jsonfile');
 const FILE = 'pokedex.json';
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({
+  extended: true
+}));
+
 const generateForm = () => {
   let form = '<html>';
   form += '<head>';
   form += '<title>Pokemon</title>';
   form += '</head>';
   form += '<body>';
-  form += '<form>';
+  form += '<form method="POST" action="/pokemon">';
   form += '<label for="id">id</label> <input type="number" name="id"><br>';
-  form += '<label for="num">num</label> <input type="text" name="num"><br>';
+  form += '<label for="num">num</label> <input type="number" name="num"><br>';
   form += '<label for="name">name</label> <input type="text" name="name"><br>';
   form += '<label for="img">img</label> <input type="url" name="img"><br>';
   form += '<label for="height">height</label> <input type="text" name="height"><br>';
@@ -51,6 +56,31 @@ app.get('/:id', (request, response) => {
 
 app.get('/', (request, response) => {
   response.send("yay");
+});
+
+app.post('/pokemon', (request, response) => {
+  jsonfile.readFile(FILE, (err, obj) => {
+    if (err) {
+      response.status(404).send(err);
+    }
+
+    let newPokemon = {};
+    newPokemon.id = request.body.id;
+    newPokemon.num = request.body.num;
+    newPokemon.name = request.body.name;
+    newPokemon.img = request.body.img;
+    newPokemon.height = request.body.height;
+    newPokemon.weight = request.body.weight;
+
+    obj.pokemon.push(newPokemon);
+
+    jsonfile.writeFile(FILE, obj, err => {
+      if (err) {
+        response.status(404).send(err);
+      }
+      response.send('New pokemon saved!');
+    });
+  });
 });
 
 app.listen(3000, () => console.log('~~~ Tuning in to the waves of port 3000 ~~~'));
