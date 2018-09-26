5b853633db463d6053c2df063956d50dfd308ec8
chrismintan <chris_acs@hotmail.com>
2018-09-26 17:57:46 +0800

    Deliverables + sort by name

--- index.js
+++ index.js
@@ -12,11 +12,67 @@ const FILE = 'pokedex.json';
 // Init express app
 const app = express();
 
-/**
- * ===================================
- * Routes
- * ===================================
- */
+app.use(express.json());
+app.use(express.urlencoded({
+  extended: true
+}));
+
+// This first deliverable seems to be asking us to create a form to add a new pokemon to the pokedex.
+
+// Creating a function to save the data added
+let currentJSON = {};
+
+function getCurrentJSON() {
+  //read the json file
+  jsonfile.readFile(FILE, (err, obj) => {
+    //save the json file into a variable
+    currentJSON = obj;
+  })
+}
+
+// Creating HTML page to request materials to be added to Pokedex
+app.get('/pokemon/new', (request, response) => {
+
+    let html = "<html>";
+    html += "<body>";
+    html += '<form method="POST" action="/submit">';
+    html += "New Pokemon: ";
+    html += '<br><input type="text" name="id" placeholder="id"><br>';
+    html += '<input type="text" name="num" placeholder="num"><br>';
+    html += '<input type="text" name="name" placeholder="name"><br>';
+    html += '<input type="text" name="img" placeholder="img"><br>';
+    html += '<input type="text" name="height" placeholder="height"><br>';
+    html += '<input type="text" name="weight" placeholder="weight"><br>';
+    html += '<input type="submit" value="Submit">';
+    html += "</form>";
+    html += "</body>";
+    html += "</html>";
+
+    response.send( html );
+});
+
+app.post('/submit',(request, response) =>{
+
+       jsonfile.readFile('pokedex.json', (err, obj) => {
+        // Save it into the current JSON variable
+        currentJSON = obj;
+        // For each Pokemon, add to the ID count
+        obj.pokemon.forEach(() => {
+           currentIdCount++;
+        });
+
+        // Adding new Pokemon to Pokedex
+        obj.pokemon.push(request.body);
+
+        // Writing to the file the original data + the new data
+        jsonfile.writeFile('data.json', obj, (err) => {
+            console.log(err);
+        });
+    });
+
+    response.send(request.body);
+
+});
 
 app.get('/:id', (request, response) => {
 
@@ -50,9 +106,80 @@ app.get('/:id', (request, response) => {
   });
 });
 
-app.get('/', (request, response) => {
-  response.send("yay");
+
+app.get('*', (request, response) => {
+
+    if ( request.query.sortby == "name" ) {
+
+        namePage();
+
+        let sortedPokemon = currentJSON.pokemon.sort(sortName);
+
+        var sortedNames = "";
+        console.log(sortedPokemon[0].name)
+        response.send(namePage());
+
+    }
+
+    else {
+        response.send(welcome());
+    }
+
 });
+
+    function welcome() {
+        let description = '<h1>Welcome to the online Pokedex!</h1>';
+
+        let pokeList = "";
+
+        for ( var i = 0; i < currentJSON.pokemon.length; i++ ) {
+            let pokes = '<li style="color: orange"><a href="/' + currentJSON.pokemon[i].name + '">' + currentJSON.pokemon[i].name + '</a></li>';
+            pokeList += pokes;
+        }
+
+        var sortButton = `<a href=/?sortby=name>Sort by name?</a>`;
+
+        var page = '<html><body><h1>' + description + '</h1>' + sortButton + '<ul>' + pokeList + '</ul>';
+        page += '</body></html>'
+
+        return page;
+    }
+
+function sortName(a,b) {
+    const nameA = a["name"];
+    const nameB = b["name"];
+
+    let comparision = 0;
+
+    if ( nameA > nameB ) {
+        comparison = 1;
+    } else if ( nameA < nameB ) {
+        comparison = -1;
+    };
+    return comparison;
+};
+
+function namePage() {
+        let sortedPokemon = currentJSON.pokemon.sort(sortName)
+
+        let description = '<h1>Welcome to the online Pokedex!</h1>';
+
+        let pokeList = "";
+
+        for ( var i = 0; i < sortedPokemon.length; i++ ) {
+            let pokes = '<li style="color: orange">' + sortedPokemon[i].name + '</li>';
+            pokeList += pokes;
+        }
+
+        let sortByName = `<a href=/?sortby=name>Sort By Name</a>`
+
+        var page = '<html><body><h1>' + description + '</h1>' + '<ul>' + pokeList + '</ul>';
+        page += '</body></html>'
+
+        return page;
+};
+
+getCurrentJSON();
 
 /**
  * ===================================
