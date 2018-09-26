205daf4c09879e96571918a2ae95d5bbb5ff1aed
Pablo101 <yaguelopezpablo@gmail.com>
2018-09-26 21:23:57 +0800

    PEP Del

--- index.js
+++ index.js
@@ -12,46 +12,38 @@ const FILE = 'pokedex.json';
 // Init express app
 const app = express();
 
+//let app use module
+app.use(express.json());
+ app.use(express.urlencoded({
+   extended: true
+ }));
 /**
  * ===================================
  * Routes
  * ===================================
  */
 
-app.get('/:id', (request, response) => {
+const pokemonStuff = () => {
 
-  // get json from specified file
-  jsonfile.readFile(FILE, (err, obj) => {
-    // obj is the object from the pokedex json file
-    // extract input data from request
-    let inputId = parseInt( request.params.id );
+    let execute = "<html>";
+    execute += "<body>";
+    execute += '<form method="POST" action="/pokemon">';
+    execute += "Pokemon name:";
+    execute += '<p>ID:<input type="text" name="id"><p>';
+    execute += '<p>Num:<input type="text" name="num"><p>';
+    execute += '<p>Name:<input type="text" name="name"><p>';
+    execute += '<p>Img:<input type="text" name="img"><p>';
+    execute += '<p>Height:<input type="text" name="height"><p>';
+    execute += '<p>Weight:<input type="text" name="weight"><p>';
+    execute += '<p>Input:<input type="submit" value="Submit"><p>';
+    execute += "</form>";
+    execute += "</body>";
+    execute += "</html>";
+      return execute;
+};
 
-    var pokemon;
-
-    // find pokemon by id from the pokedex json file
-    for( let i=0; i<obj.pokemon.length; i++ ){
-
-      let currentPokemon = obj.pokemon[i];
-
-      if( currentPokemon.id === inputId ){
-        pokemon = currentPokemon;
-      }
-    }
-
-    if (pokemon === undefined) {
-
-      // send 404 back
-      response.status(404);
-      response.send("not found");
-    } else {
-
-      response.send(pokemon);
-    }
-  });
-});
-
-app.get('/', (request, response) => {
-  response.send("yay");
+  app.get('/pokemon/new', (request, response) => {
+    response.send(pokemonStuff());
 });
 
 /**
