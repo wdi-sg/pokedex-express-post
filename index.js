43cc7acc10271b7f34f94790ee4bb727b0e467c0
ah5ive <ah5ive@gmail.com>
2018-09-26 16:50:29 +0800

    first delieverable done.

--- index.js
+++ index.js
@@ -11,12 +11,15 @@ const FILE = 'pokedex.json';
 
 // Init express app
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({
+  extended: true
+}));
+// *
+//  * ===================================
+ // app.use('/', express.static(__dirname + '/public'));
+ // * ===================================
 
-/**
- * ===================================
- * Routes
- * ===================================
- */
 
 app.get('/:id', (request, response) => {
 
@@ -53,7 +56,63 @@ app.get('/:id', (request, response) => {
 app.get('/', (request, response) => {
   response.send("yay");
 });
+//form post from user
+app.post('/pokemon', function(request, response) {
 
+    let myObj = {
+            id: parseInt(request.body["id"]),
+            num: request.body['num'],
+            name: request.body['name'],
+            img: request.body['img'],
+            height: request.body['height'],
+            weight: request.body['weight'],
+        }
+        console.log("myObj",myObj);
+
+    jsonfile.readFile(FILE, (err, obj) => {
+        //bring in the file
+        // let testfile = 'data.json';
+
+        obj["pokemon"].push(myObj);
+        //debug code (output request body)
+       // console.log(request.body);
+
+
+        // save the request body
+  jsonfile.writeFile('pokedex.json', /*the pokedex obj*/obj, (err) => {
+    console.error(err)
+
+    // now look inside your json file
+    //response.send is like a console.log
+    response.send(myObj);//change to the var myObj
+        });
+    });
+});
+//form get info from user
+app.get('/pokemon/new', (request, response) => {
+
+
+    let html = "<html>";
+    // html += '<head><link rel="stylesheet" href="public/main.css"></head>'
+    html += "<body>";
+    html += '<form name="myPokemon" method="POST" action="/pokemon">';
+    html += "<h2>Creat A New Pokemon:</h2>";
+    html += 'ID: <input type="text" name="id"></br>';
+    html += 'Num: <input type="text" name="num"></br>';
+    html += 'Name: <input type="text" name="name"></br>';
+    html += 'Upload Image: <input type="file" name="img"></br>';
+    html += 'Height: <input type="text" name="height"></br>';
+    html += 'Weight: <input type="text" name="weight"></br>';
+    html += '<input type="submit" value="Submit">';
+    html += "</form>";
+    html += "</body>";
+    html += "</html>";
+
+    response.send( html );
+
+  // render a template form here
+  //response.send("hello world");
+});
 /**
  * ===================================
  * Listen to requests on port 3000
