96b4e5ec69d160857f164d312e10bc4624b8ff37
chanleyou <chanleyou@gmail.com>
2018-09-27 00:03:27 +0800

    finished assignment

--- index.js
+++ index.js
@@ -24,33 +24,31 @@ app.get('/pokemon/new', (request, response) => {
   html += "<html><body>";
   html += `<form method="POST" action="/pokemon">`;
   html += "<h1>Make a new Pokemon:</h1>";
-  html += `ID: <input type="text" name="id"><br>`;
-  html += `No: <input type="text" name="num"><br>`;
   html += `Name: <input type="text" name="name"><br>`;
   html += `Image: <input type="text" name="img"><br>`;
   html += `Height: <input type="text" name="height"><br>`;
   html += `Weight: <input type="text" name="weight"><br>`;
   html += '<input type="submit" value="Submit"><br>';
   html += "</form>";
-  html += "</body></html>";
 
   response.send(html);
 })
 
 app.post('/pokemon', (request, response) => {
 
-  let newPokemon = {};
-  newPokemon["data"] = request.body;
-  newPokemon["id"] = request.body.id;
-  newPokemon["num"] = request.body.num;
-  newPokemon["name"] = request.body.name;
-  newPokemon["img"] = request.body.img;
-  newPokemon["height"] = request.body.height;
-  newPokemon["weight"] = request.body.weight;
 
   jsonfile.readFile(FILE, (err, obj) => {
 
+    let newPokemon = {};
     let pokedex = obj;
+
+    newPokemon["id"] = parseInt(obj.pokemon.length);
+    newPokemon["num"] = obj.pokemon.length;
+    newPokemon["name"] = request.body.name;
+    newPokemon["img"] = request.body.img;
+    newPokemon["height"] = request.body.height;
+    newPokemon["weight"] = request.body.weight;
+
 
     pokedex.pokemon.push(newPokemon);
 
@@ -63,14 +61,86 @@ app.post('/pokemon', (request, response) => {
 })
 
 app.get('/', (request, response) => {
-  response.send("This is where the Pokedex should be.");
+
+  console.log(request.query);
+
+  let sortBy = request.query.sortby;
+
+  if (sortBy) {
+
+    jsonfile.readFile(FILE, (err, obj) => {
+      if (err) console.log(err);
+
+      let pokedex = obj.pokemon;
+
+      for (let i in pokedex) {
+        if (sortBy === "spawn_time") {
+          let value = pokedex[i][sortBy].split(":");
+          value = parseInt(value[0]*60) + parseInt(value[1]);
+          pokedex[i][sortBy] = value;
+        } else {
+          let value = pokedex[i][sortBy].toString();
+          value = value.replace(/"m"|"kg"/gi, "");
+          value = parseFloat(value);
+          pokedex[i][sortBy] = value;
+        }
+      }
+
+      let sorted = pokedex.sort((a, b) => {
+        return a[sortBy] - b[sortBy];
+      })
+
+      let html = "";
+
+      html += "<html><body>";
+      html += "<h2>Listing all Pokemon sorted by: " + sortBy + "</h2>";
+      html += "<ol>";
+
+      for (let i in sorted) {
+        html += `<li>${sorted[i].name}</li>`;
+      }
+
+      html += "</ol>";
+      html += "<h2>Sort by:</h2>"
+      html += `<form method="GET" action="/">`;
+      html += `<select name="sortby">`;
+      html += `<option value="id" selected>ID Number</option>`;
+      html += `<option value="height">Height</option>`;
+      html += `<option value="weight">Weight</option>`;
+      html += `<option value="avg_spawns">Average Spawns</option>`;
+      html += `<option value="spawn_time">Spawn Time</option>`;
+      html += "</select>";
+      html += '<input type="submit" value="Submit"><br>';
+      html += `</form>`
+      html += "</body></html>"
+
+      response.send(html);
+    })
+
+
+  } else {
+
+    let html = "";
+
+    html += "<html><body>";
+    html += "<h2>Sort By:</h2>"
+    html += `<form method="GET" action="/">`;
+    html += `<select name="sortby">`;
+    html += `<option value="id" selected>ID Number</option>`;
+    html += `<option value="height">Height</option>`;
+    html += `<option value="weight">Weight</option>`;
+    html += `<option value="candy_count">Candy Count</option>`;
+    html += `<option value="avg_spawns">Average Spawns</option>`;
+    html += `<option value="spawn_time">Spawn Time</option>`;
+    html += "</select>";
+    html += '<input type="submit" value="Submit"><br>';
+    html += `</form>`
+    html += "</body></html>";
+
+    response.send(html);
+  }
+
 })
 
-
-
-/**
- * ===================================
- * Listen to requests on port 3000
- * ===================================
- */
-app.listen(3000, () => console.log('~~~ Tuning in to the waves of port 3000 ~~~'));
+const PORT = 3000;
+app.listen(PORT, () => console.log("Listening on port: " + PORT));
