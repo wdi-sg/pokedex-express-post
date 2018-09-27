501599807ab546644c7fa92249544c7f4b9c677d
wenosaurus <wen.mcjawn@gmail.com>
2018-09-27 16:51:50 +0800

    WIP

--- index.js
+++ index.js
@@ -18,53 +18,33 @@ app.use(express.urlencoded({
   extended: true
 }));
 
+// this line below, sets a layout look to your express project
+const reactEngine = require('express-react-views').createEngine();
+app.engine('jsx', reactEngine);
+
+// this tells express where to look for the view files
+app.set('views', __dirname + '/views');
+
+// this line sets react to be the default view engine
+app.set('view engine', 'jsx');
+
 /**
  * ===================================
  * Routes
  * ===================================
  */
 
-app.get('/:id', (request, response) => {
-
-  // get json from specified file
-  jsonfile.readFile(FILE, (err, obj) => {
-    // obj is the object from the pokedex json file
-    // extract input data from request
-    let inputId = parseInt( request.params.id );
-
-    var pokemon;
-
-    // find pokemon by id from the pokedex json file
-    console.log('INPUT ID:', inputId);
-
-    for( let i=0; i<obj.pokemon.length; i++ ){
-
-      let currentPokemon = obj.pokemon[i];
-      console.log('CURRENT ID:', currentPokemon.id);
-
-      if( currentPokemon.id === inputId ){
-        console.log('HELOLLOLOLLLLLL');
-        pokemon = currentPokemon;
-      }
-    }
-
-    if (pokemon === undefined) {
-
-      // send 404 back
-      response.status(404);
-      response.send("not found");
-    } else {
-
-      response.send(pokemon);
-    }
-  });
-});
-
 app.get('/', (request, response) => {
   response.send("yay");
 });
 
-app.get('/pokemon/new', (request, response) => {
+ /**
+ * ===================================
+ * Create New Pokemon
+ * ===================================
+ */
+
+ app.get('/pokemon/new', (request, response) => {
     let html = `<html>`;
     html += `<body>`;
     html += `<form method="POST" action="/pokemon">`;
@@ -85,18 +65,151 @@ app.get('/pokemon/new', (request, response) => {
 
 });
 
+/**
+ * ===================================
+ * Add Edits to JSON file
+ * ===================================
+ */
+
+ app.put('/pokemon/:id', (request, response) => {
+
+    console.log("REQ BODY:", request.body )
+
+    // get what is currently in file
+
+        //n change it with what's in the request
+
+    jsonfile.readFile( FILE, (err, obj) => {
+          // console.log( "obj:",obj );
+          console.log( "err:",err );
+
+          let requestedPokemonId = parseInt( request.params.id );
+
+          for( let i=0; i< obj.pokemon.length; i++){
+
+            if( obj.pokemon[i].id === requestedPokemonId){
+
+                var foundPokemonIndex = i;
+
+                var foundPokemon = obj.pokemon[i];
+            }
+          }
+
+          if( foundPokemon ){
+
+            console.log("FOUND:", foundPokemon );
+
+            obj.pokemon[foundPokemonIndex] = request.body;
+
+            obj.pokemon[foundPokemonIndex].id = parseInt( obj.pokemon[foundPokemonIndex].id )
+
+            jsonfile.writeFile(FILE, obj, function (err) {
+                if (err) console.log("ERROR:",err)
+
+                response.send("FOUND, WORKS")
+            })
+
+          } else{
+              response.send("Pokemon not found");
+          }
+      });
+
+})
+
+/**
+ * ===================================
+ * React
+ * ===================================
+ */
+
+ app.get('/pokemon/:id/edit', (request, response) => {
+
+    jsonfile.readFile(FILE, (err, obj) => {
+    // obj is the object from the pokedex json file
+    // extract input data from request
+    let requestPokemonId = parseInt( request.params.id );
+
+    for( let i=0; i < obj.pokemon.length; i++ ){
+
+        if( obj.pokemon[i].id === requestPokemonId ){
+
+            var foundPokemon = obj.pokemon[i];
+        }
+    }
+    if(foundPokemon){
+        console.log("FOUND:", foundPokemon );
+
+        response.render('home', foundPokemon);
+
+    } else {
+        response.send("Pokemon not found");
+    }
+
+});
+
+  // running this will let express to run home.handlebars file in your views folder
+
+});
+
+ /**
+ * ===================================
+ * ID
+ * ===================================
+ */
+
+app.get('/pokemon/:id', (request, response) => {
+
+  // get json from specified file
+  jsonfile.readFile(FILE, (err, obj) => {
+    // obj is the object from the pokedex json file
+    // extract input data from request
+    let inputId = parseInt( request.params.id );
+
+    var pokemon;
+
+    // find pokemon by id from the pokedex json file
+    // console.log('INPUT ID:', inputId);
+
+    for( let i=0; i<obj.pokemon.length; i++ ){
+
+      let currentPokemon = obj.pokemon[i];
+      // console.log('CURRENT ID:', currentPokemon.id);
+
+      if( currentPokemon.id === inputId ){
+        pokemon = currentPokemon;
+      }
+    }
+
+    if (pokemon === undefined) {
+
+      // send 404 back
+      response.status(404);
+      response.send("not found");
+    } else {
+
+      response.send(pokemon);
+    }
+  });
+});
+
+/**
+ * ===================================
+ * Add to JSON file
+ * ===================================
+ */
+
 app.post('/pokemon', function(request, response) {
 
   //debug code (output request body)
   // console.log(request.body);
 
-  // let file = 'pokedex.json';
+  let file = 'pokedex.json';
 
   const obj = request.body;
 
   obj['id'] = parseInt(obj['id']);
 
-  console.log(obj['id']);
+  // console.log(obj['id']);
 
   jsonfile.readFile(FILE, (err, obj) => {
 
