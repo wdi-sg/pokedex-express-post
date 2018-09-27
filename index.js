a475645ff154e4543405caf5ab399caa8986510f
Pablo101 <yaguelopezpablo@gmail.com>
2018-09-27 15:50:42 +0800

    updated push form

--- index.js
+++ index.js
@@ -47,6 +47,74 @@ const pokemonStuff = () => {
     response.send(pokemonStuff());
 });
 
+// //pre-load with s/edits
+// app.get('/:id', (request, response) => {
+//   // get json from specified file
+//   jsonfile.readFile(FILE, (err, obj) => {
+//     // obj is the object from the pokedex json file
+//     // extract input data from request
+//     let inputId = parseInt( request.params.id );
+//     let pokemon;
+
+//     // find pokemon by id from the pokedex json file
+//     for( let i=0; i<obj.pokemon.length; i++ ){
+//       let currentPokemon = obj.pokemon[i];
+//       if( currentPokemon.id === inputId){
+//         pokemon = currentPokemon;
+//       }
+//     }
+//     if (pokemon === undefined) {
+
+//       // send 404 back
+//       response.status(404);
+//       response.send("not found");
+//     } else {
+
+//       response.send(pokemon);
+//     }
+//   });
+// });
+
+
+app.get('/', (request, response) => {
+   response.send("yay");
+ });
+
+
+app.post('/pokemon', function (request, response)  {
+console.log(request.body)
+
+    jsonfile.readFile(FILE, (err,obj) => {
+    if (err) {
+      console.log("ERROR", err)
+    }
+  })
+
+let file = 'data.json'
+const obj = request.body;
+
+ let newPokemon = [];
+    newPokemon.id = request.body.id;
+    newPokemon.num = request.body.num;
+    newPokemon.name = request.body.name;
+    newPokemon.img = request.body.img;
+    newPokemon.height = request.body.height;
+    newPokemon.weight = request.body.weight;
+//adding new pokemon
+      // obj.pokemon.push(newPokemon);
+      newPokemon.push(pokemonStuff.name)
+
+
+//saving request.body
+  jsonfile.writeFile(FILE, obj, function(err) {
+    if (err) {
+      console.log("ERROR", err)
+    }
+  })
+  response.send(request.body);
+})
+
+
 //pre-load with s/edits
 app.get('/:id', (request, response) => {
   // get json from specified file
@@ -76,42 +144,6 @@ app.get('/:id', (request, response) => {
 });
 
 
-app.get('/', (request, response) => {
-   response.send("yay");
- });
-
-
-app.post('/pokemon', function (request, response)  {
-
-    jsonfile.readFile(FILE, (err,obj) => {
-    if (err) {
-      console.log("ERROR", err)
-    }
-  })
-
-// console.log(request.body)
-// let file = 'pokedex.json'
-const obj = request.body;
-
-
- let nuevoPokemon = {};
-    nuevoPokemon.id = request.body.id;
-    nuevoPokemon.num = request.body.num;
-    nuevoPokemon.name = request.body.name;
-    nuevoPokemon.img = request.body.img;
-    nuevoPokemon.height = request.body.height;
-    nuevoPokemon.weight = request.body.weight;
-
-      obj.pokemon.push(nuevoPokemon);
-
-//saving request.body
-  jsonfile.writeFile(FILE, obj, function(err) {
-    if (err) {
-      console.log("ERROR", err)
-    }
-  })
-  response.send(request.body);
-})
 
 /**
  * ===================================
