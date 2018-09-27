00f03b9e4a5dc260ebf2f4d39637d6a7402aa7fc
JonathanL1211 <Jonathanknight1211@gmail.com>
2018-09-27 09:03:00 +0800

    add: sorting by ID

--- index.js
+++ index.js
@@ -118,7 +118,6 @@ app.get('/', (request, response) => {
   buttonPage += '<option value="id">id</option>';
   buttonPage += '<option value="height">height</option>';
   buttonPage += '<option value="weight">weight</option>';
-  buttonPage += '<option value="weight">weight</option>';
   buttonPage += '</select><br><br><input type="submit"></form>';
   buttonPage += '</body></html>';
 
@@ -135,7 +134,7 @@ app.get('/pokemon', (request, response) => {
             response.status(404).send(err);
         }
         if (request.query.sortby == 'name'){
-            let sortedName = obj.pokemon.sort(compare);
+            let sortedName = obj.pokemon.sort(compareName);
             let nameList = '';
             for (var i = 0; i < sortedName.length; i++){
                 let list = '<li>' + sortedName[i].name + '</li>';
@@ -143,16 +142,55 @@ app.get('/pokemon', (request, response) => {
             }
 
             var pageNameQuery = '';
-            pageNameQuery += '<html><body><h1>List of pokemon names: </h1><ul>' + nameList + '</ul></body></html>';
+            pageNameQuery += '<html><body><h1>List of pokemon sorted by name: </h1><ul>' + nameList + '</ul></body></html>';
 
+            response.send(pageNameQuery);
+        }
+        else if (request.query.sortby == 'id'){
+            let sortedId = obj.pokemon.sort(compareId);
+            let sortByIdList = '';
+            for (var i = 0; i < sortedId.length; i++){
+                let list = '<li>' + sortedId[i].name + '</li>';
+                sortByIdList += list;
+            }
+            var pageNameQuery = '';
+            pageNameQuery += '<html><body><h1>List of pokemon sorted by ID: </h1><ul>' + sortByIdList + '</ul></body></html>';
             response.send(pageNameQuery);
 
         }
+
+        //Weight and height are in strings, maybe need to substring the string, then parseFloat to compare the float value
+        // else if (request.query.sortby == 'height'){
+        //     let sortedHeight = obj.pokemon.sort(compareHeight);
+        //     let sortByHeightList = '';
+        //     for (var i = 0; i < sortedHeight.length; i++){
+        //         let list = '<li>' + sortedHeight[i].name + '</li>';
+        //         sortByHeightList += list;
+        //     }
+        //     var pageNameQuery = '';
+        //     pageNameQuery += '<html><body><h1>List of pokemon sorted by Height: </h1><ul>' + sortByHeightList + '</ul></body></html>';
+        //     response.send(pageNameQuery);
+        // }
+
+        // else if (request.query.sortby == 'weight'){
+        //     let sortedWeight = obj.pokemon.sort(compareWeight);
+        //     let sortByWeightList = '';
+        //     for (var i = 0; i < sortedWeight.length; i++){
+        //         let list = '<li>' + sortedWeight[i].name + '</li>';
+        //         sortByWeightList += list;
+        //     }
+        //     var pageNameQuery = '';
+        //     pageNameQuery += '<html><body><h1>List of pokemon sorted by Height: </h1><ul>' + sortByWeightList + '</ul></body></html>';
+        //     response.send(pageNameQuery);
+
+        // }
+
+
     });
 });
 
 //write a function that sort arrays of object by string property in Javascript
-var compare = function(a,b){
+var compareName = function(a,b){
     if (a.name < b.name){
         return -1;
     }
@@ -161,6 +199,36 @@ var compare = function(a,b){
     }
     return 0;
 }
+
+var compareId = function(a,b){
+    if (a.id < b.id){
+        return -1;
+    }
+    if (a.id > b.id){
+        return +1;
+    }
+    return 0;
+}
+
+// var compareHeight = function(a,b){
+//     if (a.height < b.height){
+//         return -1;
+//     }
+//     if (a.height > b.height){
+//         return +1;
+//     }
+//     return 0;
+// }
+
+// var compareWeight = function(a,b){
+//     if (a.weight < b.weight){
+//         return -1;
+//     }
+//     if (a.weight > b.weight){
+//         return +1;
+//     }
+//     return 0;
+// }
 
 /**
  * ===================================
