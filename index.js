2cf80466d9e5d386e19a76791376157f24b4ef29
JonathanL1211 <Jonathanknight1211@gmail.com>
2018-09-27 18:48:23 +0800

    add & modify: add home.jsx for react template homepage sorting

--- index.js
+++ index.js
@@ -25,110 +25,23 @@ app.use(express.urlencoded({
   extended: true
 }));
 
-var createHtmlPagePokemon = function(request, response){
-    jsonfile.readFile(FILE, (err, obj) =>{
-        var newEmpArray = [];
-        var pokemonObj = obj.pokemon;
-        var html = '<html><body><h1>Form for new pokemon: </h1>';
-        html += '<form method="POST" action="/pokemon">';
-        html += 'Id Number:<br> <input type="text" name="id"><br>';
-        html += 'Number:<br> <input type="text" name="num"><br>';
-        html += 'Name:<br> <input type="text" name="name"><br>';
-        html += 'Image:<br> <input type="text" name="img"><br>';
-        html += 'Height:<br> <input type="text" name="height"><br>';
-        html += 'Weight:<br> <input type="text" name="weight"><br>';
-        html += '<input type="submit" value="Submit"><br>';
-        html += '</form></body></html>';
+// this line below, sets a layout look to your express project
+const reactEngine = require('express-react-views').createEngine();
+app.engine('jsx', reactEngine);
 
-        //console.log(html);
-        response.send(html);
-    })
-}
+// this tells express where to look for the view files
+app.set('views', __dirname + '/views');
 
-app.get('/pokemon/new', createHtmlPagePokemon);
+// this line sets react to be the default view engine
+app.set('view engine', 'jsx');
 
-app.post('/pokemon', (request, response) =>{
-    //console.log(request.body);
-    var requestOrd = request.body;
-    let file = 'pokedex.json';
-    //const obj = request.body;
-    let objFromHtml = {
-        id: parseInt(requestOrd['id']),
-        num: requestOrd['num'],
-        name: requestOrd['name'],
-        img: requestOrd['img'],
-        height: requestOrd['height'],
-        weight: requestOrd['weight']
-    }
-
-    jsonfile.readFile(file, (err,obj)=>{
-        obj['pokemon'].push(objFromHtml);
-
-        jsonfile.writeFile(file, obj, function(err){
-            if (err){
-                console.log("ERROR: ", err);
-            }
-            response.send("Yay added successfully!");
-        });
-
-    })
-
-
+app.get('/', (req, res) => {
+  // running this will let express to run home.handlebars file in your views folder
+  res.render('home')
 })
 
-// app.get('/:id', (request, response) => {
-
-//   // get json from specified file
-//   jsonfile.readFile(FILE, (err, obj) => {
-//     // obj is the object from the pokedex json file
-//     // extract input data from request
-//     let inputId = parseInt( request.params.id );
-
-//     var pokemon;
-
-//     // find pokemon by id from the pokedex json file
-//     for( let i=0; i<obj.pokemon.length; i++ ){
-
-//       let currentPokemon = obj.pokemon[i];
-
-//       if( currentPokemon.id === inputId ){
-//         pokemon = currentPokemon;
-//       }
-//     }
-
-//     if (pokemon === undefined) {
-
-//       // send 404 back
-//       response.status(404);
-//       response.send("not found");
-//     } else {
-
-//       response.send(pokemon);
-//     }
-//   });
-// });
-
-app.get('/', (request, response) => {
-  //create a sort by button
-  let buttonPage = '';
-  buttonPage += '<html><body><h1>How do you want to sort the pokemons:  </h1><br>';
-  //buttonPage += '<input type="button" value="Sort By Name">';
-  buttonPage += '<form action="/pokemon"><select name="sortby">';
-  buttonPage += '<option value="name">name</option>';
-  buttonPage += '<option value="id">id</option>';
-  buttonPage += '<option value="height">height</option>';
-  buttonPage += '<option value="weight">weight</option>';
-  buttonPage += '</select><br><br><input type="submit"></form>';
-  buttonPage += '</body></html>';
-
-
-  response.send(buttonPage);
-  console.log(request.query);
-
-});
-
 app.get('/pokemon', (request, response) => {
-    //console.log(request.query.sortby
+    console.log(request.query);
     jsonfile.readFile(FILE,(err, obj) => {
         if (err){
             response.status(404).send(err);
@@ -188,6 +101,98 @@ app.get('/pokemon', (request, response) => {
 
     });
 });
+
+var createHtmlPagePokemon = function(request, response){
+    jsonfile.readFile(FILE, (err, obj) =>{
+        var newEmpArray = [];
+        var pokemonObj = obj.pokemon;
+        var html = '<html><body><h1>Form for new pokemon: </h1>';
+        html += '<form method="POST" action="/pokemon">';
+        html += 'Id Number:<br> <input type="text" name="id"><br>';
+        html += 'Number:<br> <input type="text" name="num"><br>';
+        html += 'Name:<br> <input type="text" name="name"><br>';
+        html += 'Image:<br> <input type="text" name="img"><br>';
+        html += 'Height:<br> <input type="text" name="height"><br>';
+        html += 'Weight:<br> <input type="text" name="weight"><br>';
+        html += '<input type="submit" value="Submit"><br>';
+        html += '</form></body></html>';
+
+        //console.log(html);
+        response.send(html);
+    })
+}
+
+app.get('/pokemon/new', createHtmlPagePokemon);
+
+app.post('/pokemon', (request, response) =>{
+    console.log(request.body);
+    var requestOrd = request.body;
+    let file = 'pokedex.json';
+    //const obj = request.body;
+    let objFromHtml = {
+        id: parseInt(requestOrd['id']),
+        num: requestOrd['num'],
+        name: requestOrd['name'],
+        img: requestOrd['img'],
+        height: requestOrd['height'],
+        weight: requestOrd['weight']
+    }
+
+    jsonfile.readFile(file, (err,obj)=>{
+        obj['pokemon'].push(objFromHtml);
+
+        jsonfile.writeFile(file, obj, function(err){
+            if (err){
+                console.log("ERROR: ", err);
+            }
+            response.send("Yay added successfully!");
+        });
+
+    })
+
+
+})
+
+
+
+app.get('/:id', (request, response) => {
+
+  // get json from specified file
+  jsonfile.readFile(FILE, (err, obj) => {
+    // obj is the object from the pokedex json file
+    // extract input data from request
+    let inputId = parseInt( request.params.id );
+
+    var pokemon;
+
+    // find pokemon by id from the pokedex json file
+    for( let i=0; i<obj.pokemon.length; i++ ){
+
+      let currentPokemon = obj.pokemon[i];
+
+      if( currentPokemon.id === inputId ){
+        pokemon = currentPokemon;
+      }
+    }
+
+    if (pokemon === undefined) {
+
+      // send 404 back
+      response.status(404);
+      response.send("not found");
+    } else {
+        response.send(pokemon);
+    }
+  });
+});
+
+
+
+
+
+
+
+
 
 //write a function that sort arrays of object by string property in Javascript
 var compareName = function(a,b){
---
+++ views/home.jsx
@@ -0,0 +1,32 @@
+var React = require('react');
+
+class Home extends React.Component {
+    render() {
+        let inputType = 'button';
+        let formAction = '/pokemon';
+        let selectName = 'sortby';
+        let valueName = 'name';
+        let valueId = 'id';
+        let valueHeight = 'height';
+        let valueWeight = 'weight';
+        let submitInput = 'submit';
+
+        return (
+          <div>
+            <h1>Hello. Good morning! How would you like to sort the pokemons: </h1>
+            <form action={formAction}>
+                <select name={selectName}>
+                <option value={valueName}>Name</option>
+                <option value={valueId}>Id</option>
+                <option value={valueHeight}>Height</option>
+                <option value={valueWeight}>Weight</option>
+                </select>
+                <br/>
+                <input type={submitInput}/>
+            </form>
+          </div>
+        );
+    }
+}
+
+module.exports = Home;
