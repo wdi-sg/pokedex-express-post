f39fe54eab56babbc7f8f5d431a5c7ca1e6ce295
damienteo <ksteo89@gmail.com>
2018-12-20 17:14:08 +0800

    rewrote code to meet MVP for day 17

--- index.js
+++ index.js
@@ -11,6 +11,22 @@ const FILE = 'pokedex.json';
 
 // Init express app
 const app = express();
+const methodOverride = require('method-override');
+
+app.use(methodOverride('_method'));
+app.use(express.json());
+app.use(express.urlencoded({
+  extended: true
+}));
+
+// // this line below, sets a layout look to your express project
+// const reactEngine = require('express-react-views').createEngine();
+// app.engine('jsx', reactEngine);
+// // this tells express where to look for the view files
+// app.set('views', __dirname + '/views');
+// // this line sets react to be the default view engine
+// app.set('view engine', 'jsx');
+
 
 /**
  * ===================================
@@ -18,6 +34,27 @@ const app = express();
  * ===================================
  */
 
+ // method: The HTTP method used to send the data: POST or GET.
+// The POST method should always be used if the data is going to result in a change to the server's database, because this can be made more resistant to cross-site forgery request attacks.
+// The GET method should only be used for forms that don't change user data (e.g. a search form). It is recommended for when you want to be able to bookmark or share the URL.
+
+// The default method when submitting form data is GET.
+
+// However, when GET is used, the submitted form data will be visible in the page address field.
+
+// Always use POST if the form data contains sensitive or personal information. The POST method does not display the submitted form data in the page address field.
+
+// Notes on POST:
+
+// POST has no size limitations, and can be used to send large amounts of data.
+// Form submissions with POST cannot be bookmarked
+
+ //if user does not have input
+app.get('/', (request, response) => {
+  response.send("Welcome to the Pokedex!");
+});
+
+//gives basic data for user
 app.get('/:id', (request, response) => {
 
   // get json from specified file
@@ -50,8 +87,65 @@ app.get('/:id', (request, response) => {
   });
 });
 
-app.get('/', (request, response) => {
-  response.send("yay");
+// Expose a new endpoint that intercepts GET requests to `/pokemon/new`, which responds with a HTML page with a `form` that has these fields: `id`, `num`, `name`, `img`, `height`, and `weight`
+
+// * (for the `id` and `num` fields, for now, the user will simply choose a number. Obviously if they happen to pick an id that already exists, they will have a bad time. We will have the tools to correct this soon)
+
+// * Expose a new endpoint that accepts POST requests to `/pokemon`, which parses the form data and saves the new pokemon data into `pokedex.json`
+
+app.get('/pokemon/new', (request, response) => {
+
+// * Point the form to submit data to the (`/pokemon`) route using POST method
+// action: The resource/URL where data is to be sent for processing when the form is submitted. If this is not set (or set to an empty string), then the form will be submitted back to the current page URL.
+
+      let form =`<html>
+                  <body>
+                    <form action="/pokemon" method="post">
+                      <input id="id" type="text" name="id" placeholder="Please enter ID Number.">
+                      <input id="num" type="text" name="num" placeholder="Please enter a number.">
+                      <input id="name" type="text" name="name" placeholder="Please enter a name.">
+                      <input id="img" type="text" name="img" placeholder="What is the img src url?">
+                      <input id="height" type="text" name="height" placeholder="What is the pokemon's height?">
+                      <input id="weight" type="text" name="weight" placeholder="What is the pokemon's weight?">
+                      <input type="submit">
+                    </form>
+                  </body>
+                </html>`
+
+    response.send(form);
+
+});
+
+app.post('/pokemon/', (request, response) => {
+
+  //debug code (output request body)
+  console.log(request.body);
+
+  jsonfile.readFile(FILE, (err, obj) => {
+
+    newPokemon = {
+            "id":  request.body.id,
+            "num": request.body.num,
+            "name": request.body.name,
+            "img": request.body.img,
+            "height": request.body.height,
+            "weight": request.body.weight,
+            "candy": "",
+            "candy_count": "",
+            "egg": "",
+            "avg_spawns": "",
+            "spawn_time": ""
+          };
+
+          obj.pokemon.push(newPokemon);
+
+  // save the request body
+  jsonfile.writeFile(FILE, obj, (err) => {
+    console.error(err)
+    response.send("You have a new Pokemon!");
+    // now look inside your json file
+    });
+  });
 });
 
 /**
