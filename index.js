fd7ffc1ed4948a0b59ee46a3bfa30656c5f81814
rchuaml <ronchuaml@gmail.com>
2018-12-21 13:31:52 +0800

    added new put request

--- index.js
+++ index.js
@@ -3,6 +3,22 @@ const jsonfile = require('jsonfile');
 
 const file = 'pokedex.json';
 
+const app = express();
+
+// this line below, sets a layout look to your express project
+const reactEngine = require('express-react-views').createEngine();
+app.engine('jsx', reactEngine);
+
+// this tells express where to look for the view files
+app.set('views', __dirname + '/views');
+
+// this line sets react to be the default view engine
+app.set('view engine', 'jsx');
+
+// Set up method-override for PUT and DELETE forms
+const methodOverride = require('method-override')
+app.use(methodOverride('_method'));
+
 /**
  * ===================================
  * Configurations and set up
@@ -10,11 +26,11 @@ const file = 'pokedex.json';
  */
 
 // Init express app
-const app = express();
+
 
 app.use(express.json());
 app.use(express.urlencoded({
-  extended: true
+    extended: true
 }));
 /**
  * ===================================
@@ -22,16 +38,17 @@ app.use(express.urlencoded({
 
  * ===================================
  */
-jsonfile.readFile(file, (err, obj) => {
-    let pokeArr = [];
 
-    app.get('/:id', (request, response) => {
+let pokeArr = [];
 
-        // obj is the object from the pokedex json file
-        // extract input data from request
-        let inputId = parseInt(request.params.id);
+app.get('/pokemon/:id', (request, response) => {
 
-        var pokemon;
+    // obj is the object from the pokedex json file
+    // extract input data from request
+    let inputId = parseInt(request.params.id);
+
+    var pokemon;
+    jsonfile.readFile(file, (err, obj) => {
 
         // find pokemon by id from the pokedex json file
         for (let i = 0; i < obj.pokemon.length; i++) {
@@ -53,69 +70,99 @@ jsonfile.readFile(file, (err, obj) => {
             response.send(pokemon);
         }
     });
+});
 
-    app.get('/pokemon/new', (request, response) => {
+app.get('/pokemon/new', (request, response) => {
+    let form = "<html>" +
+        "<body>" +
+        '<form action="/pokemon" method="POST">' +
+        '<input name="id"/>' +
+        '<input name="num"/>' +
+        '<input name="name"/>' +
+        '<input name="img"/>' +
+        '<input name="height"/>' +
+        '<input name="weight"/>' +
+        '<input type="submit"/>' +
+        "</form>" +
+        "</body>" +
+        "</html>";
+    response.send(form);
+})
+
+app.post("/pokemon", (request, response) => {
+    request.body.id = parseInt(request.body.id);
+    console.log(request.body);
+    obj.pokemon.push(request.body);
+    jsonfile.writeFile(file, obj, (err) => {
+        console.log(err);
+    });
+    // store fruit in JSON file
+});
+
+app.get("/pokemon/:id/edit", (request, response) => {
+    jsonfile.readFile(file, (err, obj) => {
+        let form = '<form method="POST" action="/pokemon/' + request.params.id +
+            '?_method=PUT">' +
+            '<div class="pokemon-attribute">' +
+            'id: <input name="id" type="text" value="' + obj.pokemon[request.params.id - 1].id + '"/>' +
+            '<input type="submit"/>' +
+            '</div>' +
+            '</form>';
+        response.send(form);
+    });
+});
+
+app.put("/pokemon/:id", (request, response) => {
+    jsonfile.readFile(file, (err, obj) => {
+        obj.pokemon[request.params.id - 1].id = parseInt(request.body.id);
+        response.send(request.body);
+        jsonfile.writeFile(file, obj, (err) => {
+            console.log(err)
+        });
+    });
+});
+
+//query selector proper way
+app.get('/', (request, response) => {
+    let pokeList = {
+        pokeArr: []
+    };
+
+    jsonfile.readFile(file, (err, obj) => {
+
         let form = "<html>" +
             "<body>" +
-            '<form action="/pokemon" method="POST">' +
-            '<input name="id"/>' +
-            '<input name="num"/>' +
-            '<input name="name"/>' +
-            '<input name="img"/>' +
-            '<input name="height"/>' +
-            '<input name="weight"/>' +
-            '<input type="submit"/>' +
-            "</form>" +
-            "</body>" +
-            "</html>";
-            response.send(form);
-    })
-
-    app.post("/pokemon", (request, response) => {
-        request.body.id = parseInt(request.body.id);
-        console.log(request.body);
-        obj.pokemon.push(request.body);
-        jsonfile.writeFile(file, obj, (err) => {
-            console.log(err);
-        });
-        // store fruit in JSON file
-    });
-
-
-
-    app.get('/', (request, response) => {
-        pokeArr = []
-                let form = "<html>" +
-            "<body>" +
             '<form action="/" method="GET">' +
-            'enter name : <input type="text" value="name" name="sortby"/>' +
-            '<input type="submit"/>' +
+            '<select name="sortby">' +
+            '<option value = "name">' +
+            "Sort pokemon by name" +
+            "</option>" +
+            '<option value = "id">' +
+            "Sort pokemon by id" +
+            "</option>" +
+            '<option value = "weight">' +
+            "Sort pokemon by weight" +
+            "</option>" +
+            "</select>" +
+            "<input type='submit'/>" +
             "</form>" +
             "</body>" +
             "</html>";
-        for(let i = 0 ; i<obj.pokemon.length; i++){
-        pokeArr.push(obj.pokemon[i].name);
-    }
-    if (request.query.sortby === "name"){
-        pokeArr.sort();
-    }
-    var joined = pokeArr.join("<br>");
-    response.send(form + joined);
-
+        for (let i = 0; i < obj.pokemon.length; i++) {
+            pokeList.pokeArr.push(obj.pokemon[i].name);
+        }
+        if (request.query.sortby === "name") {
+            pokeList.pokeArr = pokeList.pokeArr.sort();
+        } else if (request.query.sortby === "id") {
+            pokeList.pokeArr = pokeList.pokeArr;
+        }
     });
+    response.render('home', pokeList);
 
 });
 
-//     app.post("/?sortby=name", (request, response) => {
-//         var arrName =[];
-//         for(let i = 0; i<obj.pokemon.length; i++){
-//             if(name.toLowerCase()===obj.pokemon[i].name.toLowerCase()){
-//                 arrName.push(obj.pokemon[i].name);
-//             }
-//         }
-//         console.log(arrName);
-//     });
- /*
+
+/*
  * ===================================
  * Listen to requests on port 3000
  * ===================================
