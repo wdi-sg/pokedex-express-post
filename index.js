3bcfe4904d195f42ba435d9840368fa8d257a49b
chuasweechin <sampiochua@gmail.com>
2019-04-02 18:16:48 +0800

    complete deliverables and further

--- index.js
+++ index.js
@@ -1,62 +1,147 @@
+//===================================
+// Configurations and set up
+//===================================
+const _ = require('lodash');
+const promise = require("bluebird");
+const jsonfile = promise.promisifyAll(require('jsonfile'));
+
 const express = require('express');
-const jsonfile = require('jsonfile');
-
-const FILE = 'pokedex.json';
-
-/**
- * ===================================
- * Configurations and set up
- * ===================================
- */
-
-// Init express app
 const app = express();
 
-/**
- * ===================================
- * Routes
- * ===================================
- */
+// tell your app to use the module
+app.use(express.json());
+app.use(express.urlencoded({
+  extended: true
+}));
 
-app.get('/:id', (request, response) => {
+let data;
+const file = 'pokedex.json';
 
-  // get json from specified file
-  jsonfile.readFile(FILE, (err, obj) => {
-    // obj is the object from the pokedex json file
-    // extract input data from request
-    let inputId = parseInt( request.params.id );
+//===================================
+// Server And Data Loader Function
+//===================================
+var startServer = function () {
+    // read data before starting up server
+    jsonfile.readFileAsync(file)
+        .then((JSONContent) => {
+            data = JSONContent;
+        })
+        .then(() => {
+            app.listen(3000);
+        });
+}
 
-    var pokemon;
+//===================================
+// Helper Function
+//===================================
+var addZero = function(n) {
+  let str = n.toString()
 
-    // find pokemon by id from the pokedex json file
-    for( let i=0; i<obj.pokemon.length; i++ ){
+  if (str.length === 1) {
+    str = "00" + str;
+  } else if (str.length === 2) {
+    str = "0" + str;
+  }
+  return str;
+}
 
-      let currentPokemon = obj.pokemon[i];
 
-      if( currentPokemon.id === inputId ){
-        pokemon = currentPokemon;
-      }
-    }
+// ===================================
+// Request Handlers
+// ===================================
+var homeRequestHandler = function (request, response) {
+    if (request.query.sortby !== undefined) {
+        let pokemonNames = "";
 
-    if (pokemon === undefined) {
+        if (request.query.sortby === "name") {
+            _.sortBy(data.pokemon, ['name']).forEach((o) => {
+                pokemonNames += `<li>${ o.id } - ${ o.name }</li>`;
+            });
+        } else if (request.query.sortby === "id"){
+            _.sortBy(data.pokemon, ['id']).forEach((o) => {
+                pokemonNames += `<li>${ o.id } - ${ o.name }</li>`;
+            });
+        }
 
-      // send 404 back
-      response.status(404);
-      response.send("not found");
+        response.send(pokemonNames);
+
     } else {
+        let htmlPage = `<form>
+                            <select name="sortby">
+                                <option value="id">id</option>
+                                <option value="name">name</option>
+                            </select>
+                            <input type="submit" value="Sort Pokemon by Name"/>
+                        </form>`;
 
-      response.send(pokemon);
+        response.send(htmlPage);
     }
-  });
-});
+}
 
-app.get('/', (request, response) => {
-  response.send("yay");
-});
+var getPokemonByIdRequestHandler = function (request, response) {
+    let pokemon;
 
-/**
- * ===================================
- * Listen to requests on port 3000
- * ===================================
- */
-app.listen(3000, () => console.log('~~~ Tuning in to the waves of port 3000 ~~~'));
+    _.forEach(data.pokemon, (o) => {
+        if (o.id === Number(request.params.id)) {
+            pokemon = o;
+        }
+    });
+
+    if (pokemon !== undefined) {
+        response.send(pokemon);
+    } else {
+        response.send(404, 'Not found!');
+    }
+}
+
+var newPokemonRequestHandler = function (request, response) {
+    let htmlPage = `<form method="POST" action="/pokemon">
+                        <h1>Add New Pokemon</h1>
+                        Name: <input name="name"/><br>
+                        Image: <input name="img"/><br>
+                        Height: <input name="height"/><br>
+                        Weight: <input name="weight"/><br><br>
+                        <input type="submit" value="Add new Pokemon"/>
+                    </form>`;
+
+    response.send(htmlPage);
+}
+
+var pokemonRequestHandler = function (request, response) {
+    let newPokemon = {};
+
+    newPokemon["id"] = data.pokemon.length + 1;
+    newPokemon["num"] = addZero(data.pokemon.length + 1);
+    newPokemon["name"] = request.body.name;
+    newPokemon["img"] = request.body.img;
+    newPokemon["height"] = request.body.height;
+    newPokemon["weight"] = request.body.weight;
+    newPokemon["candy"] = "None";
+    newPokemon["egg"] = "Not in Eggs";
+    newPokemon["avg_spawns"] = 0;
+    newPokemon["spawn_time"] = "N/A";
+
+    data.pokemon.push(newPokemon);
+
+    jsonfile.writeFileAsync(file, data)
+        .then(() => {
+            response.send("Added new Pokemon!");
+        }).catch((err) => {
+            response.send("Error writing file! Please try again.");
+            console.log(err);
+        });;
+}
+
+// ===================================
+// Routes
+// ===================================
+app.get('/', homeRequestHandler);
+app.get('/:id', getPokemonByIdRequestHandler);
+app.get('/pokemon/new', newPokemonRequestHandler);
+app.post('/pokemon', pokemonRequestHandler);
+
+
+// ===================================
+// Start Server
+// ===================================
+startServer();
