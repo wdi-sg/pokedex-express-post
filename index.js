a536f9332b06a4bff997f6bbddb8fecbcde7a6ff
helloimhd <herda58@gmail.com>
2019-04-03 09:16:55 +0800

    first commit

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+console.log("about to require express");
 const express = require('express');
 const jsonfile = require('jsonfile');
 
@@ -11,12 +12,46 @@ const FILE = 'pokedex.json';
 
 // Init express app
 const app = express();
+console.log("done creating app");
+
+// tell app to use the module
+app.use(express.json());
+
+app.use(express.urlencoded({
+    extended: true
+}));
 
 /**
  * ===================================
  * Routes
  * ===================================
  */
+
+
+app.get('/', (request, response) => {
+  //response.send("Welcome to the online Pokdex!");
+  const nameArray = [];
+
+  jsonfile.readFile(FILE, (err, obj) => {
+    for (i = 0; i < obj["pokemon"].length; i++) {
+        // cannot print here cause can only get one response? so if for loop means it has to keep responding?
+        nameArray.push(obj["pokemon"][i]["name"]);
+    }
+
+    const nameArrayJoin = nameArray.join(", ")
+
+    //response.send(nameArray);
+    response.send('<html><h1>Welcome to the online Pokdex!</h1><body>'+nameArrayJoin+'<br><form method="GET" action="/sortby=name"><input type="submit" value="Sort"></form></body></html>')
+  })   // end of / readfile
+});  // end of / request
+
+    /// sortby using query
+
+app.get("/sortby=name", (request, response) => {
+    nameArray.sort();
+    console.log(nameArray);
+/*      console.log( "request QUERY", request.query);*/
+});
 
 app.get('/:id', (request, response) => {
 
@@ -50,9 +85,42 @@ app.get('/:id', (request, response) => {
   });
 });
 
-app.get('/', (request, response) => {
-  response.send("yay");
-});
+app.get('/pokemon/new', (request, response) => {
+    let respond = '<h1>New Pokemon</h1>' +
+                  '<form method="POST" action="/newPokemon">' +
+                  'ID: <input type="text" name="id"><br><br>' +
+                  'Num: <input type="text" name="num"><br><br>' +
+                  'Name: <input type="text" name="name"><br><br>' +
+                  'Image: <input type="text" name="img"><br><br>' +
+                  'Height: <input type="text" name="height"><br><br>' +
+                  'Weight: <input type="text" name="weight"><br><br>' +
+                  '<input type="submit" value="Submit"><br><br>' +
+                  '</form>';
+
+    response.send(respond)
+})
+
+app.post('/newPokemon', (request, response) => {
+    // receiving the data
+    console.log("this is the request body: ",request.body);
+
+    response.send(request.body)
+    jsonfile.readFile(FILE, (err, obj) => {
+        const pokemonArray = obj["pokemon"];
+
+        pokemonArray.push(request.body);
+
+
+        // save request body
+        jsonfile.writeFile(FILE, obj, (err) => {
+            console.log(err);
+            response.send(obj);
+        });
+
+
+    })
+
+})
 
 /**
  * ===================================
