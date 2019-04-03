b1fd756dcc984d77c7b0fd3a6edebc87ad356b62
Claire Elizabeth Seah <claireseah@Claires-MacBook-Air.local>
2019-04-04 07:08:37 +0800

    installed express-react packages. Added basic syntax for views to run

--- index.js
+++ index.js
@@ -17,6 +17,20 @@ app.use(express.urlencoded({
   extended:true
 }));
 
+const reactEngine = require('express-react-views').createEngine();
+app.engine('jsx', reactEngine);
+
+// this tells express where to look for the view files
+app.set('views', __dirname + '/views');
+
+// this line sets react to be the default view engine
+app.set('view engine', 'jsx');
+
+app.get('/', (req, res) => {
+  // running this will let express to run home.handlebars file in your views folder
+  res.render('home')
+})
+
 /**
  * ===================================
  * Routes
