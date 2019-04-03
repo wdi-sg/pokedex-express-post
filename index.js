9e50df208034ad34066db30244127d148bde3ca2
andrew <andrewtan2903@hotmail.com>
2019-04-03 08:48:47 +0800

    done

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const jsonfile = require('jsonfile');
 
+// Init express app
+const app = express();
 const FILE = 'pokedex.json';
 
 /**
@@ -9,14 +11,76 @@ const FILE = 'pokedex.json';
  * ===================================
  */
 
-// Init express app
-const app = express();
+// Tell your app to use the module
+app.use(express.json());
+app.use(express.urlencoded({
+  extended: true
+}));
 
 /**
  * ===================================
  * Routes
  * ===================================
  */
+
+// Display a list of all pokemon
+app.get('/', (request, response)=>{
+    // response.send("YAY");
+    jsonfile.readFile(FILE, (err, obj)=>{
+        response.send(obj.pokemon);
+    })
+})
+
+// Submit new Pokemon
+app.get('/pokemon/new', (request, response)=>{
+
+
+  let  respond =  '<h1>Submit new Pokemon</h1>'+
+                  '<form method="POST" action="/pokemon">'+
+                  'Id:<input type="number" name="id" min="152" max="200">'+
+                  '<br>'+'<br>'+
+                  'Num:<input type="number" name="num" min="152" max="200">'+
+                  '<br>'+'<br>'+
+                  'Name:<input type="text" name="name">'+
+                  '<br>'+'<br>'+
+                  'Image:<input type="file" name="img" accept="image/*">'+
+                  '<br>'+'<br>'+
+                  'Height:<input type="text" name="height">'+
+                  '<br>'+'<br>'+
+                  'Weight:<input type="text" name="weight">'+
+                  '<br>'+'<br>'+
+                  '<input type="submit" value="Submit">'
+                  '</form>';
+
+  response.send(respond);
+})
+
+app.post('/pokemon', function(request, response) {
+
+  //debug code (output request body)
+  console.log(request.body.id);
+
+// Coverts a string into a number
+request.body.id = parseInt(request.body.id);
+
+// This will read the pokedex.json
+jsonfile.readFile(FILE, (err, obj)=>{
+
+// This will push the submitted information into an array
+    obj.pokemon.push(request.body)
+
+// This will write to the pokedex.json
+    jsonfile.writeFile(FILE, obj, (err) => {
+    console.error(err)
+
+    // now look inside your json file
+    response.send(request.body);
+  });
+})
+
+// save the request body
+
+});
 
 app.get('/:id', (request, response) => {
 
@@ -50,13 +114,13 @@ app.get('/:id', (request, response) => {
   });
 });
 
-app.get('/', (request, response) => {
-  response.send("yay");
-});
+// app.get('/', (request, response) => {
+//   response.send("yay");
+// });
 
 /**
  * ===================================
  * Listen to requests on port 3000
  * ===================================
  */
-app.listen(3000, () => console.log('~~~ Tuning in to the waves of port 3000 ~~~'));
+app.listen(3000,  () => console.log('~~~ Tuning in to the waves of port 3000 ~~~'));
