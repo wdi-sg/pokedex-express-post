68d275e8de613d98139af60996d93202b6cb3012
Claire Elizabeth Seah <claireseah@Claires-MacBook-Air.local>
2019-04-03 19:36:40 +0800

    deliverables completed

--- index.js
+++ index.js
@@ -12,6 +12,11 @@ const FILE = 'pokedex.json';
 // Init express app
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({
+  extended:true
+}));
+
 /**
  * ===================================
  * Routes
@@ -53,6 +58,35 @@ app.get('/:id', (request, response) => {
 app.get('/', (request, response) => {
   response.send("yay");
 });
+
+app.get('/pokemon/new', (request, response) => {
+  jsonfile.readFile(FILE, (err, obj) => {
+    let oldId = obj.pokemon[obj.pokemon.length -1].id;
+    let newId = oldId + 1;
+    let newPokemon = '<h1>New pokemon found!</h1>' + '<form method="POST" action="/pokemon/added">'+
+                          'ID:<input type="text" name="id" value="'+newId+'"><br>'+
+                          'Number:<input type="text" name="num" value="'+newId+'"><br>'+
+                          'Pokemon Name:<input type="text" name="name"><br>'+
+                          'Image:<input type="text" name="img"><br>'+
+                          'Height:<input type="text" name="hight" value="0.00 m"><br>'+
+                          'Weight:<input type="text" name="weight" value="0.0 kg"><br>' +
+                          '<input type="submit" value="Submit">'
+                          '</form>';
+    response.send(newPokemon);
+  });
+});
+
+app.post('/pokemon/added',(request, response) => {
+  jsonfile.readFile(FILE, (err, obj) => {
+    let newId = parseInt(request.body.id);
+    request.body.id = newId;
+    obj.pokemon.push(request.body);
+    jsonfile.writeFile(FILE, obj, (err) => {
+      console.log(err);
+      response.send(request.body);
+    });
+  });
+})
 
 /**
  * ===================================
