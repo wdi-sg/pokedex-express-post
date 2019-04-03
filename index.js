60a27f23d86110a5ce8cabb2645180869ad2e6b9
thoopal <vivekthoopal@gmail.com>
2019-04-03 09:10:46 +0800

    basic

--- index.js
+++ index.js
@@ -12,6 +12,52 @@ const FILE = 'pokedex.json';
 // Init express app
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({
+  extended: true
+}));
+//What I did not do//
+// app.get('/pokemon/new', (request, response)=>{
+
+
+//   let  respond =  '<h1>Submit new Pokemon</h1>'+
+//                   '<form method="POST" action="/pokemon">'+
+//                   'Id:<input type="number" name="id" min="152" max="200">'+
+//                   '<br>'+'<br>'+
+//                   'Num:<input type="number" name="num" min="152" max="200">'+
+//                   '<br>'+'<br>'+
+//                   'Name:<input type="text" name="name">'+
+//                   '<br>'+'<br>'+
+//                   'Image:<input type="file" name="img" accept="image/*">'+
+//                   '<br>'+'<br>'+
+//                   'Height:<input type="text" name="height">'+
+//                   '<br>'+'<br>'+
+//                   'Weight:<input type="text" name="weight">'+
+//                   '<br>'+'<br>'+
+//                   '<input type="submit" value="Submit">'
+//                   '</form>';
+
+//   response.send(respond);
+// })
+
+app.post('/new/pokemon', function(request, response) {
+
+  //debug code (output request body)
+  console.log(request.body);
+
+
+  // save the request body
+  jsonfile.writeFile('data.json', request.body, (err) => {
+    console.error(err)
+
+    // now look inside your json file
+    response.send(request.body);
+  });
+});
+
+
+
+
 /**
  * ===================================
  * Routes
@@ -52,7 +98,7 @@ app.get('/:id', (request, response) => {
 
 app.get('/', (request, response) => {
   response.send("yay");
-});
+// });
 
 /**
  * ===================================
@@ -60,3 +106,4 @@ app.get('/', (request, response) => {
  * ===================================
  */
 app.listen(3000, () => console.log('~~~ Tuning in to the waves of port 3000 ~~~'));
+
