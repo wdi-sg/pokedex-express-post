53233f4f7a30112987f5615eedb553998b7fb5f3
Muhdhatta <muhdhatta.alchemist@gmail.com>
2019-04-04 06:24:59 +0800

    hatta ver 2

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
+const pokemon = require('./pokedex.js')
 const express = require('express');
+const app = express();
 const jsonfile = require('jsonfile');
 
-const FILE = 'pokedex.json';
+// const FILE = 'pokedex.json';
 
 /**
  * ===================================
@@ -9,14 +11,24 @@ const FILE = 'pokedex.json';
  * ===================================
  */
 
+// this line below, sets a layout look to your express project
+const reactEngine = require('express-react-views').createEngine();
+app.engine('jsx', reactEngine);
 
-const app = express();
+// this tells express where to look for the view files
+app.set('views', __dirname + '/views');
+
+// this line sets handlebars to be the default view engine
+app.set('view engine', 'jsx');
+
 
 // tell your app to use the module
-app.use(express.json());
-app.use(express.urlencoded({
-  extended: true
-}));
+// app.use(express.json());
+// app.use(express.urlencoded({
+//   extended: true
+// }));
+
+
 
 app.get('/pokemon/new', function(request, response) {
     let  newForm =`<h1>Input new pokemon</h1>
@@ -81,62 +93,73 @@ app.get('/pokemon/new', function(request, response) {
 
 
 
-
-app.get('/:id', (request, response) => {
-
-  // get json from specified file
-  jsonfile.readFile(FILE, (err, obj) => {
-    // obj is the object from the pokedex json file
-    // extract input data from request
-    let inputId = parseInt( request.params.id );
-
-    var pokemon;
-
-    // find pokemon by id from the pokedex json file
-    for( let i=0; i<obj.pokemon.length; i++ ){
-
-      let currentPokemon = obj.pokemon[i];
-
-      if( currentPokemon.id === inputId ){
-        pokemon = currentPokemon;
-      }
+app.get('/pokemon/:id', (req,res) => {
+    console.log(req.params);
+    let pokemonId = parseInt( req.params.id );
+    let pokemon;
+    for (let pokemonBlock of pokemon) {
+        if (pokemonBlock.id === pokemonId) {
+            pokemon = pokemonBlock
+        }
     }
-
-    if (pokemon === undefined) {
-
-      // send 404 back
-      response.status(404);
-      response.send("not found");
-    } else {
-
-      response.send(pokemon);
-    }
-  });
+    // let user = users[userId-1];
+    console.log(pokemon);
+    res.render('SpecificPokemon', {name: pokemon.name, weight: pokemon.weight});
 });
 
-// app.post('/pokemon/new', function(request, response) {
+app.get('/pokemon', (req,res) => {
+    let array = {"data": pokemon}
+    res.render('pokemon', array);
+});
 
-//   //debug code (output request body)
-//   console.log(request.body);
-//     jsonfile.readFile('data.json', (err, obj) => {
-//         obj.pokemon.push(request.body);
+app.get('/firstpokemon', (req,res) => {
 
-//   // save the request body
-//   jsonfile.writeFile('data.json', obj, (err) => {
-//     console.error(err)
+    let firstPokemon = Pokemon[0];
+    console.log(firstPokemon);
+    res.render('SpecificPokemon', {name: firstPokemon.name, Weight: firstPokemon.Weight});
+});
 
-//     // now look inside your json file
-//     response.send(request.body);
+// app.get('pokemon/:id', (request, response) => {
+
+//   // get json from specified file
+//   jsonfile.readFile(FILE, (err, obj) => {
+//     // obj is the object from the pokedex json file
+//     // extract input data from request
+//     let inputId = parseInt( request.params.id );
+
+//     var pokemon;
+
+//     // find pokemon by id from the pokedex json file
+//     for( let i=0; i<obj.pokemon.length; i++ ){
+
+//       let currentPokemon = obj.pokemon[i];
+
+//       if( currentPokemon.id === inputId ){
+//         pokemon = currentPokemon;
+//       }
+//     }
+
+//     if (pokemon === undefined) {
+
+//       // send 404 back
+//       response.status(404);
+//       response.send("not found");
+//     } else {
+
+//       response.send(pokemon);
+//     }
 //   });
 // });
 
-app.get('/', (request, response) => {
-  response.send("yay");
-});
+// app.get('/pokemon/:id/edit', )
+
+// app.get('/', (request, response) => {
+//   response.send("yay");
+// });
 
 /**
  * ===================================
  * Listen to requests on port 3000
  * ===================================
  */
-app.listen(3000, () => console.log('~~~ Tuning in to the waves of port 3000 ~~~'));
+app.listen(3000);
