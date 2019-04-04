55512963e2b90456228a0a5254d682c32ee83cfc
riv23 <jleezhenhau@gmail.com>
2019-04-04 12:14:28 +0800

    jordan

--- index.js
+++ index.js
@@ -54,6 +54,38 @@ app.get('/', (request, response) => {
   response.send("yay");
 });
 
+app.get('/pokemon/new', (request, response)=>{
+    let respond =   '<form method="POST" action="/pokemon">'+
+    'pokemon:<input type="text" name="name" placeholder="Pokemon name">'+
+    '<input type="text" name="height" placeholder="Height">'+
+    '<input type="text" name="weight" placeholder="Weight">'+
+    '<input type="text" name="id" placeholder="id">'+
+    '<input type="text" name="num" placeholder="Num">'+
+    '<input type="text" name="img" placeholder="Img">'+
+    '<input type="submit" value="Submit">'+
+    '</form>';
+    response.send(respond);
+});
+
+
+ app.post('/pokemon', function(request, response) {
+
+  // we are recieving data
+  console.log( "this is request body:",request.body );
+  let newPokemon = request.body;
+  newPokemon.id = parseInt(newPokemon.id, 10)
+
+    jsonfile.readFile(FILE, (err, obj) => {
+        // create new list item
+        obj.pokemon.push(newPokemon);
+
+        jsonfile.writeFile(FILE, obj, (err) => {
+          if (err) { console.log(err) };
+        });
+      });
+  response.send(newPokemon);
+});
+
 /**
  * ===================================
  * Listen to requests on port 3000
