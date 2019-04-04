b647694f025e7bacb573a27a6a68a0877c83b5ae
seeo <aragonlotr5@gmail.com>
2019-04-04 09:08:55 +0800

    managed to get the values to pre-populate in the fill boxes

--- views/thisPokemon.jsx
+++ views/thisPokemon.jsx
@@ -4,37 +4,42 @@ class thisPokemon extends React.Component {
 
   render() {
 
-    const thisPokemonObj = this.props
+    const thisPokemonObj = this.props.respondKey
     const actionAttribute = `/pokemon/${this.props.idKey}/?_method=PUT`  //according to Akira.
     console.log("Printing out the selected pokemon stats as an oject with key-value pairs")
     console.log(thisPokemonObj);
     console.log("Printing out stuff that is going to render");
+    console.log("Printing out this.props.respondKey below");
+    console.log(this.props.respondKey);
+
+    console.log("Printing out this pokemon's id below:");
+    console.log(this.props.respondKey.id);
 
     return (
       // <h1>EDIT: This is Pokemon # {thisPokemonObj.num}</h1>
       <form method = "POST" action ={actionAttribute}>
         Pokemon ID (1 to 151):<br/>
-        <input type = "number" name = "pokemon_id" value = {this.props.id}/><br/>
+        <input type = "number" name = "pokemon_id" value = {this.props.respondKey.id}/><br/>
         Pokemon Number (001 to 151):<br/>
-        <input type = "text" name = "pokemon_num" value = {this.props.num}/><br/>
+        <input type = "text" name = "pokemon_num" value = {this.props.respondKey.num}/><br/>
         Pokemon Name: <br/>
-        <input type = "text" name = "pokemon_name" value = {this.props.name}/><br/>
-        Pokemon Image <br/>
-        <input type = "image" name = "pokemon_img" value = {this.props.img}/><br/>
-        Pokemon Height <br/>
-        <input type = "text" name = "pokemon_height" value = {this.props.height}/><br/>
-        Pokemon Weight <br/>
-        <input type = "text" name = "pokemon_weight" value = {this.props.weight}/><br/>
-        Pokemon Candy <br/>
-        <input type = "text" name = "pokemon_candy" value = {this.props.candy}/><br/>
+        <input type = "text" name = "pokemon_name" value = {this.props.respondKey.name}/><br/>
+        Pokemon Image: <br/>
+        <input type = "image" name = "pokemon_img" value = {this.props.respondKey.img}/><br/>
+        Pokemon Height: <br/>
+        <input type = "text" name = "pokemon_height" value = {this.props.respondKey.height}/><br/>
+        Pokemon Weight: <br/>
+        <input type = "text" name = "pokemon_weight" value = {this.props.respondKey.weight}/><br/>
+        Pokemon Candy: <br/>
+        <input type = "text" name = "pokemon_candy" value = {this.props.respondKey.candy}/><br/>
         Pokemon Candy Count: <br/>
-        <input type = "text" name = "pokemon_candy_count" value = {this.props.candy_count}/><br/>
+        <input type = "text" name = "pokemon_candy_count" value = {this.props.respondKey.candy_count}/><br/>
         Pokemon Egg: <br/>
-        <input type = "text" name = "pokemon_egg" value = {this.props.egg}/><br/>
+        <input type = "text" name = "pokemon_egg" value = {this.props.respondKey.egg}/><br/>
         Pokemon Average Spawn Count: <br/>
-        <input type = "text" name = "pokemon_avg_spawn" value = {this.props.avg_spawns}/><br/>
+        <input type = "text" name = "pokemon_avg_spawn" value = {this.props.respondKey.avg_spawns}/><br/>
         Pokemon Average Spawn Time: <br/>
-        <input type = "text" name = "pokemon_spawn_time" value = {this.props.spawn_time}/><br/>
+        <input type = "text" name = "pokemon_spawn_time" value = {this.props.respondKey.spawn_time}/><br/>
         <br/>
         <input type = "submit" value = "Submit"/>
       </form>
