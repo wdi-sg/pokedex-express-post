98b00ed3bb2d3a4fd43b83d44cd1d72dde552409
Claire Elizabeth Seah <claireseah@Claires-MacBook-Air.local>
2019-04-04 18:03:53 +0800

    completed get and post request for deliverables

--- index.js
+++ index.js
@@ -11,12 +11,64 @@ const FILE = 'pokedex.json';
 
 // Init express app
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded ({
+  extended: true
+}));
 
 /**
  * ===================================
  * Routes
  * ===================================
  */
+
+//Deliverable for part 1 pokedex-express-post. 
+//Make a GET request and a corresponding POST request.
+app.get('/pokemon/new', (request, response) => {
+    let newPokeForm = '<h1>New Pokemon</h1>'+
+                            '<form method="POST" action="/pokemon">'+
+                            'Choose your Pokemon:' + '<br>' +
+                            'Pokemon ID:     <input type="text" name="id">'+ '<br>' +
+                            'Pokemon Number: <input type="text" name="num">'+ '<br>' +
+                            'Pokemon Name:   <input type="text" name="name">'+ '<br>' +
+                            'How does it look like? (put an img link dude): <input type="text" name="img">'+ '<br>' +
+                            'Height: <input type="text" name="height">'+ '<br>' +
+                            'Weight: <input type="text" name="weight">'+ '<br>' +
+                            '<input type="submit" value="Submit">'+ '<br>' +
+                            '</form>';
+      response.send(newPokeForm);
+  });
+
+app.post('/pokemon', (request, response) => {
+  console.log(request.body);
+
+  let submitForm = '<div> <h1>Fake PokeDex Display</h1>'+
+                    '<div>' + request.body.id + '</div>' +
+                    '<div>' + request.body.num + '</div>' +
+                    '<div>' + request.body.name + '</div>' +
+                    '<div>' + request.body.height + '</div>' +
+                    '<div>' + request.body.weight + '</div></div>';
+  response.send(submitForm);
+
+  jsonfile.readFile(FILE, (err, obj) => {
+    let newPokemon = [];
+
+    newPokemon.id = request.body.id;
+    newPokemon.num = request.body.num;
+    newPokemon.name = request.body.name;
+    newPokemon.img = request.body.img;
+    newPokemon.height = request.body.height;
+    newPokemon.weight = request.body.weight;
+
+        obj.pokemon.push(newPokemon);
+
+    jsonfile.writeFile(FILE, err, (obj) =>{
+      if (err !== null) {
+        console.log(err);
+      }
+    });
+  });
+});
 
 app.get('/:id', (request, response) => {
 
