453f2bec20836a614a20575422c29b64d59334bb
Claire Elizabeth Seah <claireseah@Claires-MacBook-Air.local>
2019-04-04 18:52:09 +0800

    deliverable for part 2 attempted. GET works, PUT.. I'm not sure...

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const express = require('express');
 const jsonfile = require('jsonfile');
 
 const FILE = 'pokedex.json';
+const app = express();
+const reactEngine = require('express-react-views').createEngine();
+const methodOverride = require('method-override');
 
 /**
  * ===================================
@@ -10,12 +13,26 @@ const FILE = 'pokedex.json';
  */
 
 // Init express app
-const app = express();
 app.use(express.json());
 app.use(express.urlencoded ({
   extended: true
 }));
 
+
+app.engine('jsx', reactEngine);
+
+// this tells express where to look for the view files
+app.set('views', __dirname + '/views');
+
+// this line sets react to be the default view engine
+app.set('view engine', 'jsx');
+
+app.get('/', (req, res) => {
+  // running this will let express to run home.handlebars file in your views folder
+  res.render('home')
+})
+
+app.use(methodOverride('_method'));
 /**
  * ===================================
  * Routes
@@ -39,6 +56,7 @@ app.get('/pokemon/new', (request, response) => {
       response.send(newPokeForm);
   });
 
+//POST request to change the info submitted by the user
 app.post('/pokemon', (request, response) => {
   console.log(request.body);
 
@@ -69,6 +87,39 @@ app.post('/pokemon', (request, response) => {
     });
   });
 });
+
+app.get('/pokemon/:id/edit', (req, res) =>{
+  let arrayIndex = parseInt(req.params.id);
+    jsonfile.readFile(FILE, (err, obj)=>{
+
+    let pokemonObjList = obj.pokemon[arrayIndex-1];
+
+    res.render('home', pokemonObjList);
+    });
+})
+
+app.put('/pokemon/:id', (req, res) =>{
+  let arrayIndex = parseInt(req.params.id);
+    jsonfile.readFile(FILE, (err, obj)=>{
+    let pokemonObjList = obj.pokemon[arrayIndex-1];
+
+      pokemonObjList.name = req.body.pokeName;
+      pokemonObjList.num = req.body.pokeName;
+      pokemonObjList.img = req.body.pokeImg;
+      pokemonObjList.weight = req.body.pokeWeight;
+      pokemonObjList.height = req.body.pokeHeight;
+      pokemonObjList.egg = req.body.pokeEgg;
+
+    jsonfile.writeFile(FILE, obj, (err) => {
+      console.log(err)
+      res.send(req.body);
+    });
+  });
+})
+
+
+  
+
 
 app.get('/:id', (request, response) => {
 
---
+++ views/home.jsx
@@ -0,0 +1,32 @@
+var React = require('react');
+
+class Home extends React.Component {
+  render() {
+    const id = this.props.id;
+    const name = this.props.name;
+    const num = this.props.num;
+    const weight = this.props.weight;
+    const height = this.props.height;
+    const egg = this.props.egg;
+    const img = this.props.img;
+
+    let formAction = '/pokemon/' + id + '?_method=PUT';
+
+    return (
+      <div>
+      <h1>Edit Pokemon Details</h1>
+      <form method="POST" action={formAction} >
+      Name: <input type= "text" name="pokeName" value={name} /><br></br>
+      Num: <input type= "text" name="pokeNum" value={num} /><br></br>
+      Img: <input type= "text" name="pokeImg" value={img} /><br></br>
+      Weight: <input type= "text" name="pokeWeight" value={weight} /><br></br>
+      Height: <input type= "text" name="pokeHeight" value={height} /><br></br>
+      Egg Distance: <input type= "text" name="pokeEgg" value={egg} /><br></br>
+      <input type="submit" value="Save Changes" />
+      </form>
+      </div>
+    );
+  }
+}
+
+module.exports = Home;
