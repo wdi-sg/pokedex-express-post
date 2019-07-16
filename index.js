edd8c1446a88802571007c9440c48c3333f0c93d
wilfredloh <lohwilfred19@gmail.com>
2019-07-16 19:38:34 +0800

    created form to get user data, added sort features + id in sequence

--- index.js
+++ index.js
@@ -12,20 +12,63 @@ const FILE = 'pokedex.json';
 // Init express app
 const app = express();
 
+app.use(express.json());
+
+app.use(express.urlencoded({
+  extended: true
+}));
+
 /**
  * ===================================
- * Routes
+ * FUNCTIONS
  * ===================================
  */
 
-app.get('/:id', (request, response) => {
+let createPokemon = (request, response) => {
+    let form = '';
+    form = `
+    <form method="POST" action="/pokemon">
+      Pokemon Name:
+      <input type="text" name="id" placeholder="id">
+      <input type="text" name="num" placeholder="num">
+      <input type="text" name="name" placeholder="name">
+      <input type="text" name="img" placeholder="img">
+      <input type="text" name="height" placeholder="height">
+      <input type="text" name="weight" placeholder="weight">
+      <input type="submit" value="Submit">
+    </form>
+    `
+    response.send(form);
+}
 
-  // get json from specified file
+let showCreatedPokemon = (request, response) => {
+    let newPoke = request.body;
+    console.log('newPoke: ' +newPoke);
+
+    jsonfile.readFile(FILE, (err,obj) => {
+        // let currentNum = obj.lastKey;
+        console.log('obj lastkey: ' + obj.lastKey)
+        newPoke.num = obj.lastKey + 1;
+        obj.lastKey++;
+
+        obj.pokemon.push(newPoke);
+
+        jsonfile.writeFile(FILE, obj, (err) => {
+            if (err) {
+                console.log('error in writing!');
+            } else {
+                response.send(`You have created a new Pokemon ${newPoke.name}!`);
+            }
+        })
+    });
+}
+
+let showPokemonDetailsUsingInput = (request, response) => {
+    // get json from specified file
   jsonfile.readFile(FILE, (err, obj) => {
     // obj is the object from the pokedex json file
     // extract input data from request
     let inputId = parseInt( request.params.id );
-
     var pokemon;
 
     // find pokemon by id from the pokedex json file
@@ -39,24 +82,126 @@ app.get('/:id', (request, response) => {
     }
 
     if (pokemon === undefined) {
-
       // send 404 back
       response.status(404);
       response.send("not found");
     } else {
-
       response.send(pokemon);
     }
   });
-});
+}
 
-app.get('/', (request, response) => {
-  response.send("yay");
-});
+let showPokemonDetailsUsingLink = (request, response) => {
+    jsonfile.readFile(FILE, (err, obj) => {
+        for(let i=0; i<obj.pokemon.length; i++) {
+            if ( 7 === obj.pokemon[i].id){
+                response.send(
+                    `${obj.pokemon[i].name}<br>
+                    <img src="${obj.pokemon[i].img}"><br>Height: ${obj.pokemon[i].height}<br>Weight: ${obj.pokemon[i].weight}`)
+            }
+        }
+    });
+}
+
+let sortPokemon = (request, response) => {
+
+    let query = request.query.sortby;
+
+    jsonfile.readFile(FILE, (err,obj) => {
+
+        let content = '';
+        let form = '';
+        form = `
+        <form method="GET">
+            <select name="sortby">
+                <option value="name">Name</option>
+                <option value="weight">Weight</option>
+                <option value="height">Height</option>
+                <option value="id">Id</option>
+            </select>
+            <input type="submit" value="Submit">
+        </form>`;
+
+        content = form;
+
+        if (query === 'name') {
+            obj.pokemon.sort(compareName);
+        } else if (query === 'weight') {
+            obj.pokemon.sort(compareWeight);
+        } else if (query === 'height') {
+            obj.pokemon.sort(compareHeight);
+        }
+
+        for(let i=0; i<obj.pokemon.length; i++) {
+            let eachPoke = obj.pokemon[i].name;
+            content +=
+            `<a href='./pokemon/${obj.pokemon[i].id}'>${obj.pokemon[i].name}</a><br>`;
+        }
+        response.send(content);
+    });
+}
+
+let compareName = (a, b) => {
+  // Use toUpperCase() to ignore character casing
+  const nameA = a.name.toUpperCase();
+  const nameB = b.name.toUpperCase();
+
+  let comparison = 0;
+  if (nameA > nameB) {
+    comparison = 1;
+  } else if (nameA < nameB) {
+    comparison = -1;
+  }
+  return comparison;
+}
+
+let compareWeight = (a, b) => {
+  // Use toUpperCase() to ignore character casing
+  const weightA = parseFloat(a.weight);
+  const weightB = parseFloat(b.weight);
+
+  let comparison = 0;
+  if (weightA > weightB) {
+    comparison = 1;
+  } else if (weightA < weightB) {
+    comparison = -1;
+  }
+  return comparison;
+}
+
+let compareHeight = (a, b) => {
+  // Use toUpperCase() to ignore character casing
+  const heightA = parseFloat(a.height);
+  const heightB = parseFloat(b.height);
+
+  let comparison = 0;
+  if (heightA > heightB) {
+    comparison = 1;
+  } else if (heightA < heightB) {
+    comparison = -1;
+  }
+  return comparison;
+}
 
 /**
  * ===================================
- * Listen to requests on port 3000
+ * ROUTING
  * ===================================
  */
-app.listen(3000, () => console.log('~~~ Tuning in to the waves of port 3000 ~~~'));
+app.get('/:id', showPokemonDetailsUsingInput);
+app.get('/pokemon/:id', showPokemonDetailsUsingLink);
+
+app.get('/', sortPokemon);
+
+app.get('/pokemon/new', createPokemon);
+app.post('/pokemon', showCreatedPokemon);
+
+
+/**
+ * ===================================
+ * Listen to requests on port
+ * ===================================
+ */
+
+let port = 1500;
+app.listen(port, () => console.log(`~~~ Tuning in to the waves of port ${port} ~~~`));
