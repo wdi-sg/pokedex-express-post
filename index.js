f253ba0370b100fa9726056038b30904a9d5c7c5
MalcolmLow83 <malcomlow@gmail.com>
2019-07-16 16:07:44 +0800

    Completion of display all pokemons

--- index.js
+++ index.js
@@ -12,11 +12,74 @@ const FILE = 'pokedex.json';
 // Init express app
 const app = express();
 
+// tell your app to use the module
+app.use(express.json());
+app.use(express.urlencoded({
+  extended: true
+}));
+
+
 /**
  * ===================================
  * Routes
  * ===================================
  */
+
+app.get("/pokemon/new", (request, response) => {
+    console.log("Getting forms");
+
+    let form ='';
+    form =  `<html>
+                <body>
+                    <h1>Submit new pokemon form</h1>
+                        <form method = "POST" action ="/pokemon">
+                            <p>id</p>
+                            <input name = "id">
+                            <p>num</p>
+                            <input name = "num">
+                            <p>name</p>
+                            <input name = "name">
+                            <p>img</p>
+                            <input name = "img">
+                            <p>height</p>
+                            <input name = "height">
+                            <p>weight</p>
+                            <input name = "weight">
+                            <input type = "Submit">
+                        </form>
+                </body>
+            </html>`;
+        response.send(form);
+});
+
+app.post('/pokemon', function(request, response) {
+
+    console.log("passing values to newPokemon");
+    var newPokemon = request.body;
+    console.log(newPokemon);
+
+    jsonfile.readFile(FILE, (err, obj) => {
+        if (err) {
+            console.log(err);
+        }
+        else {
+            obj.pokemon.push(newPokemon);
+
+            jsonfile.writeFile(FILE, obj, (err) => {
+                if (err) {
+                    console.log(err);
+                }
+                else {
+                    console.log("push complete");
+                }
+            })
+        }
+    })
+
+});
+
+
+
 
 app.get('/:id', (request, response) => {
 
@@ -51,7 +114,21 @@ app.get('/:id', (request, response) => {
 });
 
 app.get('/', (request, response) => {
-  response.send("yay");
+  var showAllPokemon = [];
+
+  jsonfile.readFile(FILE, (err, obj) => {
+    if (err) {
+        console.log(err);
+    }
+    else {
+        for (let i = 0; i < obj.pokemon.length; i++) {
+            showAllPokemon.push(obj.pokemon[i].name + "\n");
+        }
+
+    }
+    response.send(showAllPokemon.toString());
+  })
+
 });
 
 /**
