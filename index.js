e84429790596142dc5bf1a88bc08807e03a810af
marcus <marcusteo@hotmail.com>
2019-07-16 21:31:47 +0800

    functioning pokedex which includes all sorting up till height

--- index.js
+++ index.js
@@ -1,62 +1,149 @@
 const express = require('express');
 const jsonfile = require('jsonfile');
-
-const FILE = 'pokedex.json';
-
-/**
- * ===================================
- * Configurations and set up
- * ===================================
- */
-
-// Init express app
 const app = express();
+const file = "pokedex.json"
 
-/**
- * ===================================
- * Routes
- * ===================================
- */
+app.use(express.json());
+app.use(express.urlencoded({
+  extended: true
+}));
 
-app.get('/:id', (request, response) => {
-
-  // get json from specified file
-  jsonfile.readFile(FILE, (err, obj) => {
-    // obj is the object from the pokedex json file
-    // extract input data from request
-    let inputId = parseInt( request.params.id );
-
-    var pokemon;
-
-    // find pokemon by id from the pokedex json file
-    for( let i=0; i<obj.pokemon.length; i++ ){
-
-      let currentPokemon = obj.pokemon[i];
-
-      if( currentPokemon.id === inputId ){
-        pokemon = currentPokemon;
+//search for pokemon
+app.get('/pokemon/search', (request, response) => {
+  //allows case-insensitive entry
+  var smallLetter = request.query.q.toLowerCase();
+  var capsFirst = smallLetter.charAt(0).toUpperCase();
+  var combined = capsFirst+smallLetter.slice(1);
+  //flags if search for pokemon name turns up negative
+  var search = false;
+  jsonfile.readFile(file, function(err,obj){
+    for (var i = 0; i < obj["pokemon"].length; i++){
+      if (obj["pokemon"][i]["name"] === combined){
+        response.write(`
+          Name: ${obj["pokemon"][i]["name"]} <br>
+          Height: ${obj["pokemon"][i]["height"]} <br>
+          Weight: ${obj["pokemon"][i]["weight"]} <br>
+        `);
+        response.end();
+        search = true;
       }
     }
-
-    if (pokemon === undefined) {
-
-      // send 404 back
-      response.status(404);
-      response.send("not found");
-    } else {
-
-      response.send(pokemon);
+    if (combined === ""){
+      response.send(`Welcome to the online Pokedex!`)
+      search = true;
+    }
+    if (!search){
+      response.status(404).send(
+        `Could not find information about ${request.query.q} - Is that a new pokemon? Gotta catch em' all!`
+      );
     }
   });
+})
+//allows user to enter in new pokemon
+app.get('/pokemon/new',(request,response)=>{
+  response.send(`
+    <form method="POST" action="/pokemon">
+      ID:
+      <input type="text" name="id" placeholder="1"><br>
+      Num:
+      <input type="text" name="num" placeholder="001"><br>
+      Name:
+      <input type="text" name="name" placeholder="Bulbasaur"><br>
+      Image:
+      <input type="text" name="image" placeholder="image url goes here"><br>
+      Height:
+      <input type="text" name="height" placeholder="height in m"><br>
+      Weight:
+      <input type="text" name="weight" placeholder="weight in kg"><br>
+      <br><br><br>
+      <input type="submit" value="Submit">
+    </form>
+`);
 });
 
-app.get('/', (request, response) => {
-  response.send("yay");
+app.post('/pokemon', function(request, response) {
+  jsonfile.readFile(file, function(err,obj){
+    console.log(request.body);
+    obj["pokemon"].push(request.body);
+    jsonfile.writeFile(file,obj,(err) => {
+      if(err){
+        console.error(err)
+      };
+      response.send("New pokemon added!");
+    });
+  })
 });
 
-/**
- * ===================================
- * Listen to requests on port 3000
- * ===================================
- */
+app.get('/', function(request, response){
+  var list = [];
+  var listHolder = "";
+  var form = `
+    <form method="GET" action="/">
+    <select name ="sortby" action="/" onchange="this.form.submit()">
+      <option disabled selected value>Select an option</option>
+      <option value="weight">Sort by weight</option>
+      <option value="height">Sort by height</option>
+      <option value="name">Sort by name</option>
+    </select>
+    </form>`
+  if (request.query.sortby === "name"){
+
+
+    jsonfile.readFile(file, function(err,obj){
+      for (var i = 0; i < obj["pokemon"].length; i++){
+        list.push(obj["pokemon"][i]["name"]);
+      }
+      list.sort();
+      for (var j = 0; j < list.length; j++){
+        listHolder += `<li>${list[j]}</li>`
+      }
+      response.send(`<h1>List of pokemon sorted by name:</h1>${form}<br><ul>${listHolder}</ul>`)
+    });
+  }else if (request.query.sortby === "weight"){
+    jsonfile.readFile(file, function(err,obj){
+    function compareValues(key){
+      return function (a,b){
+        if(!a.hasOwnProperty(key) || !b.hasOwnProperty(key)) {
+        return 0;
+        }
+        let varA = parseFloat(a[key])
+        let varB = parseFloat(b[key])
+        return varA-varB;
+      }
+    }
+      list = obj["pokemon"].sort(compareValues("weight"));
+      for (var i = 0; i < list.length; i++){
+        listHolder += `<li>${list[i]["name"]} - ${list[i]["weight"]}</li>`
+      }
+      response.send(`<h1>List of pokemon sorted by weight:</h1>${form}<br><ul>${listHolder}</ul>`)
+    });
+  }else if (request.query.sortby === "height"){
+    jsonfile.readFile(file, function(err,obj){
+    function compareValues(key){
+      return function (a,b){
+        if(!a.hasOwnProperty(key) || !b.hasOwnProperty(key)) {
+        return 0;
+        }
+        let varA = parseFloat(a[key])
+        let varB = parseFloat(b[key])
+        return varA-varB;
+      }
+    }
+      list = obj["pokemon"].sort(compareValues("height"));
+      for (var i = 0; i < list.length; i++){
+        listHolder += `<li>${list[i]["name"]} - ${list[i]["height"]}</li>`
+      }
+      response.send(`<h1>List of pokemon sorted by height:</h1>${form}<br><ul>${listHolder}</ul>`)
+    });
+  }else {
+    var list = "";
+    jsonfile.readFile(file, function(err,obj){
+      for (var i = 0; i < obj["pokemon"].length; i++){
+        list += `<li>${obj["pokemon"][i]["name"]}</li>`;
+      }
+      response.send(`<h1>List of pokemon:</h1>${form}<br><ul>${list}</ul>`)
+    });
+  }
+});
+
 app.listen(3000, () => console.log('~~~ Tuning in to the waves of port 3000 ~~~'));
