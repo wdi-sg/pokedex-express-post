7ce0ce2bf85c5154c17846235df9bceaccdcec19
EliseChia <swedesurrender@gmail.com>
2019-07-16 15:20:47 +0800

    basic requirements done - can search for poke

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const express = require('express');
 const jsonfile = require('jsonfile');
 const file = 'pokedex.json';
 
-
 const app = express();
 
 //parser thing
@@ -11,41 +10,7 @@ app.use(express.urlencoded({
   extended: true
 }));
 
-//find pokemon info by name
-app.get('/pokemon/:name', (request, response) => {
-
-    jsonfile.readFile(file, (err, obj) => {
-
-        let pokeName = "";
-        let pokeWeight = "";
-        let pokemonFound = false;
-        let lowerCase = request.params.name.toLowerCase();
-        let capsFirst = lowerCase.charAt(0).toUpperCase();
-        let pokeToFind = capsFirst + lowerCase.slice(1);
-
-        console.log(pokeToFind);
-
-        //find pokeToFind in obj and return its name and weight
-        for (let i = 0; i < obj.pokemon.length; i++) {
-            if (obj.pokemon[i].name === pokeToFind) {
-                pokeName = obj.pokemon[i].name;
-                pokeWeight = obj.pokemon[i].weight;
-                pokemonFound = true;
-
-                console.log(obj.pokemon[i].name);
-                console.log(obj.pokemon[i].weight);
-            }
-        }
-
-        if (!pokemonFound) {
-            response.status(404).send(`<html><body><h1>Pikaboo:(</h1><br><p>Could not find information on ${pokeToFind} - Is that a new pokemon? Gotta catch em all!</p></body></html>`);
-        } else if (pokemonFound) {
-            response.send(`<html><body><h1>${pokeName}</h1><br><p>Weight: ${pokeWeight}</p></body></html>`);
-        }
-    });
-});
-
-//part 2 woo - creating form on /pokemon/new
+//creating form on /pokemon/new [must be on top]
 app.get('/pokemon/new', (request, response) => {
 
   console.log("birthing form");
@@ -57,14 +22,21 @@ app.get('/pokemon/new', (request, response) => {
   let form = '';
   form = '<html>' +
     '<body>'+
-    '<h1>Add your Pokémon</h1</h1>'+
+    '<h1>Add your Pokémon</h1>'+
     '<form method="POST" action="/pokemon">'+
+    '<h4>Name:</h4>' +
+    '<input type="text" name="pokename">'+
+    '<h4>ID:</h4>' +
     '<input type="text" name="id">'+
+    '<h4>Num:</h4>' +
     '<input type="text" name="num">'+
+    '<h4>Img Source:</h4>' +
     '<input type="text" name="img">'+
+    '<h4>Height:</h4>' +
     '<input type="text" name="height">'+
+    '<h4>Weight:</h4>' +
     '<input type="text" name="weight">'+
-    '<input type="submit" value="Submit">'+
+    '<br><input type="submit" value="Submit">'+
     '</form>'+
     '</body>'+
     '</html>';
@@ -106,6 +78,40 @@ jsonfile.readFile(file, (err, obj) => {
     });
 });
 
+
+//find pokemon info by name
+app.get('/pokemon/:name', (request, response) => {
+
+    jsonfile.readFile(file, (err, obj) => {
+
+        let pokeName = "";
+        let pokeWeight = "";
+        let pokemonFound = false;
+        let lowerCase = request.params.name.toLowerCase();
+        let capsFirst = lowerCase.charAt(0).toUpperCase();
+        let pokeToFind = capsFirst + lowerCase.slice(1);
+
+        console.log(pokeToFind);
+
+        //find pokeToFind in obj and return its name and weight
+        for (let i = 0; i < obj.pokemon.length; i++) {
+            if (obj.pokemon[i].name === pokeToFind) {
+                pokeName = obj.pokemon[i].name;
+                pokeWeight = obj.pokemon[i].weight;
+                pokemonFound = true;
+
+                console.log(obj.pokemon[i].name);
+                console.log(obj.pokemon[i].weight);
+            }
+        }
+
+        if (!pokemonFound) {
+            response.status(404).send(`<html><body><h1>Pikaboo:(</h1><br><p>Could not find information on ${pokeToFind} - Is that a new pokemon? Gotta catch em all!</p></body></html>`);
+        } else if (pokemonFound) {
+            response.send(`<html><body><h1>${pokeName}</h1><br><p>Weight: ${pokeWeight}</p></body></html>`);
+        }
+    });
+});
 
 //setting port as 8080
 app.listen(8080, () => console.log('~~~ Tuning in to the waves of port 8080 ~~~'));
