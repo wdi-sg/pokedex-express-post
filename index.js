b7f7e392e0f35fdaed1c0bb698f3c78f062303d9
w0bbbles <wobblebeat@gmail.com>
2019-07-16 17:33:04 +0800

    Basic

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const jsonfile = require('jsonfile');
 
-const FILE = 'pokedex.json';
+const FILE = ('pokedex.json');
+
+
 
 /**
  * ===================================
@@ -12,6 +14,10 @@ const FILE = 'pokedex.json';
 // Init express app
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({
+    extended: true
+}));
 /**
  * ===================================
  * Routes
@@ -20,38 +26,92 @@ const app = express();
 
 app.get('/:id', (request, response) => {
 
-  // get json from specified file
-  jsonfile.readFile(FILE, (err, obj) => {
-    // obj is the object from the pokedex json file
-    // extract input data from request
-    let inputId = parseInt( request.params.id );
+    // get json from specified file
+    jsonfile.readFile(FILE, (err, obj) => {
+        // obj is the object from the pokedex json file
+        // extract input data from request
+        let inputId = parseInt(request.params.id);
 
-    var pokemon;
+        var pokemon;
 
-    // find pokemon by id from the pokedex json file
-    for( let i=0; i<obj.pokemon.length; i++ ){
+        // find pokemon by id from the pokedex json file
+        for (let i = 0; i < obj.pokemon.length; i++) {
 
-      let currentPokemon = obj.pokemon[i];
+            let currentPokemon = obj.pokemon[i];
 
-      if( currentPokemon.id === inputId ){
-        pokemon = currentPokemon;
-      }
-    }
+            if (currentPokemon.id === inputId) {
+                pokemon = currentPokemon;
+            }
+        }
 
-    if (pokemon === undefined) {
+        if (pokemon === undefined) {
 
-      // send 404 back
-      response.status(404);
-      response.send("not found");
-    } else {
+            // send 404 back
+            response.status(404);
+            response.send("not found");
+        } else {
 
-      response.send(pokemon);
-    }
-  });
+            response.send(pokemon);
+        }
+    });
+});
+/**
+ * ===================================
+ * Basic Routes
+ * ===================================
+ */
+app.get('/pokemon/new', function(request, response) {
+
+    //make text field
+    response.send(
+        '<form method="POST" action="/pokemon">' +
+
+        '<h2>POKEMON INFORMATION</h2>' +
+        '<br>' +
+        'id:' +
+        '<input type="text" name="id">' +
+        '<br>' +
+        'number:' +
+        '<input type="text" name="num">' +
+        '<br>' +
+        'pokemon name:' +
+        '<input type="text" name="name">' +
+        '<br>' +
+        'pokemon img:' +
+        '<input type="text" name="img">' +
+        '<br>' +
+        'pokemon height:' +
+        '<input type="text" name="height">' +
+        '<br>' +
+        'pokemon weight:' +
+        '<input type="text" name="weight">' +
+        '<br>' +
+        '<input type="submit" value="Submit">' +
+        '</form>'
+    );
 });
 
+app.post('/pokemon', function(request, response) {
+    jsonfile.readFile(FILE, (err, obj) => {
+        if (err) {
+            console.log(err)
+        }
+        let newPokemon = request.body;
+        obj.pokemon.push(newPokemon);
+        jsonfile.writeFile(FILE, obj, (err) => {
+            console.log('done writing');
+            // now look inside your json file
+            response.send(newPokemon)
+        });
+    })
+});
+/**
+ * ===================================
+ * If URL has no request
+ * ===================================
+ */
 app.get('/', (request, response) => {
-  response.send("yay");
+    response.send("Don't give up!");
 });
 
 /**
