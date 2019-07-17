e1cb7f9b0205979e5083eb3ed75f16b8662bc84d
EliseChia <swedesurrender@gmail.com>
2019-07-18 00:47:11 +0800

    done with home page  + sorting route

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ const reactEngine = require('express-react-views').createEngine();
 app.engine('jsx', reactEngine);
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jsx');
+app.use(express.static(__dirname + '/public'));
 app.use(express.json());
 app.use(express.urlencoded({
     extended: true
@@ -22,82 +23,12 @@ app.use(methodOverride('_method'));
 const editpage = 'edit.jsx';
 const homepage = 'home.jsx';
 
-/**
- * ===================================
- * Helper Functions
- * ===================================
- */
-
-
-//* name/number/weight/height query(sortRequest) gets passed into sortPokemon function where it goes through a switch case to dynamically recreate pokemon obj each time - thus changing what is show on home page *//
-function sortPokemon(sortRequest, obj){
-    switch(sortRequest) {
-        case "name":
-            return
-            {"pokemon": obj.pokemon.sort(function(a,b) {
-                if (a[name] < b[name]) //a goes higher
-                    return -1
-                if (a[name] > b[name]) //b goes higher
-                    return 1
-                return 0               //a and b remain "equal"
-                })
-            };
-            break;
-
-        case "num":
-            return
-            {"pokemon": obj.pokemon.sort(function(a,b) {
-                if (a[num] < b[num])
-                    return -1
-                if (a[num] > b[num])
-                    return 1
-                return 0
-                })
-            };
-            break;
-
-        case "weight":
-            return {"pokemon": obj.pokemon.sort((function(a,b) {
-                if (a[weight] < b[weight])
-                    return -1
-                if (a[weight] > b[weight])
-                    return 1
-                return 0
-                })
-            };
-            break;
-
-        case "height":
-            return {"pokemon": obj.pokemon.sort((function(a,b) {
-                if (a[height] < b[height])
-                    return -1
-                if (a[height] > b[height])
-                    return 1
-                return 0
-                })
-            };
-            break;
-
-        default:
-            return
-            {"pokemon": obj.pokemon.sort(function(a,b) {
-                if (a[num] < b[num])
-                    return -1
-                if (a[num] > b[num])
-                    return 1
-                return 0
-                })
-            };
-            break;
-    }
-};
 
 /**
  * ===================================
  * Routes
  * ===================================
  */
-
 
  /*==== Redirect to front page ==== */
 app.get('/', (request, response) => {
@@ -106,115 +37,180 @@ app.get('/', (request, response) => {
 
  /*==== Front Page ==== */
 app.get('/pokemon', (request, response) => {
+    let sortRequest = request.query.sortby;
 
     jsonfile.readFile(file, (err, obj) => {
         if (err) {
             console.log("Something went wrong when displaying the front page.")
         }
-        let sortRequest = request.query.sortby;
-        //select name attribute is used to reference form data after form is submitted
-        response.render(homepage, obj, sortPokemon(sortRequest));
+        response.render(homepage, sortPokemon(sortRequest, obj));
     });
 });
 
+/**
+ * ===================================
+ * Helper Functions
+ * ===================================
+ */
+
+//* sortPokemon function: name/number/weight/height query(sortRequest) gets passed into sortPokemon function where it goes through a switch case to dynamically recreate pokemon obj each time - thus changing what is show on home page *//
+
+function sortPokemon(sortRequest, obj){
+    switch(sortRequest) {
+        case "name":
+            return {"pokemon": obj.pokemon.sort(function(a,b) {
+                if (a.name < b.name) //a goes higher
+                    return -1
+                if (a.name > b.name) //b goes higher
+                    return 1
+                return 0               //a and b remain "equal"
+                })
+            };
+
+        //random important note: syntax-wise, it seems to break code if first return is on a line by itself (because return just ends the function?)
+
+        //random important note 2: dot notation is a lot cleaner than bracket
+
+        case "num":
+            return {"pokemon": obj.pokemon.sort(function(a,b) {
+                if (a.num < b.num)
+                    return -1
+                if (a.num > b.num)
+                    return 1
+                return 0
+                }), "sortRequest": sortRequest
+                //adds temporary property of sortRequest to obj
+            };
+
+        case "weight":
+            return {"pokemon": obj.pokemon.sort(function(a,b) {
+                if (a.weight < b.weight)
+                    return -1
+                if (a.weight > b.weight)
+                    return 1
+                return 0
+                }), "sortRequest": sortRequest
+            };
+
+        case "height":
+            return {"pokemon": obj.pokemon.sort(function(a,b) {
+                if (a.height < b.height)
+                    return -1
+                if (a.height > b.height)
+                    return 1
+                return 0
+                }), "sortRequest": sortRequest
+            };
+
+        default:
+            obj["sortRequest"] = "num";
+            return obj;
+    }
+};
+
+/**
+ * ===================================
+ * To-Do
+ * ===================================
+ */
 //creating form on /pokemon/new
-app.get('/pokemon/new', (request, response) => {
+// app.get('/pokemon/new', (request, response) => {
 
-    console.log("birthing form");
+//     console.log("birthing form");
 
-    //set conditional to make id and num unique - use onclick function to self-generate a unique ID?
+//     //set conditional to make id and num unique - use onclick function to self-generate a unique ID?
 
-    let form =
-        '<html>' +
-        '<body>' +
-        '<h1>Add your Pokémon</h1>' +
-        '<form method="POST" action="/pokemon">' +
-        '<h4>ID:</h4>' +
-        '<input type="text" name="id">' +
-        '<h4>Num:</h4>' +
-        '<input type="text" name="num">' +
-        '<h4>Name:</h4>' +
-        '<input type="text" name="name">' +
-        '<h4>Img Source:</h4>' +
-        '<input type="text" name="img">' +
-        '<h4>Height:</h4>' +
-        '<input type="text" name="height">' +
-        '<h4>Weight:</h4>' +
-        '<input type="text" name="weight">' +
-        '<br><input type="submit" value="Submit">' +
-        '</form>' +
-        '</body>' +
-        '</html>';
+//     let form =
+//         '<html>' +
+//         '<body>' +
+//         '<h1>Add your Pokémon</h1>' +
+//         '<form method="POST" action="/pokemon">' +
+//         '<h4>ID:</h4>' +
+//         '<input type="text" name="id">' +
+//         '<h4>Num:</h4>' +
+//         '<input type="text" name="num">' +
+//         '<h4>Name:</h4>' +
+//         '<input type="text" name="name">' +
+//         '<h4>Img Source:</h4>' +
+//         '<input type="text" name="img">' +
+//         '<h4>Height:</h4>' +
+//         '<input type="text" name="height">' +
+//         '<h4>Weight:</h4>' +
+//         '<input type="text" name="weight">' +
+//         '<br><input type="submit" value="Submit">' +
+//         '</form>' +
+//         '</body>' +
+//         '</html>';
 
-    response.send(form);
-});
+//     response.send(form);
+// });
 
 //save the form data to pokedex.json
-app.post('/pokemon', (request, response) => {
+// app.post('/pokemon', (request, response) => {
 
-    var newPoke = request.body;
+//     var newPoke = request.body;
 
-    console.log(newPoke);
+//     console.log(newPoke);
 
-    // save in data file
-    jsonfile.readFile(file, (err, obj) => {
-        //if error reading
-        if (err) {
-            console.log("could not read pokedex file");
-            console.log(err)
-        }
+//     // save in data file
+//     jsonfile.readFile(file, (err, obj) => {
+//         //if error reading
+//         if (err) {
+//             console.log("could not read pokedex file");
+//             console.log(err)
+//         }
 
-        //if not error
-        obj.pokemon.push(newPoke);
+//         //if not error
+//         obj.pokemon.push(newPoke);
 
-        jsonfile.writeFile(file, obj, (err) => {
-            //if error writing
-            if (err) {
-                console.log("could not write new poke");
-                console.log(err);
-                response.status(503).send("sorry, the service in unavailable..");
-            } else {
-                //show that the request was successful
-                console.log("new mon successfully added");
-                response.json(newPoke);
-            }
-        });
-    });
-});
+//         jsonfile.writeFile(file, obj, (err) => {
+//             //if error writing
+//             if (err) {
+//                 console.log("could not write new poke");
+//                 console.log(err);
+//                 response.status(503).send("sorry, the service in unavailable..");
+//             } else {
+//                 //show that the request was successful
+//                 console.log("new mon successfully added");
+//                 response.json(newPoke);
+//             }
+//         });
+//     });
+// });
 
 //find pokemon info by name
-app.get('/pokemon/:name', (request, response) => {
+// app.get('/pokemon/:name', (request, response) => {
 
-    jsonfile.readFile(file, (err, obj) => {
+//     jsonfile.readFile(file, (err, obj) => {
 
-        let pokeName = "";
-        let pokeWeight = "";
-        let pokemonFound = false;
-        let lowerCase = request.params.name.toLowerCase();
-        let capsFirst = lowerCase.charAt(0).toUpperCase();
-        let pokeToFind = capsFirst + lowerCase.slice(1);
+//         let pokeName = "";
+//         let pokeWeight = "";
+//         let pokemonFound = false;
+//         let lowerCase = request.params.name.toLowerCase();
+//         let capsFirst = lowerCase.charAt(0).toUpperCase();
+//         let pokeToFind = capsFirst + lowerCase.slice(1);
 
-        console.log(pokeToFind);
+//         console.log(pokeToFind);
 
-        //find pokeToFind in obj and return its name and weight
-        for (let i = 0; i < obj.pokemon.length; i++) {
-            if (obj.pokemon[i].name === pokeToFind) {
-                pokeName = obj.pokemon[i].name;
-                pokeWeight = obj.pokemon[i].weight;
-                pokemonFound = true;
+//         //find pokeToFind in obj and return its name and weight
+//         for (let i = 0; i < obj.pokemon.length; i++) {
+//             if (obj.pokemon[i].name === pokeToFind) {
+//                 pokeName = obj.pokemon[i].name;
+//                 pokeWeight = obj.pokemon[i].weight;
+//                 pokemonFound = true;
 
-                console.log(obj.pokemon[i].name);
-                console.log(obj.pokemon[i].weight);
-            }
-        }
+//                 console.log(obj.pokemon[i].name);
+//                 console.log(obj.pokemon[i].weight);
+//             }
+//         }
 
-        if (!pokemonFound) {
-            response.status(404).send(`<html><body><h1>Pikaboo:(</h1><br><p>Could not find information on ${pokeToFind} - Is that a new pokemon? Gotta catch em all!</p></body></html>`);
-        } else if (pokemonFound) {
-            response.send(`<html><body><h1>${pokeName}</h1><br><p>Weight: ${pokeWeight}</p></body></html>`);
-        }
-    });
-});
+//         if (!pokemonFound) {
+//             response.status(404).send(`<html><body><h1>Pikaboo:(</h1><br><p>Could not find information on ${pokeToFind} - Is that a new pokemon? Gotta catch em all!</p></body></html>`);
+//         } else if (pokemonFound) {
+//             response.send(`<html><body><h1>${pokeName}</h1><br><p>Weight: ${pokeWeight}</p></body></html>`);
+//         }
+//     });
+// });
 
  /*==== Setting Up Port ==== */
 app.listen(8080, () => console.log('~~~ Tuning in to the waves of port 8080 ~~~'));
--- views/home.jsx
+++ views/home.jsx
@@ -5,7 +5,7 @@ class Homepage extends React.Component {
     const pokeEntries = this.props.pokemon.map(pokemon =>{
         let pokePage = "/pokemon/" + pokemon.num;
         return (
-                <div>
+                <div className="pokemon-card">
                     <a href={pokePage}>
                         <img src={pokemon.img}/>
                         <p>{pokemon[this.props.sortRequest]}</p>
@@ -19,23 +19,25 @@ class Homepage extends React.Component {
         <html>
         <head>
             <title>Gotta Catch Em All!</title>
+            <link href="https://fonts.googleapis.com/css?family=Ubuntu:300,400&display=swap" rel="stylesheet"/>
             <link rel="stylesheet" type="text/css" href="/style.css"/>
         </head>
         <header>
-            <img src="../img/header.jpg"/>
-        </header>
 
+        </header>
         <body>
             <form method='GET' action='/pokemon'>
                 <select name='sortby'>
-                    <option value='name'>Name</option>
                     <option value='num'>Number</option>
+                    <option value='name'>Name</option>
                     <option value='height'>Height</option>
                     <option value='weight'>Weight</option>
                 </select>
                 <input type='submit'/>
             </form>
-            {pokeEntries}
+            <div id="pokemon-container">
+                {pokeEntries}
+            </div>
         </body>
         </html>
     );
