813e8afe314480ecfb0a87453f5c439d346b56d0
wilfredloh <lohwilfred19@gmail.com>
2019-07-18 20:50:14 +0800

    added edit feature

--- index.js
+++ index.js
@@ -1,49 +1,46 @@
-const express = require('express');
-const jsonfile = require('jsonfile');
-
-const FILE = 'pokedex.json';
-
 /**
  * ===================================
  * Configurations and set up
  * ===================================
  */
-
-// Init express app
+const express = require('express');
+const jsonfile = require('jsonfile');
+const FILE = 'pokedex.json';
 const app = express();
-
+const reactEngine = require('express-react-views').createEngine();
+app.engine('jsx', reactEngine);
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jsx');
 app.use(express.json());
-
 app.use(express.urlencoded({
-  extended: true
+    extended: true
 }));
-
+const methodOverride = require('method-override')
+app.use(methodOverride('_method'));
 /**
  * ===================================
  * FUNCTIONS
  * ===================================
  */
 
-let createPokemon = (request, response) => {
-    let form = '';
-    form = `
-    <form method="POST" action="/pokemon">
-      Pokemon Name:<br>
-      <br><input type="text" name="id" placeholder="id">
-      <br><input type="text" name="num" placeholder="num">
-      <br><input type="text" name="name" placeholder="name">
-      <br><input type="text" name="img" placeholder="img">
-      <br><input type="text" name="height" placeholder="height">
-      <br><input type="text" name="weight" placeholder="weight">
-      <br><input type="submit" value="Submit">
-    </form>
-    `
-    response.send(form);
+let showAllPokemon = (request, response) => {
+    jsonfile.readFile(FILE, (err,obj) => {
+        let query = request.query.sortby;
+        let pokemon = obj.pokemon;
+        let data = {
+            pokemonKey : pokemon,
+            queryKey : query
+        }
+        response.render('home', data)
+    });
 }
 
-let addCreatedPokemon = (request, response) => {
-    let newPoke = request.body;
+let createPokemon = (request, response) => {
+    response.render('createForm');
+}
 
+let createdPokemonResult = (request, response) => {
+    let newPoke = request.body;
     jsonfile.readFile(FILE, (err,obj) => {
         newPoke.num = obj.lastKey + 1;
         obj.lastKey++;
@@ -58,133 +55,68 @@ let addCreatedPokemon = (request, response) => {
     });
 }
 
-let showPokemonDetailsUsingInput = (request, response) => {
-    // get json from specified file
-  jsonfile.readFile(FILE, (err, obj) => {
-    // obj is the object from the pokedex json file
-    // extract input data from request
-    let inputId = parseInt( request.params.id );
-    var pokemon;
-
-    // find pokemon by id from the pokedex json file
-    for( let i=0; i<obj.pokemon.length; i++ ){
-
-      let currentPokemon = obj.pokemon[i];
-
-      if( currentPokemon.id === inputId ){
-        pokemon = currentPokemon;
-      }
-    }
-
-    if (pokemon === undefined) {
-      // send 404 back
-      response.status(404);
-      response.send("not found");
-    } else {
-      response.send(pokemon);
-    }
-  });
-}
-
-let showPokemonDetailsUsingLink = (request, response) => {
-    // console.log(request.route.path);
-    // console.log(request.route.body);
-    // console.log(request.route);
-    // console.log(request.url);
-
+let showPokemon = (request, response) => {
     jsonfile.readFile(FILE, (err, obj) => {
+        let id = parseInt(request.params.id);
         for(let i=0; i<obj.pokemon.length; i++) {
-            if ( request.url === `/poke/${obj.pokemon[i].id}`){
-                response.send(
-                    `${obj.pokemon[i].name}<br>
-                    <img src="${obj.pokemon[i].img}"><br>Height: ${obj.pokemon[i].height}<br>Weight: ${obj.pokemon[i].weight}`)
+            if ( id === obj.pokemon[i].id ){
+                console.log('id2',id)
+                console.log('obj id', obj.pokemon[i].id)
+                let pokemon = obj.pokemon[i];
+                let data = {
+                    pokemon : pokemon
+                }
+
+                response.render('pokemon', data)
             }
         }
     });
 }
 
-let sortAllPokemon = (request, response) => {
-    let query = request.query.sortby;
-
-    jsonfile.readFile(FILE, (err,obj) => {
-        let content = '';
-        let form = '';
-        form = `
-        <form action="/pokemon/new">
-            <button type="submit">Create Pokemon </button>
-        </form> <br>
-
-        <form method="GET">
-            <select name="sortby">
-                <option value="id">Id</option>
-                <option value="name">Name</option>
-                <option value="weight">Weight</option>
-                <option value="height">Height</option>
-            </select>
-            <input type="submit" value="Sort Pokemon">
-        </form>
-        `;
-
-        content = form;
-
-        if (query === 'name') {
-            obj.pokemon.sort(compareName);
-        } else if (query === 'weight') {
-            obj.pokemon.sort(compareWeight);
-        } else if (query === 'height') {
-            obj.pokemon.sort(compareHeight);
-        }
-
-        //once sorted, display all pokemon to server
+let editPokemon = (request, response) => {
+    jsonfile.readFile (FILE, (err,obj) => {
+        let id = parseInt(request.params.id);
         for(let i=0; i<obj.pokemon.length; i++) {
-            let eachPoke = obj.pokemon[i].name;
-            content +=
-            `<br><a href='/poke/${obj.pokemon[i].id}'>${obj.pokemon[i].name}</a>`;
+            if ( id === obj.pokemon[i].id){
+                let pokemon = obj.pokemon[i];
+                let data = {
+                    pokemon : pokemon
+                }
+                response.render('editForm', data)
+            }
         }
-        response.send(content);
-    });
+    })
 }
 
-let compareName = (a, b) => {
-  // Use toUpperCase() to ignore character casing
-  const nameA = a.name.toUpperCase();
-  const nameB = b.name.toUpperCase();
+let editPokemonResult = (request, response) => {
 
-  let comparison = 0;
-  if (nameA > nameB) {
-    comparison = 1;
-  } else if (nameA < nameB) {
-    comparison = -1;
-  }
-  return comparison;
-}
+    let newPoke = request.body;
+    jsonfile.readFile (FILE, (err,obj) => {
+        let id = parseInt(request.params.id);
+        for(let i=0; i<obj.pokemon.length; i++) {
+            if ( id === obj.pokemon[i].id){
+                obj.pokemon[i] = request.body;
+                // let pokemon = obj.pokemon[i];
+                // pokemon.id = request.body.id;
+                // pokemon.num = request.body.num;
+                // pokemon.name = request.body.name;
+                // pokemon.image = request.body.image;
+                // pokemon.height = request.body.height;
+                // pokemon.weight = request.body.weight;
+            }
+        }
+        jsonfile.writeFile(FILE, obj, (err) => {
+            if (err) {
+                console.log('error in writing!');
+            } else {
+                response.send(`EDIT POKEMON SUCCESS!`);
+            }
+        })
+    })
 
-let compareWeight = (a, b) => {
-  // Use toUpperCase() to ignore character casing
-  const weightA = parseFloat(a.weight);
-  const weightB = parseFloat(b.weight);
 
-  let comparison = 0;
-  if (weightA > weightB) {
-    comparison = 1;
-  } else if (weightA < weightB) {
-    comparison = -1;
-  }
-  return comparison;
-}
 
-let compareHeight = (a, b) => {
-  // Use toUpperCase() to ignore character casing
-  const heightA = parseFloat(a.height);
-  const heightB = parseFloat(b.height);
 
-  let comparison = 0;
-  if (heightA > heightB) {
-    comparison = 1;
-  } else if (heightA < heightB) {
-    comparison = -1;
-  }
-  return comparison;
 }
 
 /**
@@ -193,12 +125,13 @@ let compareHeight = (a, b) => {
  * ===================================
  */
 
+app.get('/pokemon', showAllPokemon);
 app.get('/pokemon/new', createPokemon);
-app.post('/pokemon', addCreatedPokemon);
+app.post('/pokemon', createdPokemonResult);
+app.get('/pokemon/:id', showPokemon);
+app.get('/pokemon/:id/edit', editPokemon);
+app.put('/pokemon/:id', editPokemonResult)
 
-app.get('/poke/:id', showPokemonDetailsUsingLink);
-app.get('/:id', showPokemonDetailsUsingInput);
-app.get('/', sortAllPokemon);
 
 /**
  * ===================================
---
+++ views/home.jsx
@@ -0,0 +1,100 @@
+var React = require('react');
+
+class Home extends React.Component {
+  render() {
+
+    let content = '';
+    let form = '';
+
+    if (this.props.queryKey === 'name') {
+        this.props.pokemonKey.sort(compareName);
+    } else if (this.props.queryKey  === 'weight') {
+        this.props.pokemonKey.sort(compareWeight);
+    } else if (this.props.queryKey  === 'height') {
+        this.props.pokemonKey.sort(compareHeight);
+    }
+
+
+
+    let array = this.props.pokemonKey.map(pokemon => {
+        let url = "/pokemon/" + pokemon.id;
+        return <div><a href={url}>{pokemon.name}</a></div>;
+    })
+
+    return (
+      <html>
+        <body>
+
+          <div>
+            <h1>Sort Poke!</h1>
+          </div>
+
+          <div>
+            <form action="/pokemon/new">
+                <button type="submit">Create Pokemon </button>
+            </form>
+
+            <form method="GET">
+                <select name="sortby">
+                    <option value="id">Id</option>
+                    <option value="name">Name</option>
+                    <option value="weight">Weight</option>
+                    <option value="height">Height</option>
+                </select>
+                <input type="submit" value="Sort Pokemon"/>
+            </form>
+          </div>
+
+          <div>
+            {array}
+          </div>
+
+        </body>
+      </html>
+    );
+  }
+}
+
+module.exports = Home;
+
+let compareName = (a, b) => {
+  // Use toUpperCase() to ignore character casing
+  const nameA = a.name.toUpperCase();
+  const nameB = b.name.toUpperCase();
+
+  let comparison = 0;
+  if (nameA > nameB) {
+    comparison = 1;
+  } else if (nameA < nameB) {
+    comparison = -1;
+  }
+  return comparison;
+}
+
+let compareWeight = (a, b) => {
+  // Use toUpperCase() to ignore character casing
+  const weightA = parseFloat(a.weight);
+  const weightB = parseFloat(b.weight);
+
+  let comparison = 0;
+  if (weightA > weightB) {
+    comparison = 1;
+  } else if (weightA < weightB) {
+    comparison = -1;
+  }
+  return comparison;
+}
+
+let compareHeight = (a, b) => {
+  // Use toUpperCase() to ignore character casing
+  const heightA = parseFloat(a.height);
+  const heightB = parseFloat(b.height);
+
+  let comparison = 0;
+  if (heightA > heightB) {
+    comparison = 1;
+  } else if (heightA < heightB) {
+    comparison = -1;
+  }
+  return comparison;
+}
