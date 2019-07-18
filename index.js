8d961420b45edc1ade913facecc75881671048c9
yoda14 <choobthenoob@hotmail.com>
2019-07-18 19:56:19 +0800

    Theres an edit page with prefilled values!

--- index.js
+++ index.js
@@ -3,13 +3,38 @@ const express = require('express');
 const jsonfile = require('jsonfile');
 const FILE = 'pokedex.json';
 const app = express();
+const reactEngine = require('express-react-views').createEngine();
+app.engine('jsx', reactEngine);
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jsx');
 
-// tell your app to use the module
 app.use(express.json());
-
 app.use(express.urlencoded({
   extended: true
 }));
+
+app.get('/pokemon/:id/edit', (request, response) =>{
+    jsonfile.readFile(FILE, (err, obj) => {
+        let inputId = parseInt( request.params.id );
+        var pokemon;
+        for( let i=0; i<obj.pokemon.length; i++ ){
+            let currentPokemon = obj.pokemon[i];
+            if( currentPokemon.id === inputId ){
+                pokemon = currentPokemon;
+            }
+        }
+
+        if (pokemon === undefined) {
+            response.status(404);
+            response.send("not found");
+        } else {
+            let data={
+                pokemonEdit: pokemon
+            };
+            response.render('main', data);
+        }
+    });
+});
 
 
 
@@ -51,27 +76,22 @@ app.get('/pokemon/new', (request, response) => {
 '<h1>Enter a new pokemon!</h1>'+
  'Id:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;'+
   '<input type="text" name="id">'+
-  // '<input type="submit" value="Submit">'+
 '</br>'+
 '</br>'+
  'Number:&nbsp;'+
   '<input type="text" name="num">'+
-  // '<input type="submit" value="Submit">'+
   '</br>'+
   '</br>'+
  'Name:&nbsp;&nbsp;&nbsp;&nbsp;'+
   '<input type="text" name="name">'+
-  // '<input type="submit" value="Submit">'+
   '</br>'+
   '</br>'+
  'Image:&nbsp;&nbsp;&nbsp;&nbsp;'+
   '<input type="text" name="img">'+
-  // '<input type="submit" value="Submit">'+
   '</br>'+
   '</br>'+
  'Height:&nbsp;&nbsp;&nbsp;'+
   '<input type="text" name="height">'+
-  // '<input type="submit" value="Submit">'+
   '</br>'+
   '</br>'+
  'Weight:&nbsp;&nbsp;'+
@@ -90,19 +110,15 @@ app.post('/pokemon', function(request, response) {
     var animal = request.body;
     console.log( animal );
 
-      // save in data file
   jsonfile.readFile(FILE, (err, obj) => {
     if( err ){
       console.log("error reading file");
       console.log(err)
     }
 
-    // console.log("what i currently have");
-    // console.log(obj.pokemon);
+    animal.id = parseInt(animal.id);
 
-    // save data
     obj.pokemon.push(animal);
-    // obj[key] = value;
 
     jsonfile.writeFile(FILE, obj, (err) => {
       if( err ){
@@ -117,17 +133,6 @@ app.post('/pokemon', function(request, response) {
     });
   });
 
-  //debug code (output request body)
-  // console.log(request.body);
-
-
-  // save the request body
-
-
-  // jsonfile.writeFile('pokedex.json', request.body, (err) => {
-  //   console.error(err)
-  //   response.send(request.body);
-  // });
 });
 
 
---
+++ views/main.jsx
@@ -0,0 +1,43 @@
+var React = require('react');
+
+class MainPage extends React.Component {
+  render() {
+    console.log("here")
+    return (
+        <html>
+        <body>
+        <form>
+        <h1>Edit your pokemon details here</h1>
+        <h3>Is your Pokemon gaining muscle mass?</h3>
+        <h3>Cut down on protein shakes!</h3>
+        Id:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <input type="text" name="id" defaultValue={this.props.pokemonEdit.id} />
+        <br/>
+        <br/>
+        The no. of:
+        <input type="text" name="num" defaultValue={this.props.pokemonEdit.num}/>
+        <br/>
+        <br/>
+        Name:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <input type="text" name="name" defaultValue={this.props.pokemonEdit.name}/>
+        <br/>
+        <br/>
+        Image:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <input type="text" name="img" defaultValue={this.props.pokemonEdit.img}/>
+        <br/>
+        <br/>
+        Height:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <input type="text" name="height" defaultValue={this.props.pokemonEdit.height}/>
+        <br/>
+        <br/>
+        Weight:&nbsp;&nbsp;&nbsp;&nbsp;
+        <input type="text" name="weight" defaultValue={this.props.pokemonEdit.weight}/>
+        <input type="submit" value="Submit"/>
+        </form>
+        </body>
+        </html>
+        );
+}
+}
+
+module.exports = MainPage;
