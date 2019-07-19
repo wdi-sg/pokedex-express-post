048c393ab7cb437ca4b71d511ed859912619dca0
gitwenlei <ngwenlei@gmail.com>
2019-07-19 14:57:59 +0800

    Change list to div to display pokemon cards

--- views/home.jsx
+++ views/home.jsx
@@ -1,24 +1,27 @@
 var React = require('react');
 
+const Layout = require('./layout.jsx');
+
+
 class Home extends React.Component {
     render() {
         const pokemon = this.props.pokemonKey.map(pokemon =>
            <div key={pokemon.name} className="pokemon">
-                <img src={pokemon.img} />
-                <ul>
-                    <li>Name: {pokemon.name}</li>
-                    <li>Height: {pokemon.height}</li>
-                    <li>Weight: {pokemon.weight}</li>
-                </ul>
+                <img src={pokemon.img} className="img-fluid"/>
+                    <p>Name: {pokemon.name}</p>
+                    <p>Height: {pokemon.height}</p>
+                    <p>Weight: {pokemon.weight}</p>
            </div>
         );
     return (
       <html>
+        <head>
+            <link rel="stylesheet" type="text/css" href="/style.css"></link>
+        </head>
         <body>
-          <div>
-            <h1>Hello! Welcome to Pokedex!</h1>
-          </div>
-          <div>
+          <div className="main-wrapper">
+          <div className="nav-bar"></div>
+          <div className="header"><h1>Hello! Welcome to Pokedex!</h1></div>
             {pokemon}
           </div>
         </body>
