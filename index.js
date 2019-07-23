3d9f403ad3f729303afd5648d04e4e1ec430e106
sharkyu3 <50606558+sharkyu3@users.noreply.github.com>
2019-07-23 10:34:58 +0800

    started

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 const express = require('express');
 const jsonfile = require('jsonfile');
 
-const FILE = 'pokedex.json';
-
 /**
  * ===================================
  * Configurations and set up
@@ -12,11 +10,67 @@ const FILE = 'pokedex.json';
 // Init express app
 const app = express();
 
+const reactEngine = require('express-react-views').createEngine();
+app.engine('jsx', reactEngine);
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jsx');
+app.use(express.json());
+app.use(express.urlencoded({
+    extended: true
+}));
+const methodOverride = require('method-override')
+app.use(methodOverride('_method'));
+
+/**
+ * ===================================
+ * Global Variable
+ * ===================================
+ */
+const FILE = 'pokedex.json';
+
 /**
  * ===================================
  * Routes
  * ===================================
  */
+
+ app.get('/new', (request, response) => {
+    let form = `
+    <form method="POST" action="/new">
+        <p><input name = "id" value="id"></input></p>
+        <p><input name = "num" value="num"></input></p>
+        <p><input name = "name" value="name"></input></p>
+        <p><input name = "img" value="img"></input></p>
+        <p><input name = "height" value="height"></input></p>
+        <p><input name = "weight" value="weight"></input></p>
+        <p><button type="submit">Submit</button></p>
+    </form>
+    `
+    response.send(form);
+})
+
+ app.post('/new', (request, response) => {
+    var newPokemon = request.body;
+    //save the request body
+    jsonfile.readFile(FILE, (err, obj) => {
+        if( err ){
+            console.log("error reading file");
+            console.log(err)
+        }else{
+            var pokemon = obj.pokemon;
+            pokemon.push(newPokemon);
+            jsonfile.writeFile(FILE, newPokemon, (err) => {
+                if( err ){
+                console.log("error writing file");
+                console.log(err)
+                response.status(503).send("ERROR WRITING FILE");
+                } else {
+                console.log("write file successful");
+                }
+            });
+        }
+    })
+})
 
 app.get('/:id', (request, response) => {
 
@@ -42,7 +96,7 @@ app.get('/:id', (request, response) => {
 
       // send 404 back
       response.status(404);
-      response.send("not found");
+      response.send("no such pokemon");
     } else {
 
       response.send(pokemon);
@@ -50,9 +104,11 @@ app.get('/:id', (request, response) => {
   });
 });
 
+
 app.get('/', (request, response) => {
   response.send("yay");
 });
+
 
 /**
  * ===================================
