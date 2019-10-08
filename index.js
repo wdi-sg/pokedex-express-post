ae085ef53d5bd9ebf86849601af6285d51e1d586
dannyozh <danielongzhihao@gmail.com>
2019-10-08 17:57:38 +0800

    achieved mvp

--- index.js
+++ index.js
@@ -8,6 +8,10 @@ const reactEngine = require('express-react-views').createEngine();
 app.engine('jsx', reactEngine);
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jsx');
+app.use(express.json());
+app.use(express.urlencoded({
+  extended: true
+}));
 
 // app.get('/pokemon/:id', (request, response) => {
 
@@ -42,9 +46,45 @@ app.set('view engine', 'jsx');
 // });
 
 app.get('/pokemon/new', (request, response) => {
-
+    console.log("form created");
     response.render('home');
 });
+
+app.post('/pokemon', (request, response) => {
+    console.log("submitting form details");
+    console.log(request.body);
+    jsonfile.readFile(FILE, (err, obj) => {
+        let newPokemon = request.body;
+        let allPokemon = obj;
+        console.log("reading file");
+        // // create empty loop
+        // let newPokeList = [];
+        // // create objects list of data
+        // const userData = {
+        //     id: request.body.id,
+        //     num: request.body.num,
+        //     name: request.body.name,
+        //     img: request.body.img,
+        //     height: request.body.height,
+        //     weight: request.body.weight
+        // }
+        // newPokeList.push(userData);
+        // "action part"
+        allPokemon.pokemon.push(newPokemon);
+        jsonfile.writeFile(FILE, allPokemon, (err) => {
+            if (err) {
+                console.error(err)
+            } else {
+                // "view" part
+                response.send(request.body);
+                console.log('save success!')
+            }
+
+        });
+    console.log("append to main pokedex");
+    });
+});
+
 
 
 
--- views/home.jsx
+++ views/home.jsx
@@ -8,13 +8,13 @@ class Home extends React.Component {
           <div>
             <h1>Hello pokelovers</h1>
             <form method="POST" action="/pokemon">
-              Pokemon info:
-              <input type="text" name="id"/>
-              <input type="text" name="num"/>
-              <input type="text" name="name"/>
-              <input type="text" name="img"/>
-              <input type="text" name="height"/>
-              <input type="text" name="weight"/>
+              Pokemon info:<br/>
+              <input type="text" name="id" placeholder="id"/><br/>
+              <input type="number" name="num" placeholder="num"/><br/>
+              <input type="text" name="name" placeholder="name"/><br/>
+              <input type="url" name="img" placeholder="img"/><br/>
+              <input type="number" name="height" placeholder="height"/><br/>
+              <input type="number" name="weight" placeholder="weight"/><br/>
               <input type="submit" value="Submit"/>
             </form>
           </div>
