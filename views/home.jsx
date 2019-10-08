2a6779d008b1c569bb9bec13983451a00eee60fc
David M <davidmosk@gmail.com>
2019-10-08 19:43:21 +0800

    split new and home into 2 files, make home link to new, and add search by id

--- views/home.jsx
+++ views/home.jsx
@@ -5,12 +5,14 @@ class Home extends React.Component {
       <html>
         <body>
                   <div>
-                  { this.props.data }
+                  { this.props.warning }
 
-            <h1>Pokedex Post!</h1>
-            <form method="POST" action="http://localhost:3000/pokemon">
-            Pokedex Name:<br/>
-            <input type="text" name="id"/>
+            <h1>Would you like to:</h1>
+            <a href="">Add a New Pokemon?</a><br/><br/>
+            Or Search for a Pokemon by Number?<br/>
+                        <form method="Get" action="http://localhost:3000/pokemon/id">
+            ID: <input type="text" name="id"/>
+            <br/>
             <input type="submit" value="Submit"/>
             </form>
 
