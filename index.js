155264a6d0b56849d06a7f08b6f28721216d0123
“woofborn” <wolf.born@gmail.com>
2019-10-08 17:43:08 +0800

    New pokemon input form generated through react.

--- index.js
+++ index.js
@@ -18,74 +18,38 @@ app.use(express.urlencoded({
   extended: true
 }));
 
+// this line below, sets a layout look to your express project
+const reactEngine = require('express-react-views').createEngine();
+app.engine('jsx', reactEngine);
+
+// this tells express where to look for the view files
+app.set('views', __dirname + '/views');
+
+// this line sets react to be the default view engine
+app.set('view engine', 'jsx');
+
 /**
  * ===================================
  * Routes
  * ===================================
  */
 
-// app.get('/pokemon/:id', (request, response) => {
 
-//   // get json from specified file
-//   jsonfile.readFile(FILE, (err, obj) => {
-//     // obj is the object from the pokedex json file
-//     // extract input data from request
-//     let inputId = parseInt( request.params.id );
-
-//     var pokemon;
-
-//     // find pokemon by id from the pokedex json file
-//     for( let i=0; i<obj.pokemon.length; i++ ){
-
-//       let currentPokemon = obj.pokemon[i];
-
-//       if( currentPokemon.id === inputId ){
-//         pokemon = currentPokemon;
-//       }
-//     }
-
-//     if (pokemon === undefined) {
-
-//       // send 404 back
-//       response.status(404);
-//       response.send("not found");
-//     } else {
-
-//       response.send(pokemon);
-//     }
-//   });
-// });
-
-app.get('/', (request, response) => {
+ app.get('/', (request, response) => {
   response.send("yay");
 });
 
 app.get('/pokemon/new', (request,response) => {
 
-        response.send(`<h1> POKEMON</h1>
-            <form method="POST" action="/pokemon">
-      ID:
-      <input type="text" name="id"><br>
-      Number:
-      <input type="text" name="num"><br>
-      Name:
-      <input type="text" name="name"><br>
-      Image:
-      <input type="text" name="img"><br>
-      Height:
-      <input type="text" name="height"><br>
-      Weight:
-      <input type="text" name="weight"><br>
-      <input type="submit" value="Submit">
-    </form>`)
+    response.render('home')
 
 })
 
 app.post('/pokemon', function(request, response) {
 
   //debug code (output request body)
-  console.log(request.body);
- let pokeNew = request.body
+    console.log(request.body);
+    let pokeNew = request.body
 
     jsonfile.readFile(file, (err, obj) => {
 
@@ -95,11 +59,8 @@ app.post('/pokemon', function(request, response) {
         obj.pokemon.push(pokeNew)
         console.log(obj.pokemon[obj.pokemon.length-1])
 
-
-
-
   // // save the request body
-          jsonfile.writeFile(file, obj, (err) => {
+          jsonfile.writeFile(file, obj, {spaces:2},(err) => {
             console.error(err)
 
     // now look inside your json file
@@ -107,7 +68,42 @@ app.post('/pokemon', function(request, response) {
          });
     })
     response.send(request.body);
+    console.log()
 });
+
+app.get('/pokemon/:id', (request, response) => {
+
+  // get json from specified file
+  jsonfile.readFile(FILE, (err, obj) => {
+    // obj is the object from the pokedex json file
+    // extract input data from request
+    let inputId = parseInt( request.params.id );
+
+    var pokemon;
+
+    // find pokemon by id from the pokedex json file
+    for( let i=0; i<obj.pokemon.length; i++ ){
+
+      let currentPokemon = obj.pokemon[i];
+
+      if( currentPokemon.id === inputId ){
+        pokemon = currentPokemon;
+      }
+    }
+
+    if (pokemon === undefined) {
+
+      // send 404 back
+      response.status(404);
+      response.send("not found");
+    } else {
+
+      response.send(pokemon);
+    }
+  });
+});
+
+
 
 /**
  * ===================================
---
+++ views/pokemon.jsx
@@ -0,0 +1,36 @@
+var React = require('react');
+class Pokemon extends React.Component {
+  render() {
+    return (
+      <html>
+        <body>
+          <div>
+
+           <h1> POKEMON</h1>
+
+            <form method="POST" action="/pokemon">
+              ID:
+              <input type="text" name="id" />
+              Number:
+              <input type="text" name="num" />
+              Name:
+              <input type="text" name="name" />
+              Image:
+              <input type="text" name="img" />
+              Height:
+              <input type="text" name="height" />
+              Weight:
+              <input type="text" name="weight" />
+              <input type="submit" value="Submit" />
+
+             </form>
+
+
+          </div>
+        </body>
+      </html>
+    );
+  }
+}
+
+module.exports = Pokemon;
