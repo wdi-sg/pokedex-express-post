833685dbbc2a000227ca40d32139f1724a3e6c9a
dannyozh <danielongzhihao@gmail.com>
2019-10-08 22:30:38 +0800

    created 'interactive' welcome message

--- views/landing.jsx
+++ views/landing.jsx
@@ -7,6 +7,8 @@ class Landing extends React.Component {
         <body>
           <div>
             <h1>Pokedex is loaded and ready...</h1>
+            <img src="https://i.gifer.com/GEEd.gif"/>
+            <h3>To begin, type in the filepath "/pokemon/id", where id is a numerical value between 1 and 151.</h3>
           </div>
         </body>
       </html>
