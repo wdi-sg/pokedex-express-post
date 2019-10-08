27f5535e16940257b3c069deeb485dc61965d61c
garricktgh <garrick@hotmail.sg>
2019-10-08 18:27:40 +0800

    complete deliverable

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const jsonfile = require('jsonfile');
-
+const reactEngine = require('express-react-views');
 const FILE = 'pokedex.json';
+
 
 /**
  * ===================================
@@ -12,11 +13,34 @@ const FILE = 'pokedex.json';
 // Init express app
 const app = express();
 
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jsx');
+app.engine('jsx', require('express-react-views').createEngine());
+app.use(express.json());
+app.use(express.urlencoded({
+  extended: true
+}));
+
 /**
  * ===================================
  * Routes
  * ===================================
  */
+
+app.get('/pokemon/new', (request, response) => {
+
+  response.render ('new');
+  // response.send(`<form method="POST" action="/pokemon">
+  // Pokemon details:<br><br>
+  // id: <input type="text" name="id"><br><br>
+  // num: <input type="text" name="num"><br><br>
+  // name: <input type="text" name="name"><br><br>
+  // img: <input type="text" name="img"><br><br>
+  // height: <input type="text" name="height"><br><br>
+  // weight: <input type="text" name="weight"><br><br>
+  // <input type="submit" value="Submit">
+  // /form>`);
+});
 
 app.get('/pokemon/:id', (request, response) => {
 
@@ -33,7 +57,8 @@ app.get('/pokemon/:id', (request, response) => {
 
       let currentPokemon = obj.pokemon[i];
 
-      if( currentPokemon.id === inputId ){
+      if( currentPokemon.id == inputId ){
+
         pokemon = currentPokemon;
       }
     }
@@ -50,10 +75,30 @@ app.get('/pokemon/:id', (request, response) => {
   });
 });
 
-app.get('/', (request, response) => {
-  response.send("yay");
-});
 
+
+  // jsonfile.writeFile(FILE, obj, function (err) {
+
+  //   if (err) console.error(err)
+  // })
+
+
+
+app.post('/pokemon', (request, response) => {
+  //debug code (output request body)
+  console.log(request.body);
+  jsonfile.readFile(FILE, (err, obj) => {
+  obj.pokemon.push(request.body);
+    // save the request body
+    jsonfile.writeFile(FILE, obj, {spaces:2}, (err) => {
+      console.error(err)
+      // response.render('home', FILE)
+      // // now look inside your json file
+      
+    });
+  });
+  response.send(request.body);
+});
 /**
  * ===================================
  * Listen to requests on port 3000
