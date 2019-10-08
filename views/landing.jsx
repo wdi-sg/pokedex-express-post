d75baa35ba73c80a56987ffa58993b51de774777
dannyozh <danielongzhihao@gmail.com>
2019-10-08 21:16:42 +0800

    simple landing page jsx

---
+++ views/landing.jsx
@@ -0,0 +1,17 @@
+var React = require('react');
+
+class Landing extends React.Component {
+  render() {
+    return (
+      <html>
+        <body>
+          <div>
+            <h1>Pokedex is loaded and ready...</h1>
+          </div>
+        </body>
+      </html>
+    );
+  }
+}
+
+module.exports = Landing;
