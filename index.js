524e7d50e6816e0bc3833e021cce1593b0b5ad2d
David M <davidmosk@gmail.com>
2019-10-08 19:05:30 +0800

    adds new pokemon from pokemon/new/ submit, with increments ID based on array length

--- index.js
+++ index.js
@@ -12,6 +12,8 @@ const FILE = 'pokedex.json';
 // Init express app
 const app = express();
 
+const writeData = {};
+
 // tell your app to use the module
 app.use(express.json());
 app.use(express.urlencoded({
@@ -34,7 +36,7 @@ app.set('view engine', 'jsx');
  * ===================================
  */
 
-app.get('/pokemon/:id', (request, response) => {
+app.get('/pokemon/id/:id', (request, response) => {
 
   // get json from specified file
   jsonfile.readFile(FILE, (err, obj) => {
@@ -73,6 +75,50 @@ app.get('/', (request, response) => {
   // giving home.jsx file an object/context with `name` as a property
   const data = {warning: ""};
   response.render('home', data);
+});
+
+
+app.get('/pokemon/new', (request, response) => {
+  // giving home.jsx file an object/context with `name` as a property
+  const data = {warning: ""};
+  response.render('home', data);
+});
+
+// if empty build the basic screen for input
+app.post('/pokemon', (request, response) => {
+    console.log('full body ', request.body);
+  // giving home.jsx file an object/context with `name` as a property
+  let data = {warning: ""};
+  if (request.body.name === "" || request.body.img === "" || request.body.height === "" || request.body.weight === "") {
+    data = {warning: "Empty name or other data..."}; 
+      } else  { 
+    data = {warning: "Pokemon Added!"}; 
+      }
+  response.render('home', data);
+  
+
+  // add the new data to the read object
+  // whats the current last ID?
+  jsonfile.readFile(FILE, (err, obj) => {
+  let newId = obj.pokemon.length;
+  newId++;
+  // console.log('current Obj:', obj)
+  console.log('new id no:', newId)
+  request.body.id = newId;
+  request.body.num= newId;
+  // console.log('body ', request.body);
+  // console.log('obj pokemon: ', obj["pokemon"]);
+  obj["pokemon"].push(request.body);
+    // console.log('new object:', obj)
+  // run the file write
+  // 
+  // save the request body
+  jsonfile.writeFile(FILE, obj, (err) => {
+    console.error(err)
+  });
+
+  });
+
 });
 
 // app.get('/', (request, response) => {
