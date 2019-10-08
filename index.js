f422ce5bccd5578a9e35cc31ea6398e2c891b35c
datguyrhy <54259724+datguyrhy@users.noreply.github.com>
2019-10-08 20:49:45 +0800

    added form & option to add new to newPokedex.json

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
+//jshint esversion:6
 const jsonfile = require('jsonfile');
 const express = require('express');
 const FILE = 'pokedex.json';
-const app = express()
+const newPokedex = 'newpokemon.json';
+const app = express();
 //react engine
 const reactEngine = require('express-react-views').createEngine();
 app.engine('jsx', reactEngine);
@@ -16,19 +18,6 @@ app.use(express.json());
 app.use(express.urlencoded({
   extended: true
 }));
-
-/**
- * ===================================
- * Configurations and set up
- * ===================================
- */
-
-// Init express app
-/**
- * ===================================
- * Routes
- * ===================================
- */
 
 app.get('/pokemon/:id', (request, response) => {
   jsonfile.readFile(FILE, (err, obj) => {
@@ -54,13 +43,30 @@ app.get('/pokemon/:id', (request, response) => {
     }
   });
 });
-
-app.get('/form', (req, res) => {
+//points user to form as homepage
+app.get('/pokemon/new', (req, res) => {
   console.log("hi");
-  res.render('form')
+  res.render('form');
+
 });
 
-///ccess the photos in the array
+//adds new pokemon to newpokedex.json(wanted to test the function out so i created a sepereate file)
+app.post('/pokemon',(request, response)=>{
+    console.log("EVERYTHING in the form request", request.body );
+    console.log(request.body);
+  jsonfile.writeFile('newPokedex.json', request.body, (err) => {
+    console.log(err);
+    response.send(request.body);
+  });
+});
+
+//function to capture form submisson
+
+// app.get('/',(request,respond)=>{
+//
+//
+// });
+// ///ccess the photos in the array
 // jsonfile.readFile(FILE, (err, obj) => {
 // app.get('/photos', (req, res) => {
 //   // loops through  obj
@@ -71,16 +77,16 @@ app.get('/form', (req, res) => {
 //
 // });
 ///////////////////////////////////////////////////////////////////
-app.get('/photos', (req, res) => {
-  // loops through  obj
-  for( let i=0; i<obj.pokemon.length; i++ ){
-    let currentPokemon = obj.pokemon[i].img;
-    }
-
-  });
+// app.get('/photos', (req, res) => {
+//   // loops through  obj
+//   for( let i=0; i<obj.pokemon.length; i++ ){
+//     let currentPokemon = obj.pokemon[i].img;
+//     }
+//
+//   });
 /**
  * ===================================
  * Listen to requests on port 3000
  * ===================================
  */
-app.listen(3535, () => console.log('~~~ Tuning in to the waves of port 3000 ~~~'));
+app.listen(3000, () => console.log('~~~ Tuning in to the waves of port 3000 ~~~'));
