2c8e4b62b6c5e1a7282f1299bf07b0f9f5901721
reshinto <terencekong2002@gmail.com>
2019-10-08 18:30:23 +0800

    initial commit

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
-const express = require('express');
-const jsonfile = require('jsonfile');
+const express = require("express");
+const jsonfile = require("jsonfile");
+const bodyParser = require("body-parser");
 
-const FILE = 'pokedex.json';
+const FILE = "pokedex.json";
 
 /**
  * ===================================
@@ -11,6 +12,12 @@ const FILE = 'pokedex.json';
 
 // Init express app
 const app = express();
+const reactEngine = require("express-react-views").createEngine();
+app.engine("jsx", reactEngine);
+app.set("views", __dirname + "/views");
+app.set("view engine", "jsx");
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 
 /**
  * ===================================
@@ -18,39 +25,58 @@ const app = express();
  * ===================================
  */
 
-app.get('/pokemon/:id', (request, response) => {
+app.get("/pokemon/new", (req, res) => {
+  res.render("new");
+});
 
+app.post("/pokemon/new", (req, res) => {
+  Object.keys(req.body).forEach((value) => {
+    if (req.body[value] === "") {
+      res.send(`${value} cannot be empty.`);
+    }
+  });
+  jsonfile.readFile(FILE, (err, obj) => {
+    console.log(req.body);
+    req.body.id = parseInt(req.body.id);
+    obj.pokemon.push(req.body);
+    console.log(obj.pokemon[obj.pokemon.length-1]);
+    jsonfile.writeFile(FILE, obj, (err) => {
+      res.redirect(`/pokemon/${req.body.id}`);
+    });
+  });
+});
+
+app.get("/pokemon/:id", (request, response) => {
   // get json from specified file
   jsonfile.readFile(FILE, (err, obj) => {
     // obj is the object from the pokedex json file
     // extract input data from request
-    let inputId = parseInt( request.params.id );
+    const inputId = parseInt( request.params.id );
+    console.log(inputId);
 
-    var pokemon;
+    let pokemon;
 
     // find pokemon by id from the pokedex json file
-    for( let i=0; i<obj.pokemon.length; i++ ){
+    for ( let i=0; i<obj.pokemon.length; i++ ) {
+      const currentPokemon = obj.pokemon[i];
 
-      let currentPokemon = obj.pokemon[i];
-
-      if( currentPokemon.id === inputId ){
+      if ( currentPokemon.id === inputId ) {
         pokemon = currentPokemon;
       }
     }
 
     if (pokemon === undefined) {
-
       // send 404 back
       response.status(404);
       response.send("not found");
     } else {
-
       response.send(pokemon);
     }
   });
 });
 
-app.get('/', (request, response) => {
+
+app.get("/", (request, response) => {
   response.send("yay");
 });
 
@@ -59,4 +85,4 @@ app.get('/', (request, response) => {
  * Listen to requests on port 3000
  * ===================================
  */
-app.listen(3000, () => console.log('~~~ Tuning in to the waves of port 3000 ~~~'));
+app.listen(3000, () => console.log("~~~ Tuning in to the waves of port http://localhost:3000 ~~~"));
---
+++ views/new.jsx
@@ -0,0 +1,34 @@
+const React = require("react");
+
+class New extends React.Component {
+  render() {
+    return (
+      <html>
+        <body>
+          <div>
+            <h1>New Pokemon Registration</h1>
+            <form action="/pokemon/new" method="post" id="new">
+                <label htmlFor="id">ID: </label>
+                <input type="number" name="id" />
+                <label htmlFor="num">Number: </label>
+                <input type="number" name="num" />
+                <label htmlFor="name">Name: </label>
+                <input type="text" name="name" />
+                <label htmlFor="img">Image: </label>
+                <input type="text" name="img" />
+                <label htmlFor="height">Height: </label>
+                <input type="text" name="height" />
+                <label htmlFor="weight">Weight: </label>
+                <input type="text" name="weight" />
+            </form>
+            <button type="submit" form="new" value="submit">
+                Submit
+            </button>
+          </div>
+        </body>
+      </html>
+    )
+  }
+}
+
+module.exports = New;
