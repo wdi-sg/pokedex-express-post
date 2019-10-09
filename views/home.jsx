9f2e31278d31ff792546f67d0a1eb56dea24a381
David M <davidmosk@gmail.com>
2019-10-09 08:56:56 +0800

    added link to new

--- views/home.jsx
+++ views/home.jsx
@@ -8,7 +8,7 @@ class Home extends React.Component {
                   { this.props.warning }
 
             <h1>Would you like to:</h1>
-            <a href="">Add a New Pokemon?</a><br/><br/>
+            <a href="http://localhost:3000/pokemon/">Add a New Pokemon?</a><br/><br/>
             Or Search for a Pokemon by Number?<br/>
                         <form method="Get" action="http://localhost:3000/pokemon/id">
             ID: <input type="text" name="id"/>
