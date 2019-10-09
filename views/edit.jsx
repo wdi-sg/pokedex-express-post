35ee772d446341c6069229eca5d5601238d60f78
David M <davidmosk@gmail.com>
2019-10-09 17:11:55 +0800

    add edit html template

---
+++ views/edit.jsx
@@ -0,0 +1,31 @@
+var React = require('react');
+class New extends React.Component {
+  render() {
+    return (
+      <html>
+        <body>
+                  <div>
+            
+            <h1>Edit Pokemon: { this.props.name }</h1>
+            <form method="POST" action={"/pokemon/"+this.props.id+"?_method=put"}>
+            <br/>
+            Name: <input type="text" name="name" value={ this.props.name }/>
+            <br/>
+            Image URL: <input type="text" name="img" value={ this.props.img }/>
+            <br/>
+            Height: <input type="text" name="height" value={ this.props.height }/>
+            <br/>
+            Weight: <input type="text" name="weight" value={ this.props.weight }/>
+            <br/>
+            <input type="submit" value="Submit"/>
+            </form>
+
+
+          </div>
+        </body>
+      </html>
+    );
+  }
+}
+
+module.exports = New;
