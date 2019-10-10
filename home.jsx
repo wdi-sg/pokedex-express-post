69bf3a9f9fe487546962fe8107c079469c77b9d3
datguyrhy <54259724+datguyrhy@users.noreply.github.com>
2019-10-11 00:00:34 +0800

    Add files via upload

---
+++ home.jsx
@@ -0,0 +1,31 @@
+const React = require('react');
+
+class Home extends React.Component {
+    render(){
+        return(
+            <html>
+                <body>
+                    <div>
+                        <h1>Welcome to the EDEN's SUPER AMAZING POKEDEX V3000</h1>
+                        <p>Choose the following: </p>
+                        <form method="GET" action="/pokemon/new">
+                            <input type="submit" value="Add pokemon"/>
+                        </form>
+                        <form method="GET" action="/pokemon/show">
+                            <input type="submit" value="Show all pokemon"/>
+                        </form>
+                        <form method="GET" action="/pokemon/sort">
+                            <select name="path">
+                                <option value="height">Max Height</option>
+                                <option value="weight">Max Weight</option>
+                            </select>
+                            <input type="submit"/>
+                        </form>
+                    </div>
+                </body>
+            </html>
+        );
+    }
+}
+
+module.exports = Home;
