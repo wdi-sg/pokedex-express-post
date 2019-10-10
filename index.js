7fc17ec2eb9963dd4d5439e6178ebd7bf0141a8e
SYNG <simyenng@gmail.com>
2019-10-10 20:20:27 +0800

    Fixed sort by name

--- index.js
+++ index.js
@@ -148,7 +148,13 @@ app.get('/pokemon?', (request, response) => {
                     results.push({ name : obj.pokemon[i].name, img : obj.pokemon[i].img });
                 }
                 // sort names alphabetically
-                results.sort();
+                results.sort((a,b) => {
+                    var x = a.name.toLowerCase();
+                    var y = b.name.toLowerCase();
+                    if (x < y) {return -1;}
+                    if (x > y) {return 1;}
+                    return 0;
+                  });
                 let pokeobj = { "pokemon": results };
                 // render out requested data
                 response.render('home', pokeobj);
--- views/home.jsx
+++ views/home.jsx
@@ -1,9 +1,6 @@
 var React = require('react');
 
 class Home extends React.Component {
-
-
-
     render() {
         const allPokemon = this.props.pokemon.map(pokemon => {
             let formAction = '/pokemon/' + pokemon.id;
