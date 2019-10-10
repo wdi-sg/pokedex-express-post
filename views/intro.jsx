f19ffea97c72b7981136b4b658e114ad6f8c0821
“kellylimmm” <kellylim.06@gmail.com>
2019-10-10 16:24:36 +0800

    kelly's changes

--- views/intro.jsx
+++ views/intro.jsx
@@ -5,7 +5,7 @@ class intro extends React.Component {
       <html>
         <body>
           <div>
-            <h1> Welcome to Online</h1>
+            <h1> Welcome to Ryan's World</h1>
             <img src="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/62902855-31e8-48de-986e-5080e8ef5f15/d5uxsvu-cbf56dfe-0c82-40f9-928b-1e756acf0236.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzYyOTAyODU1LTMxZTgtNDhkZS05ODZlLTUwODBlOGVmNWYxNVwvZDV1eHN2dS1jYmY1NmRmZS0wYzgyLTQwZjktOTI4Yi0xZTc1NmFjZjAyMzYucG5nIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.RLOne-7vDfGOxnvtsM9XNeF--mrcHZuILO_TVhx4GrU"/>
           </div>
         </body>
