dbdd1973354a79c252803523bd3963c484381d97
dannyozh <danielongzhihao@gmail.com>
2019-10-10 22:42:47 +0800

    added pokemon images to sort lists!

--- views/sorted.jsx
+++ views/sorted.jsx
@@ -7,7 +7,9 @@ class Home extends React.Component {
         <body>
             <h1>Pokedex sort by {this.props.sortParam}</h1>
             <ul>{this.props.pokemonSorted.map(pokemon => (
+                // react.fragment that functions as div, but doesn't display on page
                 <React.Fragment>
+                    <li><img src={pokemon.img}/></li>
                     <li>Pokemon: {pokemon.name}</li>
                     <li>Weight: {pokemon.weight}</li>
                     <li>Height: {pokemon.height}</li>
