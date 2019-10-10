ed1abc3b9b3b162f6eb80c7c0727212af2333d33
dannyozh <danielongzhihao@gmail.com>
2019-10-10 23:37:51 +0800

    added bootstrap tables to display sorted pokemon

--- views/sorted.jsx
+++ views/sorted.jsx
@@ -4,20 +4,33 @@ class Home extends React.Component {
   render() {
     return (
       <html>
+      <head>
+      <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous"/>
+      </head>
         <body>
             <h1>Pokedex sort by {this.props.sortParam}</h1>
-            <ul>{this.props.pokemonSorted.map(pokemon => (
+            {this.props.pokemonSorted.map(pokemon => (
                 // react.fragment that functions as div, but doesn't display on page
                 <React.Fragment>
-                    <li><img src={pokemon.img}/></li>
-                    <li>Pokemon: {pokemon.name}</li>
-                    <li>Weight: {pokemon.weight}</li>
-                    <li>Height: {pokemon.height}</li>
-                    <hr/>
+                <div class="container">
+                    <table class="table table-striped table-bordered table-hover table-condensed">
+                        <tr>
+                            <th>Image</th>
+                            <th>Name</th>
+                            <th>Weight</th>
+                            <th>Height</th>
+                        </tr>
+                        <tr>
+                            <td><img src={pokemon.img}/></td>
+                            <td>{pokemon.name}</td>
+                            <td>{pokemon.weight}</td>
+                            <td>{pokemon.height}</td>
+                        </tr>
+                    </table>
+                </div>
                 </React.Fragment>
             )
             )}
-            </ul>
         </body>
       </html>
     );
