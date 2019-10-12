0596e8203a55495d69ff3ed98015f04570dcbf69
syahirah <syahirah.suradi@gmail.com>
2019-10-12 15:34:50 +0800

    fixed post function from overwriting json file

--- index.js
+++ index.js
@@ -32,8 +32,30 @@ app.use(methodOverride('_method'));
  * ===================================
  */
 
-app.get('/pokemon/:id', (request, response) => {
+//////////RENDERS NEW POKEMON FORM/////////////
+app.get('/pokemon/new', (request, response) => {
+    response.render('Form');
+});
 
+//////////PAGE SHOWS NEW POKEMON CREATED//////////
+app.post('/pokemon', (request, response) => {
+    console.log(request.body);
+    let newPokemon = request.body
+
+    jsonfile.readFile(FILE, (err, obj) => {
+        obj.pokemon.push(newPokemon);
+
+    jsonfile.writeFile('pokedex.json', obj, (err) => {
+        console.error(err)
+
+        // now look inside your json file
+        response.send(request.body);
+    });
+    });
+});
+
+////////SHOWS POKEMON ID PAGE////////////////
+app.get('/pokemon/:id', (request, response) => {
   // get json from specified file
   jsonfile.readFile(FILE, (err, obj) => {
     // obj is the object from the pokedex json file
@@ -60,66 +82,53 @@ app.get('/pokemon/:id', (request, response) => {
   });
 });
 
-app.get('/pokemon/new', (request, response) => {
-    response.render('Form');
-});
-
-app.post('/pokemon', (request, response) => {
-    console.log(request.body);
-
-    jsonfile.readFile(FILE, (err, obj) => {
-        obj.pokemon.push(request.body);
-
-    jsonfile.writeFile('pokedex.json', request.body, (err) => {
-        console.error(err)
-
-        // now look inside your json file
-        response.send(request.body);
-    });
-    });
-});
-
+//////////SHOWS FORM TO EDIT POKEMON////////////
 app.get('/pokemon/:id/edit', (request, response) => {
 
-  // get json from specified file
-  jsonfile.readFile(FILE, (err, obj) => {
-    // obj is the object from the pokedex json file
-    // extract input data from request
     let inputId = parseInt( request.params.id );
-
-    var pokemon;
+  // get json from specified file
+    jsonfile.readFile(FILE, (err, obj) => {
+        var pokemon;
 
     // find pokemon by id from the pokedex json file
-    for( let i=0; i<obj.pokemon.length; i++ ){
-      let currentPokemon = obj.pokemon[i];
-      if( currentPokemon.id === inputId ){
-        pokemon = currentPokemon;
+        for( let i=0; i<obj.pokemon.length; i++ ){
+         let currentPokemon = obj.pokemon[i];
+         if( currentPokemon.id === inputId ){
+         pokemon = currentPokemon;
       }
     }
 
-    if (pokemon === undefined) {
-      // send 404 back
-      response.status(404);
-      response.send("not found");
-    } else {
       const pokemonData = pokemon;
-      response.render('EditPokemon', pokemonData);
-    }
-  });
+      response.render('Edit', pokemonData);
+    });
 });
 
+///////////PAGE SHOWS EDITED POKEMON PAGE//////////
 app.put("/pokemon/:id", (request, response) => {
   //read the file in and write out to it
+    let identity = request.params.id;
+    let index = identity - 1;
+    let edit = request.body;
+
     jsonfile.readFile(FILE, (err, obj) => {
-        obj.pokemon.push(request.body);
+        edit.id = parseInt(edit.id);
+        obj.pokemon[index] = edit;
+        let currentPokemon = obj.pokemon[index];
 
-    jsonfile.writeFile('pokedex.json', request.body, (err) => {
-        console.error(err)
+    const data = {
+        pokedex: obj.pokemon,
+        individual: obj.pokemon[index],
+        id: obj.pokemon[index].id
+        };
 
-        response.send(request.body);
-    });
+    response.send(currentPokemon);
+
+    jsonfile.writeFile(FILE, obj, { spaces: 2 }, (err) => {
+            console.error(err)
+        });
     });
 });
+
 /**
  * ===================================
  * Listen to requests on port 3000
---
+++ views/Edit.jsx
@@ -0,0 +1,25 @@
+var React = require('react');
+class Edit extends React.Component {
+  render() {
+    return (
+      <html>
+        <body>
+          <div>
+            <h1>Pokemon Data</h1>
+            <form method="POST" action= {"/pokemon/" + this.props.id + "?_method=put"}>
+            ID: <input type="text" name="id" value={this.props.id}/><br />
+            Num: <input type="text" name="num" value={this.props.num}/><br />
+            Name: <input type="text" name="name" value={this.props.name}/><br />
+            Img: <input type="text" name="img" value={this.props.img}/><br />
+            Height: <input type="text" name="height" value={this.props.height}/><br />
+            Weight: <input type="text" name="weight" value={this.props.weight}/><br />
+            <input type="submit" value="Submit"/>
+            </form>
+          </div>
+        </body>
+      </html>
+    );
+  }
+}
+
+module.exports = Edit;
