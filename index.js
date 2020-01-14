efe92d8a1a63857b4c58d889b8f3f9f9ef36c5ab
rachellesg <rachelle.yeo@outlook.com>
2020-01-14 16:37:14 +0800

    finished deliverables

--- index.js
+++ index.js
@@ -33,21 +33,42 @@ app.set('view engine', 'jsx');
  * ===================================
  */
 
-// <br> Number:<input type="text" name="num"/><br>Image URL:<input type="text" type="text" name="img"/><br> Height:<input type="text" name="height"/><br> Weight:<input name="weight"/><br>
-
 app.get('/pokemon/new', (request, response) => {
   // id, num, name, img, height, weight
-  let myForm = '<html><body><form action="/pokemon" method="POST">Pokemon Name: <input type="text" name="name"/><br> ID:<input type="text" name="id"/><input type="submit"/></form></body></html>';
+  let myForm = '<html><body><form action="/pokemon" method="POST">Pokemon Name: <input type="text" name="name"/><br> ID:<input type="text" name="id"/><br> Number:<input type="text" name="num"/><br>Image URL:<input type="text" type="text" name="img"/><br> Height:<input type="text" name="height"/><br> Weight:<input name="weight"/><br><input type="submit"/></form></body></html>';
   response.send(myForm);
 });
 
 app.post('/pokemon', (request,response) => {
+  let contents = request.body;
+
+  let newPokemonName = request.body.name;
+  let newPokemonId = parseInt(request.body.id);
+  let newPokemonNum = parseInt(request.body.num);
+  let newPokemonImgUrl = request.body.img;
+  let newPokemonHeight = request.body.height;
+  let newPokemonWeight = request.body.weight;
+  let data = {
+    "id": newPokemonId,
+    "num": newPokemonNum,
+    "name": newPokemonName,
+    "img": newPokemonImgUrl,
+    "height": newPokemonHeight,
+    "weight": newPokemonWeight,
+  }
+
   jsonfile.readFile(FILE, (err, obj) => {
     // save the request body
+    console.log(data);
+    response.send(data);
+    obj.pokemon.push(data);
 
-    let contents = request.body;
-    console.log(contents);
-    response.send(contents);
+    jsonfile.writeFile(FILE, obj, err => {
+      console.error(err)
+      // now look inside your json file
+      // response.send(request.body);
+      response.send('WOW WORKSS!!!');
+    });
   });
 })
 
