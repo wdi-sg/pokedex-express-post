b680bcbfe5fc582cda3c26d404c164c035990311
joyce-paul <joyce_paul@hotmail.com>
2020-01-14 21:26:04 +0800

    Completed the deliverables.

--- index.js
+++ index.js
@@ -12,23 +12,50 @@ const FILE = 'pokedex.json';
 // Init express app
 const app = express();
 
+
+// tell your app to use the module
+app.use(express.json());
+app.use(express.urlencoded({
+  extended: true
+}));
+
+
+
 /**
  * ===================================
  * Routes
  * ===================================
  */
 
-app.get('/pokemon/:id', (request, response) => {
+
+/*app.post('/pokemon', function(request, response) {
+
+  //debug code (output request body)
+  console.log(request.body);
+
+
+  // save the request body
+  jsonfile.writeFile('data.json', request.body, (err) => {
+    console.error(err)
+
+    // now look inside your json file
+    response.send(request.body);
+  });
+});*/
+
+
+
+app.get('/pokemon/new', (request, response) => {
 
   // get json from specified file
   jsonfile.readFile(FILE, (err, obj) => {
-    
+
     // check to make sure the file was properly read
     if( err ){
-      
+
       console.log("error with json read file:",err);
-      response.status(503).send("error reading filee");
-      return; 
+      response.status(503).send("error reading file");
+      return;
     }
     // obj is the object from the pokedex json file
     // extract input data from request
@@ -61,6 +88,98 @@ app.get('/pokemon/:id', (request, response) => {
 app.get('/', (request, response) => {
   response.send("yay");
 });
+
+
+// Deliverables:
+// Expose a new endpoint that intercepts GET requests to /pokemon/new, which responds with a HTML page with a form that has these fields: id, num, name, img, height, and weight
+//Point the form to submit data to the (/pokemon) route using POST method
+
+
+
+/*app.get('/pokemon/', (request, response) => {
+
+  let myForm  ='<form method="POST" action="/pokemon">id:<input type="text" name="name"><br/>num:<input type="text" name="name"><br/>name:<input type="text" name="name"><br/>img:<input type="text" name="name"><br/>height:<input type="text" name="name"><br/>weight:<input type="text" name="name"></br><input type="submit" value="Submit"></form>';
+
+  response.send(myForm);
+
+});*/
+
+
+
+// Expose a new endpoint that accepts POST requests to /pokemon, which parses the form data and saves the new pokemon data into pokedex.json
+
+
+/*app.post('/pokemon', (request, response)=>{
+  console.log("HEY POST REQUEST", request.body)
+  console.log('*******');
+
+  jsonfile.readFile(file, (err, obj) => {
+    // save the request body
+
+    let contents = request.body.stuff;
+
+    obj.pokemon.push( contents );
+
+    jsonfile.writeFile('/pokedex.json', obj, (err) => {
+      console.error(err)
+
+      // now look inside your json file
+      // response.send(request.body);
+      response.send('WOW WORKSS!!!');
+    });
+  });
+})*/
+
+
+
+app.post('/pokemon/', (request, response) => {
+    console.log('Received POST');
+    console.log(request.body);
+    const newPokemon = {
+        id: request.body.id,
+        num: request.body.num,
+        name: request.body.name,
+        img: request.body.img,
+        height: request.body.height,
+        weight: request.body.weight
+    }
+
+    jsonfile.readFile(FILE, (err, obj) => {
+
+        // check to make sure the file was properly read
+        if (err) {
+
+            console.log("There is an error", err);
+            response.status(503).send("Error");
+            return;
+        }
+
+        obj.pokemon.push(newPokemon);
+
+        jsonfile.writeFile(FILE, obj, (err) => {
+            if (err) console.log(err);
+            console.log('New ' + newPokemon);
+            response.send('The following pokemon related information has been added: ' + newPokemon.name);
+            return;
+        })
+    })
+})
+
+
+app.get('/pokemon/', (request, response) => {
+
+  let myForm  ='<form method="POST" action="/pokemon">id:<input type="text" name="id"><br/>num:<input type="text" name="num"><br/>name:<input type="text" name="name"><br/>img:<input type="text" name="img"><br/>height:<input type="text" name="height"><br/>weight:<input type="text" name="weight"></br><input type="submit" value="Submit"></form>';
+
+  response.send(myForm);
+
+});
+
+
+
+
+
+
+
 
 /**
  * ===================================
