70e29c1556e03e3b03e630850bdd8b4fa73900a9
rachellesg <rachelle.yeo@outlook.com>
2020-01-14 16:21:30 +0800

    initial commit

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const jsonfile = require('jsonfile');
 
+// Init express app
+const app = express();
 const FILE = 'pokedex.json';
 
 /**
@@ -9,8 +11,21 @@ const FILE = 'pokedex.json';
  * ===================================
  */
 
-// Init express app
-const app = express();
+// tell your app to use the module
+app.use(express.json());
+app.use(express.urlencoded({
+  extended: true
+}));
+
+// this line below, sets a layout look to your express project
+const reactEngine = require('express-react-views').createEngine();
+app.engine('jsx', reactEngine);
+
+// this tells express where to look for the view files
+app.set('views', __dirname + '/views');
+
+// this line sets react to be the default view engine
+app.set('view engine', 'jsx');
 
 /**
  * ===================================
@@ -18,17 +33,35 @@ const app = express();
  * ===================================
  */
 
+// <br> Number:<input type="text" name="num"/><br>Image URL:<input type="text" type="text" name="img"/><br> Height:<input type="text" name="height"/><br> Weight:<input name="weight"/><br>
+
+app.get('/pokemon/new', (request, response) => {
+  // id, num, name, img, height, weight
+  let myForm = '<html><body><form action="/pokemon" method="POST">Pokemon Name: <input type="text" name="name"/><br> ID:<input type="text" name="id"/><input type="submit"/></form></body></html>';
+  response.send(myForm);
+});
+
+app.post('/pokemon', (request,response) => {
+  jsonfile.readFile(FILE, (err, obj) => {
+    // save the request body
+
+    let contents = request.body;
+    console.log(contents);
+    response.send(contents);
+  });
+})
+
 app.get('/pokemon/:id', (request, response) => {
 
   // get json from specified file
   jsonfile.readFile(FILE, (err, obj) => {
-    
+
     // check to make sure the file was properly read
     if( err ){
-      
+
       console.log("error with json read file:",err);
       response.status(503).send("error reading filee");
-      return; 
+      return;
     }
     // obj is the object from the pokedex json file
     // extract input data from request
