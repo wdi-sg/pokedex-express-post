28c3a7d88344c3c4fd159a8dd6a3e088ff8fc90f
weizheng1910 <weizheng1910@gmail.com>
2020-01-15 23:29:49 +0800

    part 2 deliverables

--- views/deletePokemon.jsx
+++ views/deletePokemon.jsx
@@ -1,58 +1,63 @@
 var React = require('react');
 
+
 class showPokemon extends React.Component {
     render() {
+        let formUrl = "/pokemon/"+this.props.theId+"?_method=delete"
+
         return (
             <html>
                 <body>
                     <div>
-                        <h1>{this.props.name}</h1>
-                        <img src={this.props.img} alt="image" />
+                        <h1>{this.props.thePokemon.name}</h1>
+                        <img src={this.props.thePokemon.img} alt="image" />
                     </div>
                     {/*Start Table*/}
                     <table class="table">
                         <tbody>
                             <tr>
                                 <th>ID</th>
-                                <td>{this.props.id}</td>
+                                <td>{this.props.thePokemon.id}</td>
                             </tr>
                             <tr>
                                 <th>Height</th>
-                                <td>{this.props.height}</td>
+                                <td>{this.props.thePokemon.height}</td>
                             </tr>
 
                             <tr>
                                 <th>Weight</th>
-                                <td>{this.props.weight}</td>
+                                <td>{this.props.thePokemon.weight}</td>
                             </tr>
 
                             <tr>
                                 <th>Candy</th>
-                                <td>{this.props.candy}</td>
+                                <td>{this.props.thePokemon.candy}</td>
                             </tr>
 
                             <tr>
                                 <th>Egg</th>
-                                <td>{this.props.egg}</td>
+                                <td>{this.props.thePokemon.egg}</td>
                             </tr>
 
                             <tr>
                                 <th>Average Spawn</th>
-                                <td>{this.props.avg_spawns}</td>
+                                <td>{this.props.thePokemon.avg_spawns}</td>
                             </tr>
 
                             <tr>
                                 <th>Spawn Time</th>
-                                <td>{this.props.spawn_time}</td>
+                                <td>{this.props.thePokemon.spawn_time}</td>
                             </tr>
                         </tbody>
                     </table>
-
-
+                    <form action={formUrl} method="POST">
+                        <button type="submit" className="btn btn-primary">Delete Pokemon</button>
+                    </form>
                 </body>
             </html>
         );
     }
 }
+
 
 module.exports = showPokemon;
