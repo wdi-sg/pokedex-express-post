06b984a05e6b29233ce1995151815666c2f32433
SafrazHakamali <safrazhakamali@gmail.com>
2020-01-15 09:23:11 +0800

    Just got teh for to submit. There was a syntax typo that Nat helped me resolve

--- index.js
+++ index.js
@@ -1,70 +1,21 @@
 const express = require('express');
 const jsonfile = require('jsonfile');
-
-const FILE = 'pokedex.json';
-
-/**
- * ===================================
- * Configurations and set up
- * ===================================
- */
-
-// Init express app
+const file = 'pokedex.json';
 const app = express();
 
-/**
- * ===================================
- * Routes
- * ===================================
- */
+app.use(express.json());
+app.use(express.urlencoded({
+  extended: true
+}));
 
-app.get('/pokemon/:id', (request, response) => {
+app.post('/pokemon', (request, response) => {
+  console.log("POSASJHDBASJHDB")
+  console.log(request.body);
+})
 
-  // get json from specified file
-  jsonfile.readFile(FILE, (err, obj) => {
-    
-    // check to make sure the file was properly read
-    if( err ){
-      
-      console.log("error with json read file:",err);
-      response.status(503).send("error reading filee");
-      return; 
-    }
-    // obj is the object from the pokedex json file
-    // extract input data from request
-    let inputId = parseInt( request.params.id );
-
-    var pokemon;
-
-    // find pokemon by id from the pokedex json file
-    for( let i=0; i<obj.pokemon.length; i++ ){
-
-      let currentPokemon = obj.pokemon[i];
-
-      if( currentPokemon.id === inputId ){
-        pokemon = currentPokemon;
-      }
-    }
-
-    if (pokemon === undefined) {
-
-      // send 404 back
-      response.status(404);
-      response.send("not found");
-    } else {
-
-      response.send(pokemon);
-    }
-  });
+app.get('/pokemon/new', (request,response)=> {
+  console.log("LOADED")
+  response.send(`<form method="POST" action="/pokemon">ID:<input type="text" name="id"><input type="submit" value="submit"></form>`);
 });
 
-app.get('/', (request, response) => {
-  response.send("yay");
-});
-
-/**
- * ===================================
- * Listen to requests on port 3000
- * ===================================
- */
 app.listen(3000, () => console.log('~~~ Tuning in to the waves of port 3000 ~~~'));
