3448df06547f8ee0222aa2d7b93be497e30fe17e
“hweechieh” <hweechieh@gmail.com>
2020-01-15 09:57:34 +0800

    Attempting on Part 1

--- index.js
+++ index.js
@@ -1,70 +1,85 @@
-const express = require('express');
-const jsonfile = require('jsonfile');
-
-const FILE = 'pokedex.json';
-
 /**
  * ===================================
  * Configurations and set up
  * ===================================
  */
+const express = require('express');
+const jsonfile = require('jsonfile');
+const FILE = 'pokedex.json';
+
+
+// this line below, sets a layout look to your express project
+const reactEngine = require('express-react-views').createEngine();
+app.engine('jsx', reactEngine);
+
+// this tells express where to look for the view files
+app.set('views', __dirname + '/views');
+
+// this line sets react to be the default view engine
+app.set('view engine', 'jsx');
+
+app.get('/', (req, res) => {
+  // running this will let express to run home.handlebars file in your views folder
+  res.render('home')
+})
+
 
 // Init express app
-const app = express();
+// const app = express();
 
-/**
- * ===================================
- * Routes
- * ===================================
- */
+// /**
+//  * ===================================
+//  * Routes
+//  * ===================================
+//  */
 
-app.get('/pokemon/:id', (request, response) => {
+// app.get('/pokemon/:id', (request, response) => {
 
-  // get json from specified file
-  jsonfile.readFile(FILE, (err, obj) => {
-    
-    // check to make sure the file was properly read
-    if( err ){
-      
-      console.log("error with json read file:",err);
-      response.status(503).send("error reading filee");
-      return; 
-    }
-    // obj is the object from the pokedex json file
-    // extract input data from request
-    let inputId = parseInt( request.params.id );
+//   // get json from specified file
+//   jsonfile.readFile(FILE, (err, obj) => {
 
-    var pokemon;
+//     // check to make sure the file was properly read
+//     if( err ){
 
-    // find pokemon by id from the pokedex json file
-    for( let i=0; i<obj.pokemon.length; i++ ){
+//       console.log("error with json read file:",err);
+//       response.status(503).send("error reading filee");
+//       return;
+//     }
+//     // obj is the object from the pokedex json file
+//     // extract input data from request
+//     let inputId = parseInt( request.params.id );
 
-      let currentPokemon = obj.pokemon[i];
+//     var pokemon;
 
-      if( currentPokemon.id === inputId ){
-        pokemon = currentPokemon;
-      }
-    }
+//     // find pokemon by id from the pokedex json file
+//     for( let i=0; i<obj.pokemon.length; i++ ){
 
-    if (pokemon === undefined) {
+//       let currentPokemon = obj.pokemon[i];
 
-      // send 404 back
-      response.status(404);
-      response.send("not found");
-    } else {
+//       if( currentPokemon.id === inputId ){
+//         pokemon = currentPokemon;
+//       }
+//     }
 
-      response.send(pokemon);
-    }
-  });
-});
+//     if (pokemon === undefined) {
 
-app.get('/', (request, response) => {
-  response.send("yay");
-});
+//       // send 404 back
+//       response.status(404);
+//       response.send("not found");
+//     } else {
 
-/**
- * ===================================
- * Listen to requests on port 3000
- * ===================================
- */
-app.listen(3000, () => console.log('~~~ Tuning in to the waves of port 3000 ~~~'));
+//       response.send(pokemon);
+//     }
+//   });
+// });
+
+// app.get('/', (request, response) => {
+//   response.send("yay");
+// });
+
+// /**
+//  * ===================================
+//  * Listen to requests on port 3000
+//  * ===================================
+//  */
+// app.listen(3000, () => console.log('~~~ Tuning in to the waves of port 3000 ~~~'));
