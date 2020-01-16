8903b25c7ad8fb66aad071a2663bb551faf9757e
joyce-paul <joyce_paul@hotmail.com>
2020-01-16 09:36:46 +0800

    Added each field as an input and pre-populated the current data for the pokemon

--- index.js
+++ index.js
@@ -82,24 +82,46 @@ app.get('/pokemon/new', (request, response) => {
   let myForm ='<form method="POST" action="/pokemon">id:<input type="text" name="id"><br/>num:<input type="text" name="num"><br/>name:<input type="text" name="name"><br/>img:<input type="text" name="img"><br/>height:<input type="text" name="height"><br/>weight:<input type="text" name="weight"></br><input type="submit" value="Submit"></form>';
 
   response.send(myForm);
-
 });
+
 
 
 /// ADD A FORM AT THE PATH: /pokemon/:id/edit \\\
 app.get('/pokemon/:id/edit',(request, response) => {
     let index = request.params.id
+
     jsonfile.readFile(FILE, (err, obj) => {
+
         console.log(obj)
+
         let pokemon = obj.pokemon[index];
-        let html = '<form method="POST" action="/putrequest?_method=put"><input name="id" type="text" value="Id"/><input type="submit" value="Edit this"/><br/><input name="number" type="text" value="Num"/><input type="submit" value="Edit this"/><br/><input name="id" type="text" value="Name"/><input type="submit" value="Edit this"/><br/><input name="id" type="text" value="Img"/><input type="submit" value="Edit this"/><br/><input name="id" type="text" value="Height"/><input type="submit" value="Edit this"/><br/><input name="id" type="text" value="Weight"/><input type="submit" value="Edit this"/></form>';
-      response.send(html);
+
+        const data = {
+            name: pokemon,
+        };
+        response.render('edit', data);
     })
-});
+})
+
+
+      /*  let html = '<form method="POST" action="/putrequest?_method=put"><input name="id" type="text" value="Id"/><input type="submit" value="Edit this"/><br/><input name="number" type="text" value="Num"/><input type="submit" value="Edit this"/><br/><input name="id" type="text" value="Name"/><input type="submit" value="Edit this"/><br/><input name="id" type="text" value="Img"/><input type="submit" value="Edit this"/><br/><input name="id" type="text" value="Height"/><input type="submit" value="Edit this"/><br/><input name="id" type="text" value="Weight"/><input type="submit" value="Edit this"/></form>';*/
+
+
+
+
+
+
+
+
+
+
+
+
 
 ///// THE FORM SHOULD MAKE A REQUEST (THE FORM ACTION ) TO THE CORRECT ROUTE ( A PUT REQUEST TO  /pokemon/:id) \\\\\
-/*app.put('/pokemon/:id' , (request, response) => {
-    let id = request.body.change;
+app.put('/pokemon/:id' , (request, response) => {
+    let id = request.params.id;
+
     jsonfile.readFile(FILE, (err, obj) => {
         obj.names.splice(id, 1);
 
@@ -108,7 +130,7 @@ app.get('/pokemon/:id/edit',(request, response) => {
         response.send('yes');
   })
   //read the file in and write out to it
-});*/
+});
 
 /**
  * ===================================
