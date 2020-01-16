617430ef3db902303ccbb5d66438c3301357795b
“hweechieh” <hweechieh@gmail.com>
2020-01-16 17:08:03 +0800

    Level One Completed

--- index.js
+++ index.js
@@ -1,85 +1,175 @@
-/**
- * ===================================
- * Configurations and set up
- * ===================================
- */
+//Configurations and set up
 const express = require('express');
 const jsonfile = require('jsonfile');
-const FILE = 'pokedex.json';
-
-
-// this line below, sets a layout look to your express project
-const reactEngine = require('express-react-views').createEngine();
-app.engine('jsx', reactEngine);
-
-// this tells express where to look for the view files
-app.set('views', __dirname + '/views');
-
-// this line sets react to be the default view engine
-app.set('view engine', 'jsx');
-
-app.get('/', (req, res) => {
-  // running this will let express to run home.handlebars file in your views folder
-  res.render('home')
-})
+const file = 'pokedex.json';
 
 
 // Init express app
-// const app = express();
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({
+    extended: true
+}));
 
-// /**
-//  * ===================================
-//  * Routes
-//  * ===================================
-//  */
 
-// app.get('/pokemon/:id', (request, response) => {
+// Init Method-Override for PUT and DELETE
+const methodOverride = require('method-override')
+app.use(methodOverride('_method'));
 
-//   // get json from specified file
-//   jsonfile.readFile(FILE, (err, obj) => {
+
+// Init REACT
+const reactEngine = require('express-react-views').createEngine();
+app.engine('jsx', reactEngine);
+// this tells express where to look for the view files
+app.set('views', __dirname + '/views');
+// this line sets react to be the default view engine
+app.set('view engine', 'jsx');
+
+
+
+//Routes
+
+////////////////// WHEN 'NEW' IS ADDED TO PATH AND NEW POKEMON CAN BE ADDED //////////////////
+app.post('/pokemon/', (request, response) => {
+    console.log('Received POST');
+    console.log(request.body);
+    const newPokemon = {
+        id: request.body.id,
+        num: request.body.num,
+        name: request.body.name,
+        img: request.body.img,
+        height: request.body.height,
+        weight: request.body.weight
+    }
+
+
+    jsonfile.readFile(file, (err, obj) => {
+
+        // check to make sure the file was properly read
+        if (err) {
+            console.log("error with json read file:", err);
+            response.status(503).send("error reading filee");
+            return;
+        }
+
+
+        obj.pokemon.push(newPokemon);
+
+
+        jsonfile.writeFile(file, obj, (err) => {
+            if (err) console.log(err);
+            console.log('New ' + newPokemon);
+            response.send('The following pokemon related information has been added: ' + newPokemon.name);
+            return;
+        })
+    })
+})
+
+app.get('/pokemon/new', (request, response) => {
+
+    let myForm = '<form method="POST" action="/pokemon">id:<input type="text" name="id"><br/>num:<input type="text" name="num"><br/>name:<input type="text" name="name"><br/>img:<input type="text" name="img"><br/>height:<input type="text" name="height"><br/>weight:<input type="text" name="weight"></br><input type="submit" value="Submit"></form>';
+
+    response.send(myForm)
+});
+
+
+
+
+////////////////// WHEN POKEMON NUMBER IS ADDED TO END OF PATH //////////////////
+app.get('/pokemon/:id', (request, response) => {
+
+    jsonfile.readFile(file, (err, obj) => {
+
+        // check to make sure the file was properly read
+        if (err) {
+            console.log("error with json read file:", err);
+            response.status(503).send("error reading filee");
+            return;
+        }
+
+        // obj is the object from the pokedex json file
+        // extract input data from request
+        let inputId = parseInt(request.params.id);
+        var pokemon;
+
+        // find pokemon by id from the pokedex json file
+        for (let i = 0; i < obj.pokemon.length; i++) {
+            let currentPokemon = obj.pokemon[i];
+            if (currentPokemon.id === inputId) {
+                pokemon = currentPokemon;
+            }
+        }
+
+        if (pokemon === undefined) {
+
+            // send 404 back
+            response.status(404);
+            response.send("not found");
+        } else {
+
+            response.send(pokemon);
+        }
+    });
+});
+
+
+// app.get('*', (request, response) => {
+//     response.send(`bwahahahahah we've ruined everything! - Team Rocket`);
+// });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const whenNewPokemonRequest = (request, response) => {
+
+//         jsonfile.readFile(file, (err, obj) => {
+//         request.path === "/pokemon/:new";
 
 //     // check to make sure the file was properly read
 //     if( err ){
-
-//       console.log("error with json read file:",err);
-//       response.status(503).send("error reading filee");
-//       return;
-//     }
-//     // obj is the object from the pokedex json file
-//     // extract input data from request
-//     let inputId = parseInt( request.params.id );
-
-//     var pokemon;
-
-//     // find pokemon by id from the pokedex json file
-//     for( let i=0; i<obj.pokemon.length; i++ ){
-
-//       let currentPokemon = obj.pokemon[i];
-
-//       if( currentPokemon.id === inputId ){
-//         pokemon = currentPokemon;
-//       }
+//       console.log("error with json read file:", err);
+//       response.status(503).send("error reading file");
+//     return;
 //     }
 
-//     if (pokemon === undefined) {
+//     let html =
+//     '<form method="POST" action="/putrequest?_method=put">'+
+//       '<input name="id" value="'+obj.name+'"/>'+
+//       '<input name="number" value="'+obj.name +'"/>'+
+//       '<input name="name" value="'+ obj.name+'"/>'+
+//       '<input name="image" value="'+ obj.name+'"/>'+
+//       '<input name="height" value="'+ obj.name+'"/>'+
+//       '<input name="weight" value="'+ obj.name+'"/>'+
+//       '<input type="submit" value="submit"/>'+
+//     '</form>';
 
-//       // send 404 back
-//       response.status(404);
-//       response.send("not found");
-//     } else {
+//         response.send(html);
+//     });
+//   };
 
-//       response.send(pokemon);
-//     }
-//   });
+// app.get("/pokemon/:new", whenNewPokemonRequest);
+
+
+// response.send("yay");
 // });
 
-// app.get('/', (request, response) => {
-//   response.send("yay");
-// });
-
-// /**
-//  * ===================================
-//  * Listen to requests on port 3000
-//  * ===================================
-//  */
-// app.listen(3000, () => console.log('~~~ Tuning in to the waves of port 3000 ~~~'));
+/**
+ * ===================================
+ * Listen to requests on port 3000
+ * ===================================
+ */
+app.listen(3000, () => console.log('~~~ Tuning in to the waves of port 3000 ~~~'));
--- views/home.jsx
+++ views/home.jsx
@@ -6,7 +6,22 @@ class Home extends React.Component {
       <html>
         <body>
           <div>
-            <h1>Hello</h1>
+            <form>
+                ID:<br>
+                    <input type="text" name="id" value="id"><br>
+                Number:<br>
+                    <input type="text" name="number" value="number">
+                Name:<br>
+                  <input type="text" name="name" value="name">
+                Image:<br>
+                  <input type="text" name="image" value="image">
+                Height:<br>
+                  <input type="text" name="height" value="height">
+                Weight:<br>
+                  <input type="text" name="weight"value="weight">
+                  <br><br>
+                <input type="submit" value="Submit">
+            </form>
           </div>
         </body>
       </html>
