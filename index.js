a4b3df2865efb4fbf9c4eadb7bf51a4f93af375e
Stuart Myers <stuart.myers@gmail.com>
2020-01-16 15:43:11 +0800

    Tidy up

--- index.js
+++ index.js
@@ -36,40 +36,46 @@ const isValidInput = (pokemon) => {
 
     // if ID is not an integer.
     if (!parseInt(pokemon.id)) {
-      console.log('failed id');
+        console.log('failed id');
         return false;
     }
     // if num is not an integer.
     if (!parseInt(pokemon.num)) {
-      console.log('failed num');
+        console.log('failed num');
         return false;
     }
     // if name is empty
     if (!pokemon.name.trim()) {
-      console.log('failed name');
+        console.log('failed name');
         return false;
     }
     // if img is empty
     if (!pokemon.img.trim()) {
-      console.log('failed img');
+        console.log('failed img');
         return false;
     }
     // if height is not a number
     if (!pokemon.height) {
-      console.log('failed height');
+        console.log('failed height');
         return false;
     }
     // if weight is not a number
     if (!pokemon.weight) {
-      console.log('failed weight');
+        console.log('failed weight');
         return false;
     }
 
-    // if all is
+    if (!pokemon.candy) {
+        console.log('failed candy');
+        return false;
+    }
+
+    // if all works or is present.
     return true;
 
 }
 
+// This gets the pokemon details and returns it.
 const retrievePokemon = (request, response, err, obj) => {
 
     // check to make sure the file was properly read
@@ -121,11 +127,11 @@ app.get('/pokemon/new', (request, response) => {
     response.render('pokemonnew');
 })
 
-
+// Post to make a new pokemon
 app.post('/pokemon/', (request, response) => {
     console.log('Received POST');
     console.log(request.body);
-    
+
     const newPokemon = {
         id: parseInt(request.body.id),
         num: request.body.num,
@@ -160,7 +166,9 @@ app.post('/pokemon/', (request, response) => {
 
         // Not going to write the file, change this later after checking the Pokemon exists.
         jsonfile.writeFile(FILE, obj, (err) => {
-            if (err) {console.log(err)};
+            if (err) {
+                console.log(err)
+            };
             console.log('successfully written ' + newPokemon);
             response.send('Successfully added new pokemon ' + newPokemon.name);
             return;
@@ -168,7 +176,7 @@ app.post('/pokemon/', (request, response) => {
     })
 })
 
-
+// Put to replace details of pokemon at :id
 app.put('/pokemon/:id', (request, response) => {
     // console.log('Received PUT for id ' + request.params.id);
     console.log('*****************');
@@ -221,7 +229,7 @@ app.put('/pokemon/:id', (request, response) => {
     });
 });
 
-// get a Pokemon by ID.
+// get a Pokemon by ID (display the page)
 app.get('/pokemon/:id', (request, response) => {
     jsonfile.readFile(FILE, (err, obj) => {
         // get json from specified file
@@ -234,7 +242,7 @@ app.get('/pokemon/:id', (request, response) => {
     });
 });
 
-
+// get the dit page for pokemon at :id
 app.get('/pokemon/:id/edit', (request, response) => {
     jsonfile.readFile(FILE, (err, obj) => {
         // retrieve pokemon from specified file.
@@ -246,7 +254,7 @@ app.get('/pokemon/:id/edit', (request, response) => {
     });
 })
 
-
+// Test the page is working.
 app.get('/', (request, response) => {
     const data = {
         message: "yay"
@@ -254,9 +262,9 @@ app.get('/', (request, response) => {
     response.render('Home', data);
 });
 
-
+// catch all for a 404.
 app.get('*', function(request, response) {
-    const message = '404 Dunno';
+    const message = '404 Please try another address';
     const data = {
         message: message
     };
