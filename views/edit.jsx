786b3dcd0e80ba3cf2d72ed26a3cb0e2c1af750e
benjacoblee <benjacoblee@gmail.com>
2020-01-16 17:07:36 +0800

    added .jsx file, fixed some logic with index

--- views/edit.jsx
+++ views/edit.jsx
@@ -1,28 +1,47 @@
-var React = require('react');
+var React = require("react");
 class Edit extends React.Component {
   render() {
+    const filePath =
+      "/pokemon/" + (parseInt(this.props.indexPokemon.id) - 1) + "?_method=put";
+    console.log(filePath);
     return (
-        <html>
-        <title>{ this.props.indexPokemon.name }</title>
-        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous"/>
+      <html>
+        <title>{this.props.indexPokemon.name}</title>
+        <link
+          rel="stylesheet"
+          href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"
+          integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO"
+          crossorigin="anonymous"
+        />
         <body>
-            <div className="container">
-            <h1>Hello, { this.props.indexPokemon.name }!</h1>
-            <img src={this.props.indexPokemon.img} alt="Logo"/>
+          <div className="container">
+            <h1>Hello, {this.props.indexPokemon.name}!</h1>
+            <img src={this.props.indexPokemon.img} alt="Logo" />
             <h3>Height: {this.props.indexPokemon.height}</h3>
             <h3>Weight: {this.props.indexPokemon.weight}</h3>
             <h3>Candy: {this.props.indexPokemon.candy}</h3>
-            <form method="POST" action="/pokemon/:id?_method=put">
-            <div className="form-group">
+            <form method="POST" action={filePath}>
+              <div className="form-group">
                 <label for="exampleInputEmail1">Edit Form</label>
-                <input type="text" class="form-control" name="name" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Edit from here"/>
-                <small id="emailHelp" class="form-text text-muted">This allows you to edit the pokemon you want.</small>
-                </div>
-                <button type="submit" class="btn btn-primary">Edit</button>
-                </form>
-            </div>
+                <input
+                  type="text"
+                  class="form-control"
+                  name="name"
+                  id="exampleInputEmail1"
+                  aria-describedby="emailHelp"
+                  placeholder="Edit from here"
+                />
+                <small id="emailHelp" class="form-text text-muted">
+                  This allows you to edit the pokemon you want.
+                </small>
+              </div>
+              <button type="submit" class="btn btn-primary">
+                Edit
+              </button>
+            </form>
+          </div>
         </body>
-        </html>
+      </html>
     );
   }
 }
--- views/home.jsx
+++ views/home.jsx
@@ -6,7 +6,15 @@ class Home extends React.Component {
         <body>
           <div>
             <h1>Hello, { this.props.name }!</h1>
-            <form method="POST" action="/pokemon">ID:<input type="text" name="id"><br>Number:<input type="text" name="num"><br>Name:<input type="text" name="name"><br>Image:<input type="text" name="img"><br>Height:<input type="text" name="height"><br>Weight:<input type="text" name="weight"><br><input type="submit" value="Submit"></form>
+            <form method="POST" action="/pokemon">
+            ID:<input type="text" name="id"><br>
+            Number:<input type="text" name="num"><br>
+            Name:<input type="text" name="name"><br>
+            Image:<input type="text" name="img"><br>
+            Height:<input type="text" name="height"><br>
+            Weight:<input type="text" name="weight"><br>
+            <input type="submit" value="Submit">
+            </form>
           </div>
         </body>
       </html>
