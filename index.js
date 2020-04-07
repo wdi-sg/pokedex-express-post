fe52c9f92520baaa5c855d496b7a8a0062b05b7a
osbornechan <osbornecab@gmail.com>
2020-04-07 21:43:34 +0800

    Add and save new pokemon

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const jsonfile = require('jsonfile');
 
-const FILE = 'pokedex.json';
+const file = 'pokedex.json';
 
 /**
  * ===================================
@@ -12,23 +12,75 @@ const FILE = 'pokedex.json';
 // Init express app
 const app = express();
 
+// tell your app to use the module
+app.use(express.json());
+app.use(express.urlencoded({
+  extended: true
+}));
+
+const reactEngine = require('express-react-views').createEngine();
+app.engine('jsx', reactEngine);
+
+// this tells express where to look for the view files
+app.set('views', __dirname + '/views');
+
+// this line sets react to be the default view engine
+app.set('view engine', 'jsx');
+
 /**
  * ===================================
  * Routes
  * ===================================
  */
 
+app.post('/pokemon/new', (request, response) => {
+    //debug code (output request body)
+    console.log(request.body);
+
+    let newPokemonId = request.body.id;
+    let newPokemonNum = request.body.num;
+    let newPokemonName = request.body.name;
+    let newPokemonHeight = request.body.height;
+    let newPokemonWeight = request.body.weight;
+    let newPokemonImg = request.body.img;
+
+    let newPokemon = {
+        'id': newPokemonId,
+        'num': newPokemonNum,
+        'name': newPokemonName,
+        'height': newPokemonHeight,
+        'weight': newPokemonWeight,
+        'img': newPokemonImg
+    }
+
+    jsonfile.readFile(file, (err, obj) => {
+
+        obj.pokemon.push(newPokemon);
+
+        // save the request body
+        jsonfile.writeFile(file, obj, (err) => {
+            console.error(err)
+
+            // now look inside your json file
+            response.send(request.body);
+        });
+    });
+});
+
+app.get('/pokemon/new', (request, response) => {
+    response.render('form');
+});
+
 app.get('/pokemon/:id', (request, response) => {
 
   // get json from specified file
-  jsonfile.readFile(FILE, (err, obj) => {
-    
+  jsonfile.readFile(file, (err, obj) => {
+
     // check to make sure the file was properly read
-    if( err ){
-      
-      console.log("error with json read file:",err);
-      response.status(503).send("error reading filee");
-      return; 
+    if( err ) {
+      console.log("error with json read file:", err);
+      response.status(503).send("error reading file");
+      return;
     }
     // obj is the object from the pokedex json file
     // extract input data from request
@@ -47,20 +99,15 @@ app.get('/pokemon/:id', (request, response) => {
     }
 
     if (pokemon === undefined) {
-
       // send 404 back
       response.status(404);
       response.send("not found");
     } else {
-
       response.send(pokemon);
     }
   });
 });
 
-app.get('/', (request, response) => {
-  response.send("yay");
-});
 
 /**
  * ===================================
---
+++ views/form.jsx
@@ -0,0 +1,28 @@
+var React = require('react');
+class Form extends React.Component {
+    render() {
+        return (
+            <html>
+                <body>
+                    <form method='POST' action='/pokemon/new'>
+                        ID:<br/>
+                        <input type='number' name='id'/><br/><br/>
+                        Pokedex No.:<br/>
+                        <input type='number' name='num'/><br/><br/>
+                        Name:<br/>
+                        <input type='text' name='name'/><br/><br/>
+                        Height:<br/>
+                        <input type='text' name='height'/><br/><br/>
+                        Weight:<br/>
+                        <input type='text' name='weight'/><br/><br/>
+                        Image URL:<br/>
+                        <input type='url' name='img'/><br/><br/>
+                        <input type='submit' value='Submit'/>
+                    </form>
+                </body>
+            </html>
+        );
+    }
+}
+
+module.exports = Form;
