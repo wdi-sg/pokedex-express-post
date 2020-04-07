625b686028eec90a8cbe26840b16177001ec3749
Dyana Wu <dyanawu@gmail.com>
2020-04-07 17:15:31 +0800

    Set up for workflow

--- index.js
+++ index.js
@@ -1,70 +1,41 @@
+// Load modules and set up
 const express = require('express');
 const jsonfile = require('jsonfile');
-
 const FILE = 'pokedex.json';
-
-/**
- * ===================================
- * Configurations and set up
- * ===================================
- */
 
 // Init express app
 const app = express();
 
-/**
- * ===================================
- * Routes
- * ===================================
- */
+// Functions
+const getPokemon = function (req, res) {
+  let pokemon;
+  let inputId = Number(req.params.id);
+  let filePromise = jsonfile.readFile(FILE);
 
-app.get('/pokemon/:id', (request, response) => {
-
-  // get json from specified file
-  jsonfile.readFile(FILE, (err, obj) => {
-    
-    // check to make sure the file was properly read
-    if( err ){
-      
-      console.log("error with json read file:",err);
-      response.status(503).send("error reading filee");
-      return; 
-    }
-    // obj is the object from the pokedex json file
-    // extract input data from request
-    let inputId = parseInt( request.params.id );
-
-    var pokemon;
-
-    // find pokemon by id from the pokedex json file
-    for( let i=0; i<obj.pokemon.length; i++ ){
-
-      let currentPokemon = obj.pokemon[i];
-
-      if( currentPokemon.id === inputId ){
-        pokemon = currentPokemon;
+  filePromise
+    .then(obj => {
+      for(let mon of obj.pokemon){
+        if(mon.id === inputId ){
+          pokemon = mon;
+        }
       }
-    }
-
     if (pokemon === undefined) {
-
       // send 404 back
-      response.status(404);
-      response.send("not found");
+      res.status(404);
+      res.send("not found");
     } else {
-
-      response.send(pokemon);
+      res.send(pokemon);
     }
   });
-});
+};
+
+// Routes
+
+app.get('/pokemon/:id', getPokemon);
 
 app.get('/', (request, response) => {
   response.send("yay");
 });
 
-/**
- * ===================================
- * Listen to requests on port 3000
- * ===================================
- */
-app.listen(3000, () => console.log('~~~ Tuning in to the waves of port 3000 ~~~'));
+// Listen on 3000
+app.listen(3000, () => console.log('~~~ Tuning to port 3000 ~~~'));
