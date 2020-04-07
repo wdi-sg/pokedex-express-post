d0a9dff55dc4829f0fccc7d8ae0658613a34ace4
qundax <qunda.lim@outlook.sg>
2020-04-07 20:27:12 +0800

    sei-22-qunda - Added base functionality

--- index.js
+++ index.js
@@ -4,31 +4,52 @@ const jsonfile = require('jsonfile');
 const FILE = 'pokedex.json';
 
 /**
- * ===================================
- * Configurations and set up
- * ===================================
- */
+* ===================================
+* Configurations and set up
+* ===================================
+*/
 
 // Init express app
 const app = express();
 
+// Tell your app to use the module
+app.use(express.json());
+app.use(express.urlencoded({
+  extended: true
+}));
+
+// Sets layout to your react engine
+const reactEngine = require("express-react-views").createEngine();
+app.engine("jsx", reactEngine);
+
+// This tells express where to look for your view files
+app.set("views", "views");
+
+// This line sets react to be the default engine
+app.set("view engine", "jsx");
+
 /**
- * ===================================
- * Routes
- * ===================================
- */
+* ===================================
+* Routes
+* ===================================
+*/
+
+// GET requests
+app.get("/pokemon/new", (request, response) => {
+  response.render("home");
+})
 
 app.get('/pokemon/:id', (request, response) => {
 
   // get json from specified file
   jsonfile.readFile(FILE, (err, obj) => {
-    
+
     // check to make sure the file was properly read
     if( err ){
-      
+
       console.log("error with json read file:",err);
       response.status(503).send("error reading filee");
-      return; 
+      return;
     }
     // obj is the object from the pokedex json file
     // extract input data from request
@@ -62,9 +83,34 @@ app.get('/', (request, response) => {
   response.send("yay");
 });
 
+// POST request
+app.post("/pokemon", (request, response) => {
+  jsonfile.readFile(FILE, (error, data) => {
+    const inputs = request.body;
+    console.log(inputs.id);
+    const pokedex = data.pokemon;
+
+    pokedex[pokedex.length-1] = {
+      "id": parseInt(inputs.id),
+      "num": inputs.num,
+      "name": inputs.name,
+      "img": inputs.img,
+      "height": inputs.height,
+      "weight": inputs.weight
+    };
+
+    jsonfile.writeFile(FILE, data, (error) => {
+      console.log(error);
+
+      response.send("Thank you for helping complete the Pokedex!");
+    })
+
+  })
+})
+
 /**
- * ===================================
- * Listen to requests on port 3000
- * ===================================
- */
+* ===================================
+* Listen to requests on port 3000
+* ===================================
+*/
 app.listen(3000, () => console.log('~~~ Tuning in to the waves of port 3000 ~~~'));
