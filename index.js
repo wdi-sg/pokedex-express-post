d9d952d837886e17ecdf4e2132dfd9fc433ce4aa
wickker <choydianchun@gmail.com>
2020-04-07 18:06:48 +0800

    mvp done

--- index.js
+++ index.js
@@ -1,70 +1,95 @@
-const express = require('express');
-const jsonfile = require('jsonfile');
-
-const FILE = 'pokedex.json';
-
-/**
- * ===================================
- * Configurations and set up
- * ===================================
- */
-
-// Init express app
+const express = require("express");
+const jsonfile = require("jsonfile");
+const FILE = "pokedex.json";
 const app = express();
 
-/**
- * ===================================
- * Routes
- * ===================================
- */
+// this line below, sets a layout look to your express project
+const reactEngine = require("express-react-views").createEngine();
+app.engine("jsx", reactEngine);
 
-app.get('/pokemon/:id', (request, response) => {
+// this tells express where to look for the view files
+app.set("views", __dirname + "/views");
 
-  // get json from specified file
+// this line sets react to be the default view engine
+app.set("view engine", "jsx");
+
+// tell your app to use the module
+app.use(express.json());
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+
+//----------------------------
+//----------------------------
+
+app.post("/pokemon", (req, res) => {
+  console.log(req.body);
   jsonfile.readFile(FILE, (err, obj) => {
-    
-    // check to make sure the file was properly read
-    if( err ){
-      
-      console.log("error with json read file:",err);
-      response.status(503).send("error reading filee");
-      return; 
+    if (err) {
+      console.log("error with json read file:", err);
+      res.status(503).send("error reading file");
+      return;
     }
-    // obj is the object from the pokedex json file
-    // extract input data from request
-    let inputId = parseInt( request.params.id );
-
-    var pokemon;
-
-    // find pokemon by id from the pokedex json file
-    for( let i=0; i<obj.pokemon.length; i++ ){
-
-      let currentPokemon = obj.pokemon[i];
-
-      if( currentPokemon.id === inputId ){
-        pokemon = currentPokemon;
+    obj["pokemon"].push(req.body);
+    jsonfile.writeFile(FILE, obj, (err) => {
+      if (err) {
+        console.log("error with json write file:", err);
+        res.status(503).send("error writing file");
+        return;
       }
-    }
-
-    if (pokemon === undefined) {
-
-      // send 404 back
-      response.status(404);
-      response.send("not found");
-    } else {
-
-      response.send(pokemon);
-    }
+      res.send(obj);
+    });
   });
 });
 
-app.get('/', (request, response) => {
-  response.send("yay");
+app.get("/pokemon/new", (req, res) => {
+  res.render("pokemon-form");
 });
 
-/**
- * ===================================
- * Listen to requests on port 3000
- * ===================================
- */
-app.listen(3000, () => console.log('~~~ Tuning in to the waves of port 3000 ~~~'));
+// app.get('/pokemon/:id', (request, response) => {
+
+//   // get json from specified file
+//   jsonfile.readFile(FILE, (err, obj) => {
+
+//     // check to make sure the file was properly read
+//     if( err ){
+
+//       console.log("error with json read file:",err);
+//       response.status(503).send("error reading file");
+//       return;
+//     }
+//     // obj is the object from the pokedex json file
+//     // extract input data from request
+//     let inputId = parseInt( request.params.id );
+
+//     var pokemon;
+
+//     // find pokemon by id from the pokedex json file
+//     for( let i=0; i<obj.pokemon.length; i++ ){
+
+//       let currentPokemon = obj.pokemon[i];
+
+//       if( currentPokemon.id === inputId ){
+//         pokemon = currentPokemon;
+//       }
+//     }
+
+//     if (pokemon === undefined) {
+
+//       // send 404 back
+//       response.status(404);
+//       response.send("not found");
+//     } else {
+
+//       response.send(pokemon);
+//     }
+//   });
+// });
+
+app.get("/", (request, response) => {
+  response.send("Site up and running!");
+});
+
+app.listen(3000, () => console.log("~~~ Tuning in to the waves of port 3000 ~~~"));
---
+++ views/pokemon-form.jsx
@@ -0,0 +1,26 @@
+var React = require("react");
+
+class Form extends React.Component {
+  render() {
+    return (
+      <html>
+        <body>
+          <div>
+            <form method="POST" action="/pokemon">
+              <h2>Add New Pokemon:</h2>
+              <input type="text" name="id" placeholder="id (above 151)"/><br></br>
+              <input type="text" name="num" placeholder="number (above 151)"/><br></br>
+              <input type="text" name="name" placeholder="name"/><br></br>
+              <input type="text" name="img" placeholder="image link"/><br></br>
+              <input type="text" name="height" placeholder="height"/><br></br>
+              <input type="text" name="weight" placeholder="weight"/><br></br>
+              <input type="submit" value="Submit"/>
+            </form>
+          </div>
+        </body>
+      </html>
+    );
+  }
+}
+
+module.exports = Form;
