3007b76ede8cef2e3d6074bb9c67d701e5aca1b6
mdazharaw <darkaz62@hotmail.com>
2020-04-07 19:44:07 +0800

    Initial commit

--- index.js
+++ index.js
@@ -17,50 +17,81 @@ const app = express();
  * Routes
  * ===================================
  */
+// app.post('/pokemon', function (request, response) {
 
+//   //debug code (output request body)
+//   console.log(request.body.num);
+
+
+//   // save the request body
+//   jsonfile.writeFile('pokedex.json', request.body, (err) => {
+//     console.error(err)
+
+//     // now look inside your json file
+//     response.send(request.body);
+//   });
+// });
 app.get('/pokemon/:id', (request, response) => {
 
   // get json from specified file
   jsonfile.readFile(FILE, (err, obj) => {
-    
+
     // check to make sure the file was properly read
-    if( err ){
-      
-      console.log("error with json read file:",err);
+    if (err) {
+
+      console.log("error with json read file:", err);
       response.status(503).send("error reading filee");
-      return; 
+      return;
     }
     // obj is the object from the pokedex json file
     // extract input data from request
-    let inputId = parseInt( request.params.id );
+    if (request.params.id === "new") {
+      response.send(`<form method="POST" action="/pokemon">
+      Enter Pokemon:<br/><br/>
+      ID:&emsp;<input type="number" name="id"><br/><br/>
+      Num:&emsp;<input type="number" name="num"><br/><br/>
+      Name:&emsp;<input type="text" name="name"><br/><br/>
+      Img:&emsp;<input type="text" name="img"><br/><br/>
+      Height:&emsp;<input type="text" name="height"><br/><br/>
+      Weight:&emsp;<input type="text" name="weight"><br/><br/>
+      <input type="submit" value="Submit">
+    </form>`);
+    }
+    else {
 
-    var pokemon;
+      let inputId = parseInt(request.params.id);
 
-    // find pokemon by id from the pokedex json file
-    for( let i=0; i<obj.pokemon.length; i++ ){
+      var pokemon;
 
-      let currentPokemon = obj.pokemon[i];
+      // find pokemon by id from the pokedex json file
+      for (let i = 0; i < obj.pokemon.length; i++) {
 
-      if( currentPokemon.id === inputId ){
-        pokemon = currentPokemon;
+        let currentPokemon = obj.pokemon[i];
+
+        if (currentPokemon.id === inputId) {
+          pokemon = currentPokemon;
+        }
+      }
+
+      if (pokemon === undefined) {
+
+        // send 404 back
+        response.status(404);
+        response.send("not found");
+      } else {
+
+        response.send(pokemon);
       }
     }
-
-    if (pokemon === undefined) {
-
-      // send 404 back
-      response.status(404);
-      response.send("not found");
-    } else {
-
-      response.send(pokemon);
-    }
   });
-});
+}
+
+);
 
 app.get('/', (request, response) => {
   response.send("yay");
 });
+
 
 /**
  * ===================================
