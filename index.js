f58baab38dfeeb271ea85f6774c347e13f0771d0
jordanswp <sohweepin@gmail.com>
2020-04-08 22:06:56 +0800

    done second part

--- index.js
+++ index.js
@@ -1,16 +1,37 @@
 const express = require('express');
 const jsonfile = require('jsonfile');
+const app = express();
+
+
+// this line below, sets a layout look to your express project
+const reactEngine = require('express-react-views').createEngine();
+app.engine('jsx', reactEngine);
+
+// this tells express where to look for the view files
+app.set('views', __dirname + '/views');
+
+// this line sets react to be the default view engine
+app.set('view engine', 'jsx');
+
 
 const FILE = 'pokedex.json';
+
+app.get('/pokemon', (req, res) => {
+  // running this will let express to run home.handlebars file in your views folder
+      jsonfile.readFile(FILE, (err, obj) => {
+      const data = {pokemon: obj.pokemon}
+      res.render('index', data)
+    });
+});
+
+
+
 
 /**
  * ===================================
  * Configurations and set up
  * ===================================
  */
-
-// Init express app
-const app = express();
 
 /**
  * ===================================
