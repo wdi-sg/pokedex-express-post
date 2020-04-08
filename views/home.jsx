dd772d6957b329731cfd31f7702615cce138ab20
Nausheen_S <salauddinnausheen@gmail.com>
2020-04-08 21:06:34 +0800

    created deliverable 1 of part2

--- views/home.jsx
+++ views/home.jsx
@@ -3,13 +3,16 @@ class Form extends React.Component {
     render(){
         return(
             <form method="POST" action="/pokemon">
-            Pokemon details:
-            id<input type = "number" name = "id"/>
-            number<input type = "number" name = "num"/>
-            name<input type="text" name="name"/>
-            image<input type = "text" name="image"/>
-            height<input type = "number" name = "height"/>
-            weight<input type = "number" name = "weight"/>
+            Pokemon details:<br/>
+
+            <input type = "number" name = "id"placeholder = "id"/><br/>
+
+            <input type = "number" name = "num" placeholder = "number"/><br/>
+
+            <input type="text" name="name" placeholder = "name"/><br/>
+            <input type = "text" name="image" placeholder ="image"/><br/>
+            <input type = "number" name = "height" placeholder ="height"/><br/>
+            <input type = "number" name = "weight" placeholder ="weight"/><br/>
             <input type="submit" value="Submit"/>
             </form>
             )
---
+++ views/main.jsx
@@ -0,0 +1,21 @@
+var React = require('react');
+
+class Main extends React.Component {
+  render() {
+
+    return (
+      <div>
+        <p>POKEMON:{this.proms.name}</p>
+        <p>ID:{this.props.id}</p>
+        <p>NUM:{this.props.num}</p>
+        <p>NAME:{this.props.name}</p>
+        <p>IMAGE:{this.props.img}</p>
+        <p>HEIGHT:{this.props.height}</p>
+        <p>WEIGHT:{this.props.weight}</p>
+
+      </div>
+    );
+  }
+}
+
+module.exports = Main;
