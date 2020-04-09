edead47f82e61cd661328a4549c602d7934d4dc8
ReadyHash <Readyhash@gmail.com>
2020-04-09 10:06:27 +0800

    Added function and modules

--- index.js
+++ index.js
@@ -5,13 +5,14 @@
  */
 
 console.log("Poke express node is working..")
+console.log("------------------------------")
+// Importing modules
 const jsonfile = require('jsonfile');
 const express = require('express');
-// Init express app
-const app = express();
 const reactEngine = require('express-react-views').createEngine();
 
-
+// Init express app
+const app = express();
 // tell your app to use the module
 app.engine('jsx', reactEngine);
 // this line set views to view directory
@@ -19,22 +20,38 @@ app.set('views', __dirname + '/views');
 
 // this line sets react to be the default view engine
 app.set('view engine', 'jsx');
-
+//
 app.use(express.json());
 app.use(express.urlencoded({
-  extended: true
+extended: true
 }));
 
-let FILE = "./pokedex.json";
-let newDex = "./newpokedex.json"
+
+let pokedexData = "./pokedex.json";
+let newDex = "./newpokedex.json";
+let formPage = "./views/form.jsx";
 /**
  * ===================================
  * Routes
  * ===================================
  */
 
+
+
+
+app.get('/pokemon/index', (request, response) => {
+    jsonfile.readFile(pokedexData, (err, obj) => {
+        console.log("reading from pokedex....")
+        let pokemon = obj.pokemon
+
+    })
+response.send(obj);
+    // response.send("welcome to the index page");
+})
+
+
 app.get('/pokemon/new', (request, response) => {
-    jsonfile.readFile(FILE, (err,obj) => {
+    jsonfile.readFile(pokedexData, (err,obj) => {
         console.log("reading from pokedex....")
         let pokedex = obj.pokemon
         response.render('form.jsx', pokedex);
@@ -44,18 +61,22 @@ app.get('/pokemon/new', (request, response) => {
 const uploadpokedex = (request, response) =>{
   //your inputs
    console.log(request.body);
-  jsonfile.writeFile(file, request.body, (err) => {
+  jsonfile.writeFile(pokedexData, request.body, (err) => {
     console.error(err)
     response.send(request.body);
   });
 }
 
+
+
 app.post(newDex, uploadpokedex);
+
+
 
 app.get('/pokemon/:id', (request, response) => {
 
   // get json from specified file
-  jsonfile.readFile(FILE, (err, obj) => {
+  jsonfile.readFile(pokedexData, (err, obj) => {
 
     // check to make sure the file was properly read
     if( err ){
@@ -96,9 +117,11 @@ app.get('/pokemon/:id', (request, response) => {
   });
 });
 
+
+
 app.get('/', (request, response) => {
   response.send("yay");
-  console.log(FILE)
+  console.log(pokedexData)
 });
 
 /**
@@ -106,4 +129,6 @@ app.get('/', (request, response) => {
  * Listen to requests on port 3000
  * ===================================
  */
+
+
 app.listen(3000, () => console.log('~~~ Tuning in to the waves of port 3000 ~~~'));
--- views/form.jsx
+++ views/form.jsx
@@ -2,6 +2,7 @@ var React = require('react');
 class Forms extends React.Component {
   render() {
     console.log(this.props)
+    // <h4>Image</h4><input type= "image" name= "image"/>
     return (
       <html>
         <body>
@@ -11,7 +12,7 @@ class Forms extends React.Component {
                 <h4>ID number</h4><input type= "number" name= "id"/>
                 <h4>Pokemon number</h4><input type= "number" name= "num"/>
                 <h4>Pokemon Name</h4><input type= "text" name= "name"/>
-                <h4>Image</h4><input type= "image" name= "image"/>
+
                 <h4>Height</h4><input type= "number" name= "height"/>
                 <h4>Weight</h4><input type= "number" name= "weight"/>
                 <input type= "submit" name= "submit"/>
